7aa4b6ad98376d354ce0185b35f1cb903c5c8ff1
Paul <gafgafgarion05@gmail.com>
2022-03-16 18:18:16 +0300

    init

---
+++ 7 kyu Exclamation marks series #8 Move all exclamation marks to the end of the sentence.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Exclamation marks series #8: Move all exclamation marks to the end of the sentence
+
+
+def remove(s):
+    pass
+
+
+print(remove("Hi!"), "Hi!")
+print(remove("Hi! Hi!"), "Hi Hi!!")
+print(remove("Hi! Hi! Hi!"), "Hi Hi Hi!!!")
+print(remove("Hi! !Hi Hi!"), "Hi Hi Hi!!!")
+print(remove("Hi! Hi!! Hi!"), "Hi Hi Hi!!!!")
