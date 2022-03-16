b716bbecd2dfb87e48c998f0bd772f3d2a1a79ac
Paul <gafgafgarion05@gmail.com>
2022-03-16 18:11:31 +0300

    init

---
+++ 7 kyu Odder Than the Rest.py
@@ -0,0 +1,12 @@
+def odd_one(arr):
+    pass
+
+
+# test.it("Expected '[2,4,6,7,10]' = 3")
+print(odd_one([2, 4, 6, 7, 10]), 3)
+# test.it("Expected '[2,16,98,10,13,78]' = 4")
+print(odd_one([2, 16, 98, 10, 13, 78]), 4)
+# test.it("Expected '[4,-8,98,-12,-7,90,100]' = 4")
+print(odd_one([4, -8, 98, -12, -7, 90, 100]), 4)
+# test.it("Expected '[2,4,6,8]' = -1")
+print(odd_one([2, 4, 6, 8]), -1)
