1433f5ff0ba2ba06d236897b08116a3ebb2e6287
Paul <gafgafgarion05@gmail.com>
2022-03-17 12:22:19 +0300

    1

--- 7 kyu Bingo ( Or Not ).py
+++ 7 kyu Bingo ( Or Not ).py
@@ -1,9 +1,14 @@
 def bingo(array):
-    # your code here
-    pass
+    cnt_bingo = 5
+    for i in array:
+        print(chr(i+96))
 
 
 print(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), "LOSE")
 print(bingo([20, 12, 23, 14, 6, 22, 12, 17, 2, 26]), "LOSE")
 print(bingo([1, 2, 3, 7, 5, 14, 7, 15, 9, 10]), "WIN")
 print(bingo([5, 2, 13, 7, 5, 14, 17, 15, 9, 10]), "WIN")
+
+
+# print(ord('a'))
+# print(chr(97))
