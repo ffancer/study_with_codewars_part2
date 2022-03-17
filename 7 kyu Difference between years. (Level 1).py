ebced1d84058a73f6c4e92851efdf7c6d5a1d581
Paul <gafgafgarion05@gmail.com>
2022-03-17 12:15:16 +0300

    init

---
+++ 7 kyu Difference between years. (Level 1).py
@@ -0,0 +1,9 @@
+def how_many_years(date1, date2):
+    return date1 + date2
+
+
+print(how_many_years('1997/10/10', '2015/10/10'), 18)
+print(how_many_years('1990/10/10', '2015/10/10'), 25)
+print(how_many_years('2015/10/10', '1990/10/10'), 25)
+print(how_many_years('1992/10/24', '2015/10/24'), 23)
+print(how_many_years('2018/10/10', '2000/10/10'), 18)
