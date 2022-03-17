8f77355d4e8234ed38b5d3fcb31b84afa67aa0de
Paul <gafgafgarion05@gmail.com>
2022-03-17 10:19:46 +0300

    2

--- 7 kyu The Poet And The Pendulum.py
+++ 7 kyu The Poet And The Pendulum.py
@@ -1,7 +1,17 @@
 def pendulum(values):
     lst_middle = [min(values)]
     values.remove(min(values))
-    return values, lst_middle
+    lst_left, lst_right = [], []
+    cnt = 0
+    while values:
+        min_val = min(values)
+        if cnt % 2 == 0:
+            lst_right.append(min_val)
+        else:
+            lst_left.append(min_val)
+        cnt += 1
+        values.remove(min(values))
+    return lst_left, lst_right
 
 print(pendulum([4, 6, 8, 7, 5]), [8, 6, 4, 5, 7])
 print(pendulum([19, 30, 16, 19, 28, 26, 28, 17, 21, 17]), [28, 26, 19, 17, 16, 17, 19, 21, 28, 30])
