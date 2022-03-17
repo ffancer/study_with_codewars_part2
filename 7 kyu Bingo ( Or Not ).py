31f22233061ae0bee9f2f321d70f2816996cbc97
Paul <gafgafgarion05@gmail.com>
2022-03-17 12:19:48 +0300

    init

---
+++ 7 kyu Bingo ( Or Not ).py
@@ -0,0 +1,9 @@
+def bingo(array):
+    # your code here
+    pass
+
+
+print(bingo([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), "LOSE")
+print(bingo([20, 12, 23, 14, 6, 22, 12, 17, 2, 26]), "LOSE")
+print(bingo([1, 2, 3, 7, 5, 14, 7, 15, 9, 10]), "WIN")
+print(bingo([5, 2, 13, 7, 5, 14, 17, 15, 9, 10]), "WIN")
