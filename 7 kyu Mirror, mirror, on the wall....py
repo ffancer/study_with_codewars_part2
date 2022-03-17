b626f659c4cbfe4737e90f649621940e6156b76d
Paul <gafgafgarion05@gmail.com>
2022-03-17 09:16:19 +0300

    init

---
+++ 7 kyu Mirror, mirror, on the wall....py
@@ -0,0 +1,10 @@
+def mirror(data: list) -> list:
+    pass
+
+
+print(mirror([]), [])
+print(mirror([1]), [1])
+print(mirror([2, 1]), [1, 2, 1])
+print(mirror([1, 3, 2]), [1, 2, 3, 2, 1])
+print(mirror([-8, 42, 18, 0, -16]), [-16, -8, 0, 18, 42, 18, 0, -8, -16])
+print(mirror([-3, 15, 8, -1, 7, -1]), [-3, -1, -1, 7, 8, 15, 8, 7, -1, -1, -3])
