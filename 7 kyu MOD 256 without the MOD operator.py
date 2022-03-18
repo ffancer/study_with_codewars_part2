64796a463ae7019d55d36e181ec5d82646ea5703
Paul <gafgafgarion05@gmail.com>
2022-03-18 14:02:15 +0300

    init

---
+++ 7 kyu MOD 256 without the MOD operator.py
@@ -0,0 +1,10 @@
+def mod256_without_mod(number):
+    pass
+
+
+print(mod256_without_mod(254), 254)
+print(mod256_without_mod(256), 0)
+print(mod256_without_mod(258), 2)
+print(mod256_without_mod(-254), 2)
+print(mod256_without_mod(-256), 0)
+print(mod256_without_mod(-258), 254)
