523501765bf3fa3e3d72c33dd0ded94732112fc6
Paul <gafgafgarion05@gmail.com>
2022-03-18 14:10:44 +0300

    1

--- 7 kyu Stanton measure.py
+++ 7 kyu Stanton measure.py
@@ -1,5 +1,5 @@
 def stanton_measure(arr):
-    return  # stanton measure of the array
+    how_much_1 = arr.count(1)
+    return how_much_1
 
-
-print(stanton_measure([1, 4, 3, 2, 1, 2, 3, 2]), 3);
+print(stanton_measure([1, 4, 3, 2, 1, 2, 3, 2]), 3)
