f534bfd008ed1dc5ad4ad79f7059b62002088421
Paul <gafgafgarion05@gmail.com>
2022-03-18 13:48:40 +0300

    my solution

--- 7 kyu Battle of the characters (Easy).py
+++ 7 kyu Battle of the characters (Easy).py
@@ -1,7 +1,15 @@
+"""
+Сегодня мой отец умер, в 6 утра. Пусть эта информация затеряется здесь, словно капля во время дождя.
+Сижу и продолжаю решать задачи.
+"""
+
 def battle(x, y):
     cnt_x, cnt_y = 0, 0
+
     cnt_x = sum(ord(i) - 64 for i in x)
-    print(cnt_x)
+    cnt_y = sum(ord(i) - 64 for i in y)
+
+    return x if cnt_x > cnt_y else y if cnt_y > cnt_x else 'Tie!'
 
 
 print(battle("AAA", "Z"), "Z", "Unfair fight!")
