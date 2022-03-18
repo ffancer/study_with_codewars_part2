c32b99d1891a4bf1d941ac9175d412f9e2abf174
Paul <gafgafgarion05@gmail.com>
2022-03-18 13:32:35 +0300

    init

---
+++ 7 kyu Battle of the characters (Easy).py
@@ -0,0 +1,9 @@
+def battle(x, y):
+    # Let the battle begin!
+    pass
+
+
+print(battle("AAA", "Z"), "Z", "Unfair fight!")
+print(battle("ONE", "TWO"), "TWO", "Unfair fight!")
+print(battle("ONE", "NEO"), "Tie!", "Unfair fight!")
+print(battle("FOUR", "FIVE"), "FOUR", "Unfair fight!")
