42a5418f0aed8e68837d2e61606e86057a367dc8
Paul <gafgafgarion05@gmail.com>
2022-03-18 10:01:56 +0300

    init

---
+++ 7 kyu Substring fun.py
@@ -0,0 +1,10 @@
+def nth_char(words):
+    pass
+
+
+print(nth_char(['yoda', 'best', 'has']), 'yes')
+print(nth_char([]), '')
+print(nth_char(['X-ray']), 'X')
+print(nth_char(['No', 'No']), 'No')
+print(nth_char(['Chad', 'Morocco', 'India', 'Algeria', 'Botswana', 'Bahamas', 'Ecuador', 'Micronesia']),
+      'Codewars')
