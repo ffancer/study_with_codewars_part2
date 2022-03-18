69791dd14c3f9e3bb6def12c286ead1d1284b0db
Paul <gafgafgarion05@gmail.com>
2022-03-18 10:27:38 +0300

    init

---
+++ 7 kyu Cat Years, Dog Years (2).py
@@ -0,0 +1,8 @@
+def owned_cat_and_dog(cat_years, dog_years):
+    # Your code here
+    return [0, 0]
+
+
+print(owned_cat_and_dog(15, 15), [1, 1])
+print(owned_cat_and_dog(24, 24), [2, 2])
+print(owned_cat_and_dog(56, 64), [10, 10])
