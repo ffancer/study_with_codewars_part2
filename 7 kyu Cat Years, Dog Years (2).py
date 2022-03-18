f9958b812191e23bd67a9b7343f4a043c0b99822
Paul <gafgafgarion05@gmail.com>
2022-03-18 10:33:29 +0300

    1

--- 7 kyu Cat Years, Dog Years (2).py
+++ 7 kyu Cat Years, Dog Years (2).py
@@ -1,6 +1,21 @@
 def owned_cat_and_dog(cat_years, dog_years):
-    # Your code here
-    return [0, 0]
+    cat_ans, dog_ans = 0, 0
+
+    if cat_years == 15:
+        cat_ans += 1
+    elif cat_years == 24:
+        cat_ans += 2
+    else:
+        cat_ans = (cat_years - 24) // 4 + 2
+
+    if dog_years == 15:
+        dog_ans += 1
+    elif dog_years == 24:
+        dog_ans += 2
+    else:
+        dog_ans = (dog_years - 24) // 5 + 2
+
+    return cat_ans, dog_ans
 
 
 print(owned_cat_and_dog(15, 15), [1, 1])
