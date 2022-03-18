ad334fa418736c92757bc70472301c1da9defba2
Paul <gafgafgarion05@gmail.com>
2022-03-18 13:49:27 +0300

    my solution more compact

--- 7 kyu Battle of the characters (Easy).py
+++ 7 kyu Battle of the characters (Easy).py
@@ -3,11 +3,9 @@
 Сижу и продолжаю решать задачи.
 """
 
-def battle(x, y):
-    cnt_x, cnt_y = 0, 0
 
-    cnt_x = sum(ord(i) - 64 for i in x)
-    cnt_y = sum(ord(i) - 64 for i in y)
+def battle(x, y):
+    cnt_x, cnt_y = sum(ord(i) - 64 for i in x), sum(ord(i) - 64 for i in y)
 
     return x if cnt_x > cnt_y else y if cnt_y > cnt_x else 'Tie!'
 
