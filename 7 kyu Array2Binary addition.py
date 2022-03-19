6c0d598dbc345155102949895d3cb1d262677876
Paul <gafgafgarion05@gmail.com>
2022-03-19 09:14:23 +0300

    my 1 line solution

--- 7 kyu Array2Binary addition.py
+++ 7 kyu Array2Binary addition.py
@@ -1,14 +1,8 @@
 def arr2bin(arr):
-    ans = 0
-
-    for i in arr:
-        if type(i) != int:
-            return False
-        ans += i
-
-    return bin(ans)[2:]
+    return bin(sum(i for i in arr))[2:] if all(type(i) == int for i in arr) else False
 
 
 print(arr2bin([1, 2]), "11")
 print(arr2bin([1, 2, 3, 4, 5]), "1111")
 print(arr2bin([1, 10, 100, 1000]), "10001010111")
+print(arr2bin([1, 10, 100, 'bb']), "False")
