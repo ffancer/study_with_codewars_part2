63701b87ee1ed9a55f255b09bddf018c1ebbd205
Paul <gafgafgarion05@gmail.com>
2022-03-19 09:20:46 +0300

    init

---
+++ 7 kyu The Office VI - Sabbatical.py
@@ -0,0 +1,8 @@
+def sabb(s, val, happiness):
+    return
+
+
+print(sabb("Can I have a sabbatical?", 5, 5), "Sabbatical! Boom!")
+print(sabb("Why are you shouting?", 7, 2), "Back to your desk, boy.")
+print(sabb("What do you mean I cant learn to code??", 8, 9), "Sabbatical! Boom!")
+print(sabb("Please calm down", 9, 1), "Back to your desk, boy.")
