3b0feb6d8f1ad3c88c90c66d3b6db0e4477d3d01
Paul <gafgafgarion05@gmail.com>
2022-03-19 09:58:09 +0300

    my solution

--- 7 kyu Capitals first!.py
+++ 7 kyu Capitals first!.py
@@ -1,5 +1,15 @@
 def capitals_first(text):
-    return ' '.join(sorted(text.split(), key=str.upper))
+    text = text.split()
+    lst_upper, lst_lower = [], []
+
+    for i in text:
+        if i[0].isupper():
+            lst_upper.append(i)
+
+        if i[0].islower():
+            lst_lower.append(i)
+
+    return ' '.join(lst_upper + lst_lower)
 
 
 print(capitals_first("sense Does to That Make you?"), "Does That Make sense to you?")
