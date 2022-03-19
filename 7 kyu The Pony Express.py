06aeec0a1f0cb0d62d860e3e7027a4a189424886
Paul <gafgafgarion05@gmail.com>
2022-03-19 10:06:48 +0300

    init

---
+++ 7 kyu The Pony Express.py
@@ -0,0 +1,9 @@
+def riders(stations):
+    # Your code here!
+    pass
+
+
+print(riders([18, 15]), 1)
+print(riders([43, 23, 40, 13]), 2)
+print(riders([33, 8, 16, 47, 30, 30, 46]), 3)
+print(riders([6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49]), 4)
