5c276b17a958c69214583322882a294436a63e30
Paul <gafgafgarion05@gmail.com>
2022-03-19 09:46:51 +0300

    1

--- 7 kyu Capitals first!.py
+++ 7 kyu Capitals first!.py
@@ -1,6 +1,5 @@
 def capitals_first(text):
-    # biggies first...
-    pass
+    return ' '.join(sorted(text.split(), key=str.upper))
 
 
 print(capitals_first("sense Does to That Make you?"), "Does That Make sense to you?")
