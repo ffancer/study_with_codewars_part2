c98d939c1f060b178568378b8fccd42c0e5c9a3d
Paul <gafgafgarion05@gmail.com>
2022-03-20 13:34:01 +0300

    init

---
+++ 7 kyu Histogram - H1.py
@@ -0,0 +1,21 @@
+def histogram(results):
+    pass
+
+
+res = """6|##### 5
+5|
+4|# 1
+3|########## 10
+2|### 3
+1|####### 7
+"""
+print(histogram([7, 3, 10, 1, 0, 5]), res)
+
+res = """6|
+5|
+4|
+3|
+2|
+1|
+"""
+print(histogram([0, 0, 0, 0, 0, 0]), res)
