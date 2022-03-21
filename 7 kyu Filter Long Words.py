624fa4d23fb75a29f8eeb88ee58b9a4d5c26529b
Paul <gafgafgarion05@gmail.com>
2022-03-21 10:15:16 +0300

    my 1 line solution

--- 7 kyu Filter Long Words.py
+++ 7 kyu Filter Long Words.py
@@ -1,12 +1,5 @@
 def filter_long_words(sentence, n):
-    sentence = sentence.split()
-    lst = []
-
-    for i in sentence:
-        if len(i) > n:
-            lst.append(i)
-
-    return lst
+    return [i for i in sentence.split() if len(i) > n]
 
 
 print(filter_long_words("The quick brown fox jumps over the lazy dog", 4), ['quick', 'brown', 'jumps'])
