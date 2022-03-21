b6b7683be79c8500c7a97c84799ea8062df72597
Paul <gafgafgarion05@gmail.com>
2022-03-21 13:35:15 +0300

    my solution

--- 7 kyu Delta Bits.py
+++ 7 kyu Delta Bits.py
@@ -1,13 +1,10 @@
 def convert_bits(a, b):
     a = bin(a)[2:].zfill(32)
     b = bin(b)[2:].zfill(32)
-    #
-    c = a.count('0') + a.count('1')
-    d = b.count('0') + b.count('1')
-    return c, d
-    # return a, b
+
+    return sum(1 for i, j in zip(a, b) if i != j)
 
 print(convert_bits(31, 14), 2)
-# print(convert_bits(7, 17), 3)
-# print(convert_bits(31, 0), 5)
-# print(convert_bits(0, 0), 0)
+print(convert_bits(7, 17), 3)
+print(convert_bits(31, 0), 5)
+print(convert_bits(0, 0), 0)
