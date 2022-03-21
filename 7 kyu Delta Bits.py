676e83b43bb7c8f2510c5445ed02c1019cf67b34
Paul <gafgafgarion05@gmail.com>
2022-03-21 13:33:38 +0300

    3

--- 7 kyu Delta Bits.py
+++ 7 kyu Delta Bits.py
@@ -1,11 +1,11 @@
 def convert_bits(a, b):
-    a = abs(len(bin(a)[2:]) - len(bin(b)[2:]))
-    cnt = 0
-    for i in bin(a)[2:]:
-        if i not in bin(b)[2:]:
-            cnt += 1
-    return cnt
-
+    a = bin(a)[2:].zfill(32)
+    b = bin(b)[2:].zfill(32)
+    #
+    c = a.count('0') + a.count('1')
+    d = b.count('0') + b.count('1')
+    return c, d
+    # return a, b
 
 print(convert_bits(31, 14), 2)
 # print(convert_bits(7, 17), 3)
