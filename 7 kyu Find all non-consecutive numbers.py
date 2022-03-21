db477eb0597cf051d19a21ed4aa1dae28cba7d30
Paul <gafgafgarion05@gmail.com>
2022-03-21 17:14:09 +0300

    1

--- 7 kyu Find all non-consecutive numbers.py
+++ 7 kyu Find all non-consecutive numbers.py
@@ -1,5 +1,10 @@
 def all_non_consecutive(arr):
-    pass
+    lst = []
+
+    for i in range(min(arr), max(arr) + 1):
+        lst.append(i)
+
+    return lst
 
 
 print(all_non_consecutive([1, 2, 3, 4, 6, 7, 8, 10]), [{'i': 4, 'n': 6}, {'i': 7, 'n': 10}])
