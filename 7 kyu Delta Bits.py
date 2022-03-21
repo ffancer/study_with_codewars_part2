e18ca89a8bcd65730f81dc835578539778956bd0
Paul <gafgafgarion05@gmail.com>
2022-03-21 13:26:44 +0300

    1

--- 7 kyu Delta Bits.py
+++ 7 kyu Delta Bits.py
@@ -1,8 +1,8 @@
 def convert_bits(a, b):
-    pass
+    print(bin(a)[2:], bin(b)[2:], sep='\n')
 
 
 print(convert_bits(31, 14), 2)
-print(convert_bits(7, 17), 3)
-print(convert_bits(31, 0), 5)
-print(convert_bits(0, 0), 0)
+# print(convert_bits(7, 17), 3)
+# print(convert_bits(31, 0), 5)
+# print(convert_bits(0, 0), 0)
