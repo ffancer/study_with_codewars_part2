6e35ed581bc76bc442c698f9433aae232dee62dd
Paul <gafgafgarion05@gmail.com>
2022-03-22 13:33:12 +0300

    init

---
+++ 7 kyu Well of Ideas - Harder Version.py
@@ -0,0 +1,8 @@
+def well(arr):
+    pass
+
+
+print(well([['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad']]), 'Fail!')
+print(well([['gOOd', 'bad', 'BAD', 'bad', 'bad'], ['bad', 'bAd', 'bad'], ['GOOD', 'bad', 'bad', 'bAd']]),
+      'Publish!')
+print(well([['gOOd', 'bAd', 'BAD', 'bad', 'bad', 'GOOD'], ['bad'], ['gOOd', 'BAD']]), 'I smell a series!')
