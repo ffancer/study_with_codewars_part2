024f881c169279d0f48c23015e23ad00356e6560
Paul <gafgafgarion05@gmail.com>
2022-03-22 09:31:47 +0300

    4

--- 7 kyu Thinking & Testing  Something capitalized.py
+++ 7 kyu Thinking & Testing  Something capitalized.py
@@ -3,17 +3,26 @@
 
 
 def testit(s):
+    s1 = s
     s = s.split()
+    s = [i.replace("'", '') for i in s]
 
-    if len(s[0]) == 2:
-        return ' '.join(i[0].lower() + i[1] for i in s)
+    try:
+        if len(s[0]) == 2:
+            return ' '.join(i[0].lower() + i[1].upper() for i in s)
+        if len(s[0]) == 4:
+            return ' '.join(i[:3].lower() + i[3].upper() for i in s)
+    except:
+        return s1.swapcase()
 
 
-# print(testit(""), "")
-# print(testit("a"), "A")
-# print(testit("b"), "B")
-# print(testit("a a"), "A A")
-# print(testit("a b"), "A B")
-# print(testit("a b c"), "A B C")
+
+
+print(testit(""), "")
+print(testit("a"), "A")
+print(testit("b"), "B")
+print(testit("a a"), "A A")
+print(testit("a b"), "A B")
+print(testit("a b c"), "A B C")
 print(testit("AA"), "aA")
-
+print(testit("'fukv zsor tjss'"), 'fukV zsoR tjsS')
