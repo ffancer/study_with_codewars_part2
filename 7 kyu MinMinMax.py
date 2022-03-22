d4f3a784a12c7f92d5be1f84b0e4b40e0a262a6c
Paul <gafgafgarion05@gmail.com>
2022-03-22 09:50:51 +0300

    init

---
+++ 7 kyu MinMinMax.py
@@ -0,0 +1,7 @@
+def minMinMax(arr):
+    pass
+
+
+print(minMinMax([-1, 4, 5, -23, 24]), [-23, -22, 24])
+print(minMinMax([1, 3, -3, -2, 8, -1]), [-3, 0, 8])
+print(minMinMax([2, -4, 8, -5, 9, 7]), [-5, -3, 9])
