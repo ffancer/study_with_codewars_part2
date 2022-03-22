ce80c86797eeee7f37f075ea1261c8420deee1ac
Paul <gafgafgarion05@gmail.com>
2022-03-22 09:17:33 +0300

    3

--- 7 kyu Thinking & Testing  Something capitalized.py
+++ 7 kyu Thinking & Testing  Something capitalized.py
@@ -3,20 +3,17 @@
 
 
 def testit(s):
-    ans = ''
+    s = s.split()
 
-    for i in s:
-        if i.islower():
-            ans += i.upper()
-        else:
-            ans += i
-
-    return ans
+    if len(s[0]) == 2:
+        return ' '.join(i[0].lower() + i[1] for i in s)
 
 
-print(testit(""), "")
-print(testit("a"), "A")
-print(testit("b"), "B")
-print(testit("a a"), "A A")
-print(testit("a b"), "A B")
-print(testit("a b c"), "A B C")
+# print(testit(""), "")
+# print(testit("a"), "A")
+# print(testit("b"), "B")
+# print(testit("a a"), "A A")
+# print(testit("a b"), "A B")
+# print(testit("a b c"), "A B C")
+print(testit("AA"), "aA")
+
