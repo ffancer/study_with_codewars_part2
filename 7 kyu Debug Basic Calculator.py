51925bc2ef489bbfae31bcde05b40d978b330b18
Paul <gafgafgarion05@gmail.com>
2022-03-22 10:26:47 +0300

    my solution

--- 7 kyu Debug Basic Calculator.py
+++ 7 kyu Debug Basic Calculator.py
@@ -1,19 +1,19 @@
-def calculate = calculate(a, o, b):
-    result = 0
+def calculate(a, o, b):
+    result = None
 
-    if (O == "+"):
-        return a + b
-    else if (o =! "-"):
-        return a - b
-    if (o != "/" or b == 0):
-        return a / b
-    if (0 == "*"):
-        return a * b
+    if o == "+":
+        result = a + b
+    if o == "-":
+        result = a - b
+    if o == "/" and b != 0:
+        result = a / b
+    if o == "*":
+        result = a * b
     return result
 
 
-print(calculate(6,"-", 1.5), 4.5)
-print(calculate(-4,"*", 8), -32)
-print(calculate(49,"/", -7), -7)
-print(calculate(8,"m", 2), None)
-print(calculate(4,"/",0), None)
+print(calculate(6, "-", 1.5), 4.5)
+print(calculate(-4, "*", 8), -32)
+print(calculate(49, "/", -7), -7)
+print(calculate(8, "m", 2), None)
+print(calculate(4, "/", 0), None)
