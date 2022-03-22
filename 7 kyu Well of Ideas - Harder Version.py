0b933bb4fd31153547d9d318b8eb36f079197c18
Paul <gafgafgarion05@gmail.com>
2022-03-22 13:43:20 +0300

    my solution

--- 7 kyu Well of Ideas - Harder Version.py
+++ 7 kyu Well of Ideas - Harder Version.py
@@ -1,7 +1,16 @@
 def well(arr):
-    for i,j in enumerate(arr):
+    cnt = 0
+
+    for i, j in enumerate(arr):
         for k in j:
-            print(k)
+            if str(k).lower() == 'good':
+                cnt += 1
+
+    if cnt == 1 or cnt == 2:
+        return 'Publish!'
+    if cnt > 2:
+        return 'I smell a series!'
+    return 'Fail!'
 
 
 print(well([['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad'], ['bad', 'bAd', 'bad']]), 'Fail!')
