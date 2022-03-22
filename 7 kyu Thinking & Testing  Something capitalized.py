a6f307d8084775fca63a9d4535991bc5f466ef53
Paul <gafgafgarion05@gmail.com>
2022-03-22 09:09:46 +0300

    init

---
+++ 7 kyu Thinking & Testing  Something capitalized.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# Thinking & Testing : Something capitalized
+
+
+def testit(s):
+    pass
+
+
+print(testit(""), "")
+print(testit("a"), "A")
+print(testit("b"), "B")
+print(testit("a a"), "A A")
+print(testit("a b"), "A B")
+print(testit("a b c"), "A B C")
