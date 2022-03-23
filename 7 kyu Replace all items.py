bf1441268225b6894d78f56fa25f745cdb12184c
Paul <gafgafgarion05@gmail.com>
2022-03-23 14:37:58 +0300

    2

--- 7 kyu Replace all items.py
+++ 7 kyu Replace all items.py
@@ -1,9 +1,21 @@
 def replace_all(obj, find, replace):
-    obj = str(obj)
-    obj = obj.replace(str(find), str(replace))
-    return [obj]
+    if obj is []:
+        return []
+    if type(obj[0]) == str:
+        obj = str(obj)
+        obj = obj.replace(str(find), str(replace))
+        return obj
+    if type(obj[0]) == int:
+        lst = []
+        for i in obj:
+            if i == find:
+                lst.append(replace)
+            else:
+                lst.append(i)
+        return lst
 
-# print(replace_all([], 1, 2), [])
+
+print(replace_all([], 1, 2), [])
 print(replace_all([1, 2, 2], 1, 2), [2, 2, 2])
 print(replace_all([1, 1, 2], 1, 2), [2, 2, 2])
 print(replace_all([1, 2, 1, 2, 3], 1, 2), [2, 2, 2, 2, 3])
