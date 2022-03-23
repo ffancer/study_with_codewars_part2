b8d403fa9e833618427f7510e1b20a2e6ad6691c
Paul <gafgafgarion05@gmail.com>
2022-03-23 14:06:05 +0300

    1

--- 7 kyu Cartesian neighbors.py
+++ 7 kyu Cartesian neighbors.py
@@ -1,5 +1,5 @@
 def cartesian_neighbor(x, y):
-    return ...
+    return [(x-1, y-1), (x-1, y), (x-1, y+1), (), (), (), (), ()]
 
 
 print(sorted(cartesian_neighbor(2, 2)), [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)])
