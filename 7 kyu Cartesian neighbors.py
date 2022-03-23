d193b2a7793dbd598e69073129c1145939281b02
Paul <gafgafgarion05@gmail.com>
2022-03-23 14:20:53 +0300

    my solution

--- 7 kyu Cartesian neighbors.py
+++ 7 kyu Cartesian neighbors.py
@@ -1,5 +1,6 @@
 def cartesian_neighbor(x, y):
-    return [(x-1, y-1), (x-1, y), (x-1, y+1), (), (), (), (), ()]
+    return [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1), (x, y + 1), (x + 1, y - 1), (x + 1, y),
+            (x + 1, y + 1)]
 
 
 print(sorted(cartesian_neighbor(2, 2)), [(1, 1), (1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2), (3, 3)])
