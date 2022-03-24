6fcdd7aa1bad0a6ac97fb136705265e9352ce75c
Paul <gafgafgarion05@gmail.com>
2022-03-24 09:30:31 +0300

    best practice

--- 7 kyu Working with arrays I (and why your code fails in some katas).py
+++ 7 kyu Working with arrays I (and why your code fails in some katas).py
@@ -1,5 +1,10 @@
 def without_last(lst):
-    return lst[:-1]
+    lst_copy = list(lst) # creates a shallow copy
+    lst_copy.pop()
+    return lst_copy
+
+# There are several other ways to solve this
+# like returning lst[:-1] directly or creating a new list iteratively with all the elements of lst minus the last one
 
 
 print(without_last([1, 2, 3, 4, 5]), [1, 2, 3, 4])
