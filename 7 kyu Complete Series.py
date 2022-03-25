c239b8576b335c46cd823d3de08d3057be988867
Paul <gafgafgarion05@gmail.com>
2022-03-25 09:28:50 +0300

    my solution

--- 7 kyu Complete Series.py
+++ 7 kyu Complete Series.py
@@ -1,6 +1,10 @@
 def complete_series(seq):
     second_seq = list(set(seq))
-    return second_seq
+
+    if len(seq) != len(second_seq):
+        return [0]
+    return [i for i in range(max(seq) + 1)]
+
 
 print(complete_series([0, 1]), [0, 1])
 print(complete_series([1, 4, 6]), [0, 1, 2, 3, 4, 5, 6])
