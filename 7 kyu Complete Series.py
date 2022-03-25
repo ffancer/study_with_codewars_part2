aa99a42ae4933e55f40b00d384df4c49d6d48b3f
Paul <gafgafgarion05@gmail.com>
2022-03-25 09:26:13 +0300

    init

---
+++ 7 kyu Complete Series.py
@@ -0,0 +1,10 @@
+def complete_series(seq):
+    # your code here
+    pass
+
+
+print(complete_series([0, 1]), [0, 1])
+print(complete_series([1, 4, 6]), [0, 1, 2, 3, 4, 5, 6])
+print(complete_series([3, 4, 5]), [0, 1, 2, 3, 4, 5])
+print(complete_series([2, 1]), [0, 1, 2])
+print(complete_series([1, 4, 4, 6]), [0])
