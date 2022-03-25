c1dd39489a6a875f5552f7e6db54cad5cb4f7a1f
Paul <gafgafgarion05@gmail.com>
2022-03-25 09:16:24 +0300

    init

---
+++ 7 kyu Number to digit tiers.py
@@ -0,0 +1,10 @@
+def create_array_of_tiers(n):
+    # your awesome code here
+    return []
+
+
+print(create_array_of_tiers(420), ["4", "42", "420"])
+print(create_array_of_tiers(2017), ["2", "20", "201", "2017"])
+print(create_array_of_tiers(2010), ["2", "20", "201", "2010"])
+print(create_array_of_tiers(4020), ["4", "40", "402", "4020"])
+print(create_array_of_tiers(80200), ["8", "80", "802", "8020", "80200"])
