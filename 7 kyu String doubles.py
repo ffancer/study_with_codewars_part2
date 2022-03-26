045c15f26e1f6e335bf84351eae71389f5cc640e
Paul <gafgafgarion05@gmail.com>
2022-03-26 08:07:51 +0300

    init

---
+++ 7 kyu String doubles.py
@@ -0,0 +1,10 @@
+def doubles(s):
+    pass
+
+
+print(doubles('abbbzz'), 'ab')
+print(doubles('zzzzykkkd'), 'ykd')
+print(doubles('abbcccdddda'), 'aca')
+print(doubles('vvvvvoiiiiin'), 'voin')
+print(doubles('rrrmooomqqqqj'), 'rmomj')
+print(doubles('xxbnnnnnyaaaaam'), 'bnyam')
