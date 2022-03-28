0d42b0516421a0618396c48ec2e7722fabc6d16d
Paul <gafgafgarion05@gmail.com>
2022-03-28 10:28:41 +0300

    init

---
+++ 7 kyu Do you speak retsec.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Do you speak retsec?
+
+
+def reverse_by_center(s):
+    return
+
+
+print(reverse_by_center("secret"), "retsec")
+print(reverse_by_center("agent"), "nteag")
+print(reverse_by_center("raw"), "war")
+print(reverse_by_center("onion"), "onion")
