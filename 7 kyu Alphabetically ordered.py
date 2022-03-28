04c9dfe57fe18e0933fc12979fc48939a99a1a4e
Paul <gafgafgarion05@gmail.com>
2022-03-28 14:47:32 +0300

    init

---
+++ 7 kyu Alphabetically ordered.py
@@ -0,0 +1,10 @@
+def alphabetic(s):
+    pass
+
+
+print(alphabetic('asd'), False)
+print(alphabetic('codewars'), False)
+print(alphabetic('door'), True)
+print(alphabetic('cell'), True)
+print(alphabetic('z'), True)
+print(alphabetic(''), True)
