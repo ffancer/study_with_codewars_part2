94822447fbf079df291aa3abe3908ef365d6d851
Paul <gafgafgarion05@gmail.com>
2022-03-28 15:04:34 +0300

    doesn't work coz "Your solution contains quotes!"

--- 7 kyu Hello World - Without Strings.py
+++ 7 kyu Hello World - Without Strings.py
@@ -1,8 +1,6 @@
 def hello_world():
-    s = 'Hello, World!'
-    lst = []
-    for i in s:
-        lst.append(ord(i))
-    return lst
+    lst = [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+    return ''.join(chr(i) for i in lst)
+
 
 print(hello_world(), 'Hello, World!')
