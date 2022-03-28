3235f1fa30059da383942cdceccff35a2b0b7204
Paul <gafgafgarion05@gmail.com>
2022-03-28 14:51:55 +0300

    best solutions in my opinion

--- 7 kyu Alphabetically ordered.py
+++ 7 kyu Alphabetically ordered.py
@@ -1,6 +1,9 @@
 def alphabetic(s):
-    return list(s) == sorted(list(s))
+    return sorted(s) == list(s)
 
+
+def alphabetic(s):
+    return s == ''.join(sorted(s))
 
 print(alphabetic('asd'), False)
 print(alphabetic('codewars'), False)
