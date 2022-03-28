9133b0bcd3a10d9851df07c799aec00fbb297359
Paul <gafgafgarion05@gmail.com>
2022-03-28 15:21:16 +0300

    init

---
+++ 7 kyu Multiples!.py
@@ -0,0 +1,10 @@
+def multiple(x):
+    pass
+
+
+print(multiple(30), "BangBoom")
+print(multiple(3), "Bang")
+print(multiple(98), "Miss")
+print(multiple(65), "Boom")
+print(multiple(23), "Miss")
+print(multiple(15), "BangBoom")
