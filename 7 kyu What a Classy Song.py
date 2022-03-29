d81147f9713e91e5823548495b743e089d27b6e4
Paul <gafgafgarion05@gmail.com>
2022-03-29 09:25:26 +0300

    2

--- 7 kyu What a Classy Song.py
+++ 7 kyu What a Classy Song.py
@@ -3,13 +3,23 @@
 
 
 class Song:
-    def __init__(self):
-        pass
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+        self.listeners = set()
 
-    def count(self):
-        pass
+    def how_many(self, listeners_today):
+        cnt = 0
+
+        for i in listeners_today:
+            if i.lower() not in self.listeners:
+                cnt += 1
+                self.listeners += i.lower()
+
+        return cnt
 
 
+mount_moose = Song('Mount Moose', 'The Snazzy Moose')
 print(mount_moose.how_many(['John', 'Fred', 'Bob', 'Carl', 'RyAn']), 5)
 print(mount_moose.how_many(['JoHn', 'Luke', 'AmAndA']), 2)
 print(mount_moose.how_many(['Amanda', 'CalEb', 'CarL', 'Furgus']), 2)
