836421d1c5c57193da349b8ca4c3ef8ec710b616
Paul <gafgafgarion05@gmail.com>
2022-03-29 13:29:49 +0300

    init

---
+++ 7 kyu Larger Product or Sum.py
@@ -0,0 +1,14 @@
+def sum_or_product(array, n):
+    pass
+
+
+print(sum_or_product([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4), "sum")
+print(sum_or_product([10, 41, 8, 16, 20, 36, 9, 13, 20], 3), "product")
+print(sum_or_product([10, 20, 3, 30, 5, 4], 3), "same")
+print(sum_or_product([23, 7, 12, 9, 12, 17, 3, 8, 5, 2, 23], 3), "sum")
+print(sum_or_product([13, 8, 22, 39, 12, 6, 14, 19, 4, 7, 33], 4), "product")
+print(sum_or_product([-10, 42, 5, -7, 3, 18], 4), "product")
+print(sum_or_product([-4, -1, 5, -7, -2, 6, 20, 23, 7], 4), "same")
+print(sum_or_product([-13, 2, -15, 5, -17, 3], 3), "sum")
+print(sum_or_product([-100, -70, -50, -20, 10, 40, 70, 100], 6), "product")
+print(sum_or_product([4, 8, 12, 7, 8, -2, 3, -10], 2), "same")
