5fa6f325e2ca6b249799a362d8f09159ebf37bf5
Paul <gafgafgarion05@gmail.com>
2022-03-29 09:13:10 +0300

    init

---
+++ 7 kyu What a Classy Song.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# What a "Classy" Song
+
+
+# code away
+
+
+print(mount_moose.how_many(['John', 'Fred', 'Bob', 'Carl', 'RyAn']), 5)
+print(mount_moose.how_many(['JoHn', 'Luke', 'AmAndA']), 2)
+print(mount_moose.how_many(['Amanda', 'CalEb', 'CarL', 'Furgus']), 2)
+print(mount_moose.how_many(['JOHN', 'FRED', 'BOB', 'CARL', 'RYAN', 'KATE']), 1)
