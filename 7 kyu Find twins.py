22cff351290a3ec82eb0c6e5580d46f393297882
Paul <gafgafgarion05@gmail.com>
2022-03-29 09:49:29 +0300

    my solution

--- 7 kyu Find twins.py
+++ 7 kyu Find twins.py
@@ -1,10 +1,15 @@
 def elimination(arr):
-    try:
-        return int(''.join(str(i) for i in arr if arr.count(i) == 2)[0])
-    except IndexError:
-        return None
+    dct = {}
+
+    for i in arr:
+        dct.update({arr.count(i): i})
+
+    return dct.get(2)
 
 
 print(elimination([2, 5, 34, 1, 22, 1]), 1, "twins are 1s")
 print(elimination([2, 2, 34, 1, 22]), 2, "twins are 2s")
 print(elimination([2, 5, 34, 1, 22]), None, "There are no twins in the city")
+print(elimination(
+    [84, 73, 9, 26, 31, 87, 22, 50, 20, 6, 30, 55, 40, 14, 58, 3, 44, 97, 57, 67, 60, 83, 13, 7, 19, 99, 77, 71, 68, 33,
+     36, 35, 81, 80, 83, 42, 4, 79, 47, 45, 65, 52]), 83)
