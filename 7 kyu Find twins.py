84acf4bd1c5d71a5b887f4add87c570a86fe67d4
Paul <gafgafgarion05@gmail.com>
2022-03-29 09:41:31 +0300

    init

---
+++ 7 kyu Find twins.py
@@ -0,0 +1,7 @@
+def elimination(arr):
+    pass
+
+
+print(elimination([2, 5, 34, 1, 22, 1]), 1, "twins are 1s")
+print(elimination([2, 2, 34, 1, 22]), 2, "twins are 2s")
+print(elimination([2, 5, 34, 1, 22]), None, "There are no twins in the city")
