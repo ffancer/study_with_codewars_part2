40c12174ff99f1b1ae2d53a160d5b2dfc5a35113
Paul <gafgafgarion05@gmail.com>
2022-03-29 13:02:38 +0300

    init

---
+++ 7 kyu Turkish Numbers, 0-99.py
@@ -0,0 +1,10 @@
+def get_turkish_number(num):
+    pass
+
+
+print(get_turkish_number(1), "bir")
+print(get_turkish_number(13), "on üç")
+print(get_turkish_number(27), "yirmi yedi")
+print(get_turkish_number(38), "otuz sekiz")
+print(get_turkish_number(77), "yetmiş yedi")
+print(get_turkish_number(94), "doksan dört")
