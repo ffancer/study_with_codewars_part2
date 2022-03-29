983c83759249eba31744deb2b85d0b81ef70dc5f
Paul <gafgafgarion05@gmail.com>
2022-03-29 09:15:36 +0300

    1

--- 7 kyu What a Classy Song.py
+++ 7 kyu What a Classy Song.py
@@ -2,7 +2,12 @@
 # What a "Classy" Song
 
 
-# code away
+class Song:
+    def __init__(self):
+        pass
+
+    def count(self):
+        pass
 
 
 print(mount_moose.how_many(['John', 'Fred', 'Bob', 'Carl', 'RyAn']), 5)
