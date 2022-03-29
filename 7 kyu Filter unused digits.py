291be291a18c889afe6d2ec2f746e8b2fd364439
Paul <gafgafgarion05@gmail.com>
2022-03-29 10:12:44 +0300

    init

---
+++ 7 kyu Filter unused digits.py
@@ -0,0 +1,9 @@
+def unused_digits():
+    pass
+
+
+print(unused_digits(12, 34, 56, 78), "09")
+print(unused_digits(2015, 8, 26), "3479")
+print(unused_digits(276, 575), "013489")
+print(unused_digits(643), "0125789")
+print(unused_digits(864, 896, 744), "01235")
