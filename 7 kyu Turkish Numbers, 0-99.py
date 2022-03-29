f5eb1944c8f10d84acbd7be90cb4d5d21da2e412
Paul <gafgafgarion05@gmail.com>
2022-03-29 13:04:41 +0300

    1

--- 7 kyu Turkish Numbers, 0-99.py
+++ 7 kyu Turkish Numbers, 0-99.py
@@ -1,5 +1,25 @@
 def get_turkish_number(num):
-    pass
+    dct = {
+        0: 'sıfır',
+        1: 'bir',
+        2: 'iki',
+        3: 'üç',
+        4: 'dört',
+        5: 'beş',
+        6: 'altı',
+        7: 'yedi',
+        8: 'sekiz',
+        9: 'dokuz',
+        10: 'on',
+        20: 'yirmi',
+        30: 'otuz',
+        40: 'kırk',
+        50: 'elli',
+        60: 'altmış',
+        70: 'yetmiş',
+        80: 'seksen',
+        90: 'doksan'
+    }
 
 
 print(get_turkish_number(1), "bir")
