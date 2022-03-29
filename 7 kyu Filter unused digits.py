6c4db027a751d5988960132ff2a2830bfbcfb90f
Paul <gafgafgarion05@gmail.com>
2022-03-29 10:17:21 +0300

    my solution

--- 7 kyu Filter unused digits.py
+++ 7 kyu Filter unused digits.py
@@ -1,7 +1,13 @@
 def unused_digits(*args):
     ten_digits = '0123456789'
     digits = ''.join(str(i) for i in args)
-    return digits
+    ans = ''
+
+    for i in ten_digits:
+        if i not in digits:
+            ans += i
+
+    return ans
 
 print(unused_digits(12, 34, 56, 78), "09")
 print(unused_digits(2015, 8, 26), "3479")
