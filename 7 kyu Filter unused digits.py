5b3a7bd0e412ceb4cbe0db0a51ab39db2c1582c5
Paul <gafgafgarion05@gmail.com>
2022-03-29 10:15:26 +0300

    1

--- 7 kyu Filter unused digits.py
+++ 7 kyu Filter unused digits.py
@@ -1,6 +1,7 @@
-def unused_digits():
-    pass
-
+def unused_digits(*args):
+    ten_digits = '0123456789'
+    digits = ''.join(str(i) for i in args)
+    return digits
 
 print(unused_digits(12, 34, 56, 78), "09")
 print(unused_digits(2015, 8, 26), "3479")
