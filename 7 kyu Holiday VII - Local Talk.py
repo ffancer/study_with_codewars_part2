07483ecfae197e488072adc3bdea8717eebae7b5
Paul <gafgafgarion05@gmail.com>
2022-03-30 10:19:26 +0300

    init

---
+++ 7 kyu Holiday VII - Local Talk.py
@@ -0,0 +1,11 @@
+def pak(s):
+    pass
+
+
+print(pak("Man I need a taxi up to Ubud"), "Man pak I pak need pak a pak taxi pak up pak to pak Ubud")
+print(pak("What time are we climbing up the volcano?"),
+      "What pak time pak are pak we pak climbing pak up pak the pak volcano?")
+print(pak("Take me to Semynak!"), "Take pak me pak to pak Semynak!")
+print(pak("Massage Yes Massage Yes Massage!"), "Massage pak Yes pak Massage pak Yes pak Massage!")
+print(pak("I'll take 12 bintang and a dance please"),
+      "I'll pak take pak 12 pak bintang pak and pak a pak dance pak please")
