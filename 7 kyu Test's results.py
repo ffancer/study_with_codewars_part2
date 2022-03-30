a7d318d39a74963733aae9b1f351e89c8e269c0e
Paul <gafgafgarion05@gmail.com>
2022-03-30 10:43:35 +0300

    2

--- 7 kyu Test's results.py
+++ 7 kyu Test's results.py
@@ -1,8 +1,13 @@
 def test(r):
     ans = []
     average_mark = round(sum(r) / len(r), 3)
-    return average_mark
-
+    lst1 = ['h', 'a', 'l']
+    h = r.count(10) + r.count(9)
+    a = r.count(8) + r.count(7)
+    l = r.count(6) + r.count(5) + r.count(4) + r.count(3) + r.count(2) + r.count(1)
+    lst2 = [h, a, l]
+    dct = dict(zip(lst1, lst2))
+    return dct
 print(test([10, 9, 9, 10, 9, 10, 9]), [9.429, {'h': 7, 'a': 0, 'l': 0}, 'They did well'])
 print(test([5, 6, 4, 8, 9, 8, 9, 10, 10, 10]), [7.9, {'h': 5, 'a': 2, 'l': 3}])
 print(test([5, 6, 5, 7, 4, 5, 6, 6, 5]), [5.444, {'h': 0, 'a': 1, 'l': 8}])
