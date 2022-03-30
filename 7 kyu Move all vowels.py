439dfd9b3cd1deb57e4e9a146373fd4083438117
Paul <gafgafgarion05@gmail.com>
2022-03-30 13:01:36 +0300

    init

---
+++ 7 kyu Move all vowels.py
@@ -0,0 +1,15 @@
+def move_vowels(input):
+    pass
+
+
+print(move_vowels('day'), 'dya')
+print(move_vowels('apple'), 'pplae')
+print(move_vowels('peace'), 'pceae')
+print(move_vowels('maker'), 'mkrae')
+print(move_vowels('programming'), 'prgrmmngoai')
+print(move_vowels('javascript'), 'jvscrptaai')
+print(move_vowels('python'), 'pythno')
+print(move_vowels('ruby'), 'rbyu')
+print(move_vowels('haskell'), 'hskllae')
+print(move_vowels('clojure'), 'cljroue')
+print(move_vowels('csharp'), 'cshrpa')
