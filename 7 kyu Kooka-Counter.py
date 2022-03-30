8611ad64f8b16125db034c2275119726d7b7e5b3
Paul <gafgafgarion05@gmail.com>
2022-03-30 10:10:04 +0300

    init

---
+++ 7 kyu Kooka-Counter.py
@@ -0,0 +1,9 @@
+def kooka_counter(laughing):
+    pass
+
+
+print(kooka_counter(""), 0)
+print(kooka_counter("hahahahaha"), 1)
+print(kooka_counter("hahahahahaHaHaHa"), 2)
+print(kooka_counter("HaHaHahahaHaHa"), 3)
+print(kooka_counter("hahahahahahahaHaHa"), 2)
