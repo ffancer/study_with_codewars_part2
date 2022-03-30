dbd92e698b0cd1f0db04c3e8adb2cf0f1fc94507
Paul <gafgafgarion05@gmail.com>
2022-03-30 13:56:04 +0300

    init

---
+++ 7 kyu Sentences should start with capital letters..py
@@ -0,0 +1,9 @@
+def fix(paragraph):
+    pass
+
+
+print(fix(""), "")
+print(fix("hi."), "Hi.")
+print(
+    fix("hello. my name is inigo montoya. you killed my father. prepare to die."),
+    "Hello. My name is inigo montoya. You killed my father. Prepare to die.")
