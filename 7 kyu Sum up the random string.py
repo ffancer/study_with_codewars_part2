6e659fa8de3c94e3233a7192cc5e173323b8cb15
Paul <gafgafgarion05@gmail.com>
2022-03-31 10:47:48 +0300

    my solution

--- 7 kyu Sum up the random string.py
+++ 7 kyu Sum up the random string.py
@@ -1,5 +1,18 @@
-def sum_from_string(string):
-    pass
+def sum_from_string(strng):
+    ans = 0
+    s = ''
+
+    for i in strng:
+        if i.isdigit():
+            s += i
+        elif s:
+            ans += int(s)
+            s = ''
+
+    if s:
+        ans += int(s)
+
+    return ans
 
 
 print(sum_from_string("In 2015, I want to know how much does iPhone 6+ cost?"), 2021)
