8c50d0b7d9826f509b3a43594bb4a02d0e950e34
Paul <gafgafgarion05@gmail.com>
2022-03-31 10:40:27 +0300

    init

---
+++ 7 kyu Sum up the random string.py
@@ -0,0 +1,7 @@
+def sum_from_string(string):
+    pass
+
+
+print(sum_from_string("In 2015, I want to know how much does iPhone 6+ cost?"), 2021)
+print(sum_from_string("1+1=2"), 4)
+print(sum_from_string("e=mc^2"), 2)
