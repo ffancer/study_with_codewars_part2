f61012331da91927f249ff23e5094613b5431c4e
Paul <gafgafgarion05@gmail.com>
2022-03-31 10:18:14 +0300

    init

---
+++ 7 kyu Thinkful - List and Loop Drills Inverse Slicer.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Thinkful - List and Loop Drills: Inverse Slicer
+
+
+def inverse_slice(items, a, b):
+    pass
+
+
+print(inverse_slice([12, 14, 63, 72, 55, 24], 2, 4), [12, 14, 55, 24])
+print(inverse_slice([12, 14, 63, 72, 55, 24], 0, 3), [72, 55, 24])
+print(inverse_slice(['Intuition', 'is', 'a', 'poor', 'guide', 'when', 'facing', 'probabilistic', 'evidence'], 5, 13),
+      ['Intuition', 'is', 'a', 'poor', 'guide'])
