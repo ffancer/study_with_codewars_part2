3233f30bc35580725739144250e354334b79f553
Paul <gafgafgarion05@gmail.com>
2022-03-31 09:32:23 +0300

    init

---
+++ 7 kyu Running out of space.py
@@ -0,0 +1,7 @@
+def spacey(array):
+    return []
+
+
+print(spacey(['kevin', 'has', 'no', 'space']), ['kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace'])
+print(spacey(['this', 'cheese', 'has', 'no', 'holes']),
+                   ['this', 'thischeese', 'thischeesehas', 'thischeesehasno', 'thischeesehasnoholes'])
