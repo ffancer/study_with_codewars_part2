d3f243299435eee8116a0aa466251bf5b390214a
Paul <gafgafgarion05@gmail.com>
2022-03-31 09:42:43 +0300

    1

--- 7 kyu Running out of space.py
+++ 7 kyu Running out of space.py
@@ -1,5 +1,13 @@
 def spacey(array):
-    return []
+    # i = 0
+    # first = array[i]
+    # lst = []
+    # while array:
+    #     lst.append(first)
+    #     first = array[0] + array[:i+1]
+    #     i += 1
+    # return lst
+    return array[:1+1+1]
 
 
 print(spacey(['kevin', 'has', 'no', 'space']), ['kevin', 'kevinhas', 'kevinhasno', 'kevinhasnospace'])
