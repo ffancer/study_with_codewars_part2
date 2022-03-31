55279fae827329ed5da2143aecb27418b2fff5d9
Paul <gafgafgarion05@gmail.com>
2022-03-31 10:30:59 +0300

    init

---
+++ 7 kyu Simple Fun #10 Range Bit Counting.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Simple Fun #10: Range Bit Counting
+
+
+def range_bit_count(a, b):
+    pass
+
+
+print(range_bit_count(2, 7), 11)
+print(range_bit_count(0, 1), 1)
+print(range_bit_count(4, 4), 1)
