aa217b882be2b59f1771ef93b31b3d6e0265f0a0
Paul <gafgafgarion05@gmail.com>
2022-04-01 13:00:24 +0300

    init

---
+++ 7 kyu Sum - Square Even, Root Odd.py
@@ -0,0 +1,7 @@
+def sum_square_even_root_odd(nums):
+    # your code goes here
+    pass
+
+
+print(sum_square_even_root_odd([4, 5, 7, 8, 1, 2, 3, 0]), 91.61)
+print(sum_square_even_root_odd([1, 14, 9, 8, 17, 21]), 272.71)
