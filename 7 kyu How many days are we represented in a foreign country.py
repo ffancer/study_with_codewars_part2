a4a31bd0efa697d19be95e51f082c5c7ef0aff1b
Paul <gafgafgarion05@gmail.com>
2022-04-01 13:34:38 +0300

    init

---
+++ 7 kyu How many days are we represented in a foreign country.py
@@ -0,0 +1,10 @@
+# 7 kyu
+# How many days are we represented in a foreign country?
+
+
+def days_represented(trips):
+    return 0
+
+
+print(days_represented([[10, 15], [25, 35]]), 17)
+print(days_represented([[2, 8], [220, 229], [10, 16]]), 24)
