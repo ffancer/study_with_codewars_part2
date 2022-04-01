0c559fe7761f43d66ca817a39925685d56c15237
Paul <gafgafgarion05@gmail.com>
2022-04-01 13:08:21 +0300

    my solution

--- 7 kyu Sum - Square Even, Root Odd.py
+++ 7 kyu Sum - Square Even, Root Odd.py
@@ -7,7 +7,8 @@ def sum_square_even_root_odd(nums):
         else:
             lst.append(i ** .5)
 
-    return lst
+    return round(sum(lst), 2)
+
 
 print(sum_square_even_root_odd([4, 5, 7, 8, 1, 2, 3, 0]), 91.61)
 print(sum_square_even_root_odd([1, 14, 9, 8, 17, 21]), 272.71)
