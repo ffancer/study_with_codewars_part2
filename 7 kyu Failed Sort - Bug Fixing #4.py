e2d6322ab788c1f365a2feaa7dc083a64c511465
Paul <gafgafgarion05@gmail.com>
2022-04-01 10:34:05 +0300

    init

---
+++ 7 kyu Failed Sort - Bug Fixing #4.py
@@ -0,0 +1,7 @@
+def sort_array(value):
+    return "".join(sorted(value,key=lambda a: -int(a)))
+
+
+print(sort_array('12345'), '12345', 'Return a descending sorted array')
+print(sort_array('54321'), '12345', 'Return a descending sorted array')
+print(sort_array('34251'), '12345', 'Return a descending sorted array')
