646f2ae83a1ee8d88e13b987e8d0c16ff2281cda
Paul <gafgafgarion05@gmail.com>
2022-04-01 10:10:40 +0300

    init

---
+++ 7 kyu What dominates your array.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# What dominates your array?
+
+
+def dominator(arr):
+    pass
+
+
+print(dominator([3, 4, 3, 2, 3, 1, 3, 3]), 3)
+print(dominator([1, 2, 3, 4, 5]), -1)
+print(dominator([1, 1, 1, 2, 2, 2]), -1)
+print(dominator([1, 1, 1, 2, 2, 2, 2]), 2)
+print(dominator([]), -1)
