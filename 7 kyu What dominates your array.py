9e41d9dcd0da252c2342331e1e25a152e438d107
Paul <gafgafgarion05@gmail.com>
2022-04-01 10:18:20 +0300

    2

--- 7 kyu What dominates your array.py
+++ 7 kyu What dominates your array.py
@@ -1,18 +1,14 @@
 # 7 kyu
 # What dominates your array?
 
+from collections import Counter
 
 def dominator(arr):
-    dct = {}
-
-    for i in arr:
-        dct.update({i: arr.count(i)})
-
-    return dct
+    return Counter(arr)
 
 
 print(dominator([3, 4, 3, 2, 3, 1, 3, 3]), 3)
 print(dominator([1, 2, 3, 4, 5]), -1)
 print(dominator([1, 1, 1, 2, 2, 2]), -1)
 print(dominator([1, 1, 1, 2, 2, 2, 2]), 2)
-print(dominator([]), -1)
+# print(dominator([]), -1)
