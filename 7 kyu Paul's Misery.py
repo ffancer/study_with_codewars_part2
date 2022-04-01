25632b4c6a876e305cd74ef765a757ad68a74ea6
Paul <gafgafgarion05@gmail.com>
2022-04-01 10:41:25 +0300

    init

---
+++ 7 kyu Paul's Misery.py
@@ -0,0 +1,8 @@
+def paul(x):
+    # your code here
+    return ''
+
+
+print(paul(['life', 'eating', 'life']), 'Super happy!')
+print(paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating']), 'Super happy!')
+print(paul(['Petes kata', 'Petes kata', 'eating', 'Petes kata', 'Petes kata', 'eating']), 'Happy!')
