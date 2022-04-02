07d2244ff8a1fef6b2478f526ca1c183a015c428
Paul <gafgafgarion05@gmail.com>
2022-04-02 10:26:43 +0300

    init

---
+++ 7 kyu Convert the score.py
@@ -0,0 +1,8 @@
+def scoreboard(string):
+    return
+
+
+print(scoreboard("The score is four nil"), [4, 0])
+print(scoreboard("new score: two three"), [2, 3])
+print(scoreboard("two two"), [2, 2])
+print(scoreboard("Arsenal just conceded another goal, two nil"), [2, 0])
