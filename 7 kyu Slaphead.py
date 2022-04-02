b0e9f3d60353b5f11f92baf2431875dd120880f1
Paul <gafgafgarion05@gmail.com>
2022-04-02 10:55:04 +0300

    my solution

--- 7 kyu Slaphead.py
+++ 7 kyu Slaphead.py
@@ -1,6 +1,13 @@
 def bald(s):
-    cnt_stray = s.count('/')
+    cnt = s.count('/')
     lst = [s.replace('/', '-')]
+
+    if cnt == 0: lst.append("Clean!")
+    if cnt == 1: lst.append("Unicorn!")
+    if cnt == 2: lst.append("Homer!")
+    if 5 >= cnt >= 3: lst.append("Careless!")
+    if cnt > 5: lst.append("Hobo!")
+
     return lst
 
 print(bald("/---------"), "Unicorn!", )
