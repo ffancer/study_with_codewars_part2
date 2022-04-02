b3288e95edbe768297cbe73d2c0e083a43ad2f98
Paul <gafgafgarion05@gmail.com>
2022-04-02 13:06:53 +0300

    my solution

--- 7 kyu Triangle area.py
+++ 7 kyu Triangle area.py
@@ -1,6 +1,5 @@
 def t_area(t_str):
-    # return t_str.count('\n')
-    return t_str.split('\n')[-2].count(' ')
+    return t_str.split('\n')[-2].count(' ') * (t_str.count('\n') - 2) / 2
 
 
 print(t_area('\n.\n. .\n'), .5)
