78f73ad87605c9eaf2c45a436c5ba6e7c532850d
Paul <gafgafgarion05@gmail.com>
2022-04-02 10:32:51 +0300

    1

--- 7 kyu Convert the score.py
+++ 7 kyu Convert the score.py
@@ -1,5 +1,16 @@
 def scoreboard(string):
-    return
+    dct = {
+        'nil': 0,
+        'one': 1,
+        'two': 2,
+        'three': 3,
+        'four': 4,
+        'five': 5,
+        'six': 6,
+        'seven': 7,
+        'eight': 8,
+        'nine': 9
+    }
 
 
 print(scoreboard("The score is four nil"), [4, 0])
