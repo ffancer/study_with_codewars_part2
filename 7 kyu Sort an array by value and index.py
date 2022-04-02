359f787acef9bec394ef885c6a2139c0fdc2ad1b
Paul <gafgafgarion05@gmail.com>
2022-04-02 13:29:08 +0300

    init

---
+++ 7 kyu Sort an array by value and index.py
@@ -0,0 +1,8 @@
+def sort_by_value_and_index(arr):
+    pass
+
+
+print(sort_by_value_and_index([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+print(sort_by_value_and_index([23, 2, 3, 4, 5]), [2, 3, 4, 23, 5])
+print(sort_by_value_and_index([26, 2, 3, 4, 5]), [2, 3, 4, 5, 26])
+print(sort_by_value_and_index([9, 5, 1, 4, 3]), [1, 9, 5, 3, 4])
