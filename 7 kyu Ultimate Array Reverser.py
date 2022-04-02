153af035146723d9e875f28079acbb3b1e77e4b3
Paul <gafgafgarion05@gmail.com>
2022-04-02 10:08:33 +0300

    init

---
+++ 7 kyu Ultimate Array Reverser.py
@@ -0,0 +1,12 @@
+def reverse(a):
+    return
+
+
+print(reverse(["I", "like", "big", "butts", "and", "I", "cannot", "lie!"]),
+      ["!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"])
+
+print(reverse(
+    ["?kn", "ipnr", "utotst", "ra", "tsn", "iksr", "uo", "yer", "ofebta", "eote", "vahu", "oyodpm", "ir", "hsyn",
+     "amwoH"]),
+      ["How", "many", "shrimp", "do", "you", "have", "to", "eat", "before", "your", "skin", "starts", "to", "turn",
+       "pink?"])
