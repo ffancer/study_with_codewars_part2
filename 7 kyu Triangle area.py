876a8f72b9c0e8b3b0af7b2d381419536b4d3894
Paul <gafgafgarion05@gmail.com>
2022-04-02 13:02:39 +0300

    init

---
+++ 7 kyu Triangle area.py
@@ -0,0 +1,10 @@
+def t_area(t_str):
+    pass
+
+
+print(t_area('\n.\n. .\n'), .5)
+print(t_area('\n.\n. .\n. . .\n. . . .\n. . . . .\n'), 8.0)
+print(t_area('\n.\n. .\n. . .\n'), 2.0)
+print(t_area(
+    '\n.\n. .\n. . .\n. . . .\n. . . . .\n. . . . . .\n. . . . . . .\n. . . . . . . .\n. . . . . . . . .\n'),
+    32.0)
