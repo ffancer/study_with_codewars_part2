83fe4efe051de1d742084f61114ce32be1d34bba
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:12:58 +0300

    1

--- 7 kyu Number climber.py
+++ 7 kyu Number climber.py
@@ -1,7 +1,15 @@
 def climb(n):
-    # your code here
-    pass
-
+    i = 0
+    lst = []
+    while i < n:
+        if i % 2 == 0:
+            i = i * 2 + 1
+            lst.append(i)
+        else:
+            i *= 2
+            lst.append(i)
+        # i += 1
+    return lst
 
 print(climb(13), [1, 3, 6, 13])
 print(climb(10), [1, 2, 5, 10])
