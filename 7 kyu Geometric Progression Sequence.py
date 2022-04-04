8da8fb1693a8fff807ac2cbd38b800caff100578
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:25:06 +0300

    init

---
+++ 7 kyu Geometric Progression Sequence.py
@@ -0,0 +1,7 @@
+def geometric_sequence_elements(a, r, n):
+    pass
+
+
+print(geometric_sequence_elements(2, 3, 5), '2, 6, 18, 54, 162')
+print(geometric_sequence_elements(2, 2, 10), '2, 4, 8, 16, 32, 64, 128, 256, 512, 1024')
+print(geometric_sequence_elements(1, -2, 10), '1, -2, 4, -8, 16, -32, 64, -128, 256, -512')
