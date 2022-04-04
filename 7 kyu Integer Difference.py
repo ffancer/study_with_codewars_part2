2a85463dfdbb66e57fc6a32642fe26b3bd773bbe
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:53:24 +0300

    my solution

--- 7 kyu Integer Difference.py
+++ 7 kyu Integer Difference.py
@@ -1,13 +1,13 @@
-def int_diff(lst, n):
-    cnt = 0
-    i,j=0,1
+def int_diff(arr, n):
+    res = 0
 
-    while j < len(lst)-1:
-        if abs(lst[i] - lst[j]) == n:
-            cnt += 1
-            i += 1
-        j += 1
-    return cnt
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if i != j and abs(arr[i] - arr[j]) == n:
+                res += 1
+
+    return res
+
 
 print(int_diff([1, 1, 5, 6, 9, 16, 27], 4), 3)
 print(int_diff([1, 1, 3, 3], 2), 4)
