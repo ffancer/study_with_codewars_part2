237dda7a34445ef286efceb4f7898634b31941c0
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:33:50 +0300

    init

---
+++ 7 kyu How many animals are there.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# How many animals are there?
+
+
+def count_animals(sentence):
+    # Your code here
+    return
+
+
+print(count_animals("I see 3 zebras, 5 lions and 6 giraffes.") == 14, 'Live from the Savannah')
+print(count_animals("Mom, 3 rhinoceros and 6 snakes come to us!"), 'Close the car !')
+print(count_animals("I do not see any animals here!") == 0, 'Live from the kitchen')
