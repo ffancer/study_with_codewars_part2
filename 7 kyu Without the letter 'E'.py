9de0c3b8de4513a76eee6b091bad773ab011ea40
Paul <gafgafgarion05@gmail.com>
2022-04-04 13:15:00 +0300

    init

---
+++ 7 kyu Without the letter 'E'.py
@@ -0,0 +1,9 @@
+def find_e(s):
+    pass
+
+
+print(find_e('actual'), "There is no \"e\".")
+print(find_e('English'), '1')
+print(find_e('English exercise'), '4')
+print(find_e(''), '')
+print(find_e(None), None)
