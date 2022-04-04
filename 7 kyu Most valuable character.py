db7e139527c3bd9a005121c20c6971d34b28677b
Paul <gafgafgarion05@gmail.com>
2022-04-04 13:34:15 +0300

    solution

--- 7 kyu Most valuable character.py
+++ 7 kyu Most valuable character.py
@@ -1,5 +1,6 @@
-def solve(st):
-    pass
+def solve(strng):
+    pos = [(len(strng) - strng[::-1].index(i) - strng.index(i), i) for i in set(strng)]
+    return max(pos, key=lambda item: (item[0], ord('z') - ord(item[1])))[1]
 
 
 print(solve('a'), 'a')
