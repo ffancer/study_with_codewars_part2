9b93bca09a1164ba969cadf96b1af4dee18b737b
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:36:16 +0300

    1

--- 7 kyu How many animals are there.py
+++ 7 kyu How many animals are there.py
@@ -3,10 +3,9 @@
 
 
 def count_animals(sentence):
-    # Your code here
-    return
+    return sentence.split()
 
 
-print(count_animals("I see 3 zebras, 5 lions and 6 giraffes.") == 14, 'Live from the Savannah')
+print(count_animals("I see 3 zebras, 5 lions and 6 giraffes."), 'Live from the Savannah')
 print(count_animals("Mom, 3 rhinoceros and 6 snakes come to us!"), 'Close the car !')
-print(count_animals("I do not see any animals here!") == 0, 'Live from the kitchen')
+print(count_animals("I do not see any animals here!"), 'Live from the kitchen')
