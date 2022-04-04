d96988e9bb8dbe050cdd6a23273cc71824928e4b
Paul <gafgafgarion05@gmail.com>
2022-04-04 10:52:27 +0300

    1

--- 7 kyu Integer Difference.py
+++ 7 kyu Integer Difference.py
@@ -1,6 +1,13 @@
 def int_diff(lst, n):
-    pass
+    cnt = 0
+    i,j=0,1
 
+    while j < len(lst)-1:
+        if abs(lst[i] - lst[j]) == n:
+            cnt += 1
+            i += 1
+        j += 1
+    return cnt
 
 print(int_diff([1, 1, 5, 6, 9, 16, 27], 4), 3)
 print(int_diff([1, 1, 3, 3], 2), 4)
