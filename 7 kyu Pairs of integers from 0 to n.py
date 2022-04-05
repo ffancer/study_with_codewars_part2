a09ff2ff3275df9ccb52ad490ec39fa992551b9f
Paul <gafgafgarion05@gmail.com>
2022-04-05 09:13:53 +0300

    1

--- 7 kyu Pairs of integers from 0 to n.py
+++ 7 kyu Pairs of integers from 0 to n.py
@@ -1,6 +1,9 @@
-def generate_pairs(n):
-    pass
+from random import randint
 
+
+def generate_pairs(n):
+    n = randint(0, n)
+    return n
 
 print(generate_pairs(2), [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2]])
 print(generate_pairs(0), [[0, 0]])
