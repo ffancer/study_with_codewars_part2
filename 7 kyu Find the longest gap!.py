a13afa8bf9bb8e7e99493f400d3ce3d16fe4cef5
Paul <gafgafgarion05@gmail.com>
2022-04-05 10:43:15 +0300

    3

--- 7 kyu Find the longest gap!.py
+++ 7 kyu Find the longest gap!.py
@@ -1,15 +1,16 @@
 def gap(num):
-    cnt, biggest_zero = 1, 0
-    i = 0
-    num = bin(num)[2:]
-    while i < len(num)-1:
-        if num[i] == num[i+1] == '0':
-            cnt += 1
-
-        biggest_zero = cnt
-        cnt = 0
-        i += 1
-    return biggest_zero
+    # cnt, biggest_zero = 1, 0
+    # i = 0
+    # num = bin(num)[2:]
+    # while i < len(num)-1:
+    #     if num[i] == num[i+1] == '0':
+    #         cnt += 1
+    #
+    #     biggest_zero = cnt
+    #     cnt = 0
+    #     i += 1
+    # return biggest_zero
+    return bin(num)[2:].split('1')
 
 
 print(gap(9), 2)
