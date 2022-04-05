205c89979d1484b6f0068bdbbdcba4c4745b53a0
Paul <gafgafgarion05@gmail.com>
2022-04-05 09:47:26 +0300

    init

---
+++ 7 kyu Easy Time Convert.py
@@ -0,0 +1,14 @@
+def time_convert(num):
+    pass
+
+
+print(time_convert(0), '00:00', 'Test at 0')
+print(time_convert(-6), '00:00', 'Negative number')
+print(time_convert(8), '00:08', '8 minutes')
+print(time_convert(32), '00:32', '32 minutes')
+print(time_convert(75), '01:15', 'over an hour')
+print(time_convert(63), '01:03', 'over an hour')
+print(time_convert(134), '02:14', 'over two hours')
+print(time_convert(180), '03:00', 'three hours')
+print(time_convert(970), '16:10', 'over 16 hours')
+print(time_convert(565757), '9429:17', 'big numbers')
