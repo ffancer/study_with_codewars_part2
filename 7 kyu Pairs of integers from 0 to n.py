5a0934be46e5c12939eef0c4104f4052b6d84199
Paul <gafgafgarion05@gmail.com>
2022-04-05 09:34:16 +0300

    my solution

--- 7 kyu Pairs of integers from 0 to n.py
+++ 7 kyu Pairs of integers from 0 to n.py
@@ -1,9 +1,14 @@
-from random import randint
-
-
 def generate_pairs(n):
-    n = randint(0, n)
-    return n
+    lst = list(range(n + 1))
+    res = []
+
+    for i in range(0, len(lst)):
+        for j in range(0, len(lst)):
+            if j >= i:
+                res.append([i, j])
+
+    return res
+
 
 print(generate_pairs(2), [[0, 0], [0, 1], [0, 2], [1, 1], [1, 2], [2, 2]])
 print(generate_pairs(0), [[0, 0]])
