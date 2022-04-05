669747d5e58a3d0d0f5c1a8df81b61301dae74f8
Paul <gafgafgarion05@gmail.com>
2022-04-05 10:20:52 +0300

    init

---
+++ 7 kyu String Scramble.py
@@ -0,0 +1,7 @@
+def scramble(string, array):
+    pass
+
+
+print(scramble('abcd', [0, 3, 1, 2]), 'acdb', "Should return acdb")
+print(scramble('sc301s', [4, 0, 3, 1, 5, 2]), "c0s3s1", "Should return c0s3s1")
+print(scramble('bskl5', [2, 1, 4, 3, 0]), "5sblk", "Should return 5sblk")
