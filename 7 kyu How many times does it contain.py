3e809a682df418201250a81d076796bea2d88028
Paul <gafgafgarion05@gmail.com>
2022-04-06 10:11:57 +0300

    init

---
+++ 7 kyu How many times does it contain.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# How many times does it contain?
+
+
+def string_counter(string, char):
+    pass
+
+
+print(string_counter("Hello world", "o"), 2)
+print(string_counter("Wait isn't it supposed to be cynical?", "i"), 4)
+print(string_counter("I'm gona be the best code warrior ever dad", "r"), 4)
+print(string_counter("Do you like Harry Potter?", "?"), 1)
