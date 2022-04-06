acc1f3f15ecdfd7567c3dbb6b7943706c7493158
Paul <gafgafgarion05@gmail.com>
2022-04-06 13:30:21 +0300

    init

---
+++ 7 kyu Ones' Complement.py
@@ -0,0 +1,9 @@
+def ones_complement(binary_number):
+    pass
+
+
+print(ones_complement("0"), "1")
+print(ones_complement("1"), "0")
+print(ones_complement("01"), "10")
+print(ones_complement("10"), "01")
+print(ones_complement("1101"), "0010")
