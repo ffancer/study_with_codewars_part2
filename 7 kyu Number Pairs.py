59147be209fe77a6af1a828acfb70f5c3e9e85be
Paul <gafgafgarion05@gmail.com>
2022-04-06 10:16:23 +0300

    init

---
+++ 7 kyu Number Pairs.py
@@ -0,0 +1,10 @@
+def get_larger_numbers(a, b):
+    return
+
+
+a = [13, 64, 15, 17, 88]
+b = [23, 14, 53, 17, 80]
+print(get_larger_numbers(a, b), [23, 64, 53, 17, 88], "Wrong result for a = {}, b = {}".format(a, b))
+a = [34, -64, 15, 17, 88]
+b = [23, 14, 53, 17, 80]
+print(get_larger_numbers(a, b), [34, 14, 53, 17, 88], "Wrong result for a = {}, b = {}".format(a, b))
