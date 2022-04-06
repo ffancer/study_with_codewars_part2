774f4f5e4aa1a18510118a141bffeb9457fd80c0
Paul <gafgafgarion05@gmail.com>
2022-04-06 14:06:40 +0300

    init

---
+++ 7 kyu Fruit string calculator.py
@@ -0,0 +1,9 @@
+def calculate(string):
+    pass
+
+
+print(calculate("Panda has 48 apples and loses 4"), 44)
+print(calculate("Jerry has 34 apples and gains 6"), 40)
+print(calculate("Tom has 20 apples and gains 15"), 35)
+print(calculate("Fred has 110 bananas and loses 50"), 60)
+print(calculate("Pippi has 20 tunas and gains 0"), 20)
