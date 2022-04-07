35769dde90b07ea4c06c9503371b993d493b8a4b
Paul <gafgafgarion05@gmail.com>
2022-04-07 09:57:29 +0300

    init

---
+++ 7 kyu Cesrd Strings.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Ce*s*r*d Strings
+
+
+def uncensor(infected, discovered):
+    pass
+
+
+print(uncensor('*h*s *s v*ry *tr*ng*', 'Tiiesae'), 'This is very strange')
+print(uncensor('A**Z*N*', 'MAIG'), 'AMAZING')
+print(uncensor('xyz', ''), 'xyz')
+print(uncensor('', ''), '')
+print(uncensor('***', 'abc'), 'abc')
