165865dc931156600b17191dee26bb1cb6cfa1d2
Paul <gafgafgarion05@gmail.com>
2022-04-07 09:24:34 +0300

    1

--- 7 kyu Multiply array values and filter non-numeric.py
+++ 7 kyu Multiply array values and filter non-numeric.py
@@ -1,6 +1,10 @@
 def multiply_and_filter(seq, multiplier):
-    # your code here
-    pass
+    lst = []
+
+    for i in seq:
+        lst.append(i * multiplier)
+
+    return lst
 
 
 print(multiply_and_filter([1, 2, 3, 4], 1.5), [1.5, 3, 4.5, 6])
@@ -8,3 +12,4 @@ print(multiply_and_filter([1, 2, 3], 0), [0, 0, 0])
 print(multiply_and_filter([0, 0, 0], 2), [0, 0, 0])
 print(multiply_and_filter([1, None, lambda x: x, 2.5, 'string', 10, None, {}, []], 3), [3, 7.5, 30])
 print(multiply_and_filter([1, None, lambda x: x, 2.5, 'string', 10, None, {}, [], True, False], 3), [3, 7.5, 30])
+
