b1383593cc0c7857a515cf7befc49764b09eb3d0
Paul <gafgafgarion05@gmail.com>
2022-04-07 09:12:18 +0300

    init

---
+++ 7 kyu The Barksdale Code.py
@@ -0,0 +1,9 @@
+def decode(strng):
+    pass
+
+
+print(decode("4103432323"), "6957678787")
+print(decode("4103438970"), "6957672135")
+print(decode("4104305768"), "6956750342")
+print(decode("4102204351"), "6958856709")
+print(decode("4107056043"), "6953504567")
