e8243484bfd3b0ca093914341d9d0a3d27422e68
Paul <gafgafgarion05@gmail.com>
2022-04-07 10:29:33 +0300

    my solution

--- 7 kyu Simple Fun #144 Distinct Digit Year.py
+++ 7 kyu Simple Fun #144 Distinct Digit Year.py
@@ -1,6 +1,10 @@
 def distinct_digit_year(year):
-    while True:
-        return set(str(year))
+    year += 1
+
+    while len(set(str(year))) != 4:
+        year += 1
+
+    return year
 
 
 print(distinct_digit_year(1987), 2013)
