16eaf6366c5fb50d9ed41d8fa66a9158e23bf98b
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:24:17 +0300

    1

--- 7 kyu ASCII letters from Number.py
+++ 7 kyu ASCII letters from Number.py
@@ -1,6 +1,11 @@
 def convert(number):
-    pass
+    lst = []
+    i = 0
+    while i < len(number):
+        lst.append(number[i: i+2])
+        i += 2
 
+    return lst
 
 print(convert("65"), "A")
 print(convert("656667"), "ABC")
