87f2da4401d236d4d9805c0242c0b068222fb138
Paul <gafgafgarion05@gmail.com>
2022-04-08 10:11:39 +0300

    init

---
+++ 7 kyu Thinkful - String Drills Repeater level 2.py
@@ -0,0 +1,8 @@
+def repeater(string, n):
+    pass
+
+
+print(repeater('yo', 3), '"yo" repeated 3 times is: "yoyoyo"')
+print(repeater('WuB', 6), '"WuB" repeated 6 times is: "WuBWuBWuBWuBWuBWuB"')
+print(repeater('code, eat, code, sleep... ', 2),
+      '"code, eat, code, sleep... " repeated 2 times is: "code, eat, code, sleep... code, eat, code, sleep... "')
