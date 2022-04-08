e7c17ce5c4b9058e29a430493b6dc3d7c4c7f8b4
Paul <gafgafgarion05@gmail.com>
2022-04-08 10:18:12 +0300

    init

---
+++ 7 kyu Array comparator.py
@@ -0,0 +1,10 @@
+def match_arrays(v, r):
+    pass
+
+# DON'T remove
+verbose = False # set to True to diplay arrays being tested in the random tests
+
+
+print(match_arrays(['Perl','Closure','JavaScript'],['Go', 'C++','Erlang']),0)
+print(match_arrays(['Erlang','JavaScript'],['Go', 'C++','Python']),0)
+print(match_arrays([True,3,9,11,15],[True,3,11]),3)
