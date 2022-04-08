b491678ca6238d019b1070db8dbb588d8715f892
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:44:05 +0300

    1

--- 7 kyu Composing squared strings.py
+++ 7 kyu Composing squared strings.py
@@ -1,5 +1,13 @@
 def compose(s1, s2):
-    pass
+    s1, s2 = s1.split('\n'), s2.split('\n')
+    ans = ''
+
+    ans += s1[0][0] + s2[3][0:] + ' '
+    ans += s1[1][0] + s2[2][0:] + ' '
+    ans += s1[2][0] + s2[1][0:] + ' '
+    ans += s1[3][0] + s2[0][0:] + ' '
+
+    return ans.replace(' ', '\n')[:-1]
 
 
 print(compose("byGt\nhTts\nRTFF\nCnnI", "jIRl\nViBu\nrWOb\nNkTB"),
