fea7c400d6fce5518fec4c1ab0c80cfc417b5e4f
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:47:59 +0300

    best solution

--- 7 kyu Composing squared strings.py
+++ 7 kyu Composing squared strings.py
@@ -1,13 +1,14 @@
 def compose(s1, s2):
-    s1, s2 = s1.split('\n'), s2.split('\n')
-    lst = []
+    s1 = s1.split("\n")
+    s2 = s2.split("\n")[::-1]
 
-    for n in range(len(s1)):
-        append_1 = s1[n][:n + 1]
-        append_2 = s2[-(n + 1)][:-n] if n > 0 else s2[-(n + 1)][:]
-        lst.append(append_1 + append_2)
+    n = len(s1)
+    out = []
 
-    return '\n'.join(lst)
+    for i in range(n):
+        out.append(s1[i][:i + 1] + s2[i][:(n - i)])
+
+    return "\n".join(out)
 
 
 print(compose("byGt\nhTts\nRTFF\nCnnI", "jIRl\nViBu\nrWOb\nNkTB"),
