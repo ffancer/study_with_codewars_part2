015b9a9034cec42bba8c8d52f0c5954c97b69cea
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:21:04 +0300

    init

---
+++ 7 kyu ASCII letters from Number.py
@@ -0,0 +1,8 @@
+def convert(number):
+    pass
+
+
+print(convert("65"), "A")
+print(convert("656667"), "ABC")
+print(convert("676584"), "CAT")
+print(convert("73327673756932858080698267658369"), "I LIKE UPPERCASE")
