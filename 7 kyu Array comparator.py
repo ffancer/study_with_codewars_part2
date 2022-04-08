eed7e3c75f0c8ff74d4d78395eb5f4f8b9e3a51d
Paul <gafgafgarion05@gmail.com>
2022-04-08 10:19:33 +0300

    my solution

--- 7 kyu Array comparator.py
+++ 7 kyu Array comparator.py
@@ -1,10 +1,16 @@
 def match_arrays(v, r):
-    pass
+    lst = []
+
+    for i in v:
+        if i in r:
+            lst.append(i)
+
+    return len(lst)
+
 
 # DON'T remove
-verbose = False # set to True to diplay arrays being tested in the random tests
+verbose = False  # set to True to diplay arrays being tested in the random tests
 
-
-print(match_arrays(['Perl','Closure','JavaScript'],['Go', 'C++','Erlang']),0)
-print(match_arrays(['Erlang','JavaScript'],['Go', 'C++','Python']),0)
-print(match_arrays([True,3,9,11,15],[True,3,11]),3)
+print(match_arrays(['Perl', 'Closure', 'JavaScript'], ['Go', 'C++', 'Erlang']), 0)
+print(match_arrays(['Erlang', 'JavaScript'], ['Go', 'C++', 'Python']), 0)
+print(match_arrays([True, 3, 9, 11, 15], [True, 3, 11]), 3)
