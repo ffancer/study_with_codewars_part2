046e5f0237aee28bb185da5abf9c87cabd7e8467
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:39:05 +0300

    init

---
+++ 7 kyu Composing squared strings.py
@@ -0,0 +1,8 @@
+def compose(s1, s2):
+    pass
+
+
+print(compose("byGt\nhTts\nRTFF\nCnnI", "jIRl\nViBu\nrWOb\nNkTB"),
+      "bNkTB\nhTrWO\nRTFVi\nCnnIj")
+print(compose("HXxA\nTGBf\nIPhg\nuUMD", "Hcbj\nqteH\nGbMJ\ngYPW"),
+      "HgYPW\nTGGbM\nIPhqt\nuUMDH")
