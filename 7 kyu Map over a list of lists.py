dba6b143f796135a5db4b5e93361527b934ae5b5
Paul <gafgafgarion05@gmail.com>
2022-04-08 10:23:34 +0300

    init

---
+++ 7 kyu Map over a list of lists.py
@@ -0,0 +1,8 @@
+def grid_map(inp, op):
+    pass
+
+
+num_grid = [[1, 2, 3, 4], [5, 6, 7, 8, 9], [0, 2, 4]]
+char_grid = [['h', 'E', 'l', 'l', 'O'], ['w', 'O', 'r', 'L', 'd']]
+
+print(grid_map(num_grid, lambda x: x + 1))
