f713c842ba5281e19e8260070510bff655d020b7
Paul <gafgafgarion05@gmail.com>
2022-04-08 13:08:11 +0300

    init

---
+++ 7 kyu letters only, please!.py
@@ -0,0 +1,6 @@
+def remove_chars(s):
+    pass
+
+
+print(remove_chars("test for error!"), "test for error", 'remove_chars("test for error!") did not return correct value')
+print(remove_chars(".tree1"), 'tree', 'remove_chars".tree1") did not return correct value')
