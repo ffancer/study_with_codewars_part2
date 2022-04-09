702d2e83740c509c09eb209fe89b22fd3faff12b
Paul <gafgafgarion05@gmail.com>
2022-04-09 10:27:19 +0300

    3

--- 7 kyu Rock Off!.py
+++ 7 kyu Rock Off!.py
@@ -1,12 +1,17 @@
 def solve(a, b):
-    alice_total, bob_total = 0, 0
+    alice_total, bob_total, alice_cnt, bob_cnt = 0, 0,0,0
     for i in range(len(a)):
         if a[i] == b[i]:
             continue
         else:
             alice_total += a[i]
             bob_total += b[i]
-    return alice_total, bob_total
+            if a[i] > b[i]:
+                alice_cnt += 1
+            else:
+                bob_cnt += 1
+
+    return alice_total, bob_total, alice_cnt, bob_cnt
 print(solve([47, 7, 2], [47, 7, 2]), '0, 0: that looks like a "draw"! Rock on!')
 print(solve([47, 49, 22], [26, 47, 12]), '3, 0: Alice made "Kurt" proud!')
 print(solve([25, 50, 22], [34, 49, 50]), '1, 2: Bob made "Jeff" proud!')
