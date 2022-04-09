b9e3d0a7a1e61bf924819790de504a988d090fdc
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:55:44 +0300

    init

---
+++ 7 kyu Strange mathematics.py
@@ -0,0 +1,7 @@
+def strange_math(n, k):
+    pass
+
+
+print(strange_math(11, 2), 4, 'Wrong result for (11,2)')
+print(strange_math(15, 5), 11, 'Wrong result for (15,5)')
+print(strange_math(15, 15), 7, 'Wrong result for (15,15)')
