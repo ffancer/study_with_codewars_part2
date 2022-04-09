d67e0d3cfdeadac31e82c9eaeeca999b56df2bf6
Paul <gafgafgarion05@gmail.com>
2022-04-09 13:11:31 +0300

    my solution

--- 7 kyu Simple Fun #20 First Reverse Try.py
+++ 7 kyu Simple Fun #20 First Reverse Try.py
@@ -2,7 +2,9 @@
 # Simple Fun #20: First Reverse Try
 
 def first_reverse_try(arr):
-    return arr[1:-1]
+    if not arr or len(arr) == 1:
+        return arr
+    return [arr[-1]] + arr[1:-1] + [arr[0]]
 
 
 print(first_reverse_try([1, 2, 3, 4, 5]), [5, 2, 3, 4, 1])
