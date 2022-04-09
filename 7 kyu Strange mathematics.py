f5539dd1a3f2fe7e517340fab63323546c28d5d4
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:59:39 +0300

    my 1 line solution

--- 7 kyu Strange mathematics.py
+++ 7 kyu Strange mathematics.py
@@ -1,6 +1,5 @@
 def strange_math(n, k):
-    lst = sorted(str(i) for i in range(0, n + 1))
-    return lst.index(str(k))
+    return sorted(str(i) for i in range(0, n + 1)).index(str(k))
 
 
 print(strange_math(11, 2), 4, 'Wrong result for (11,2)')
