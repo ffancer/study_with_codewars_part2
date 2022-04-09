6e8a48262314b08490e7ce21129dfaa48703e6dd
Paul <gafgafgarion05@gmail.com>
2022-04-09 10:33:25 +0300

    my solution

--- 7 kyu Rock Off!.py
+++ 7 kyu Rock Off!.py
@@ -1,20 +1,18 @@
 def solve(a, b):
-    alice_total, bob_total, alice_cnt, bob_cnt = 0, 0, 0, 0
+    alice_cnt, bob_cnt = 0, 0
 
     for i in range(len(a)):
         if a[i] == b[i]:
             continue
         else:
-            alice_total += a[i]
-            bob_total += b[i]
             if a[i] > b[i]:
                 alice_cnt += 1
             else:
                 bob_cnt += 1
 
-    return f'{alice_cnt}, {bob_cnt}: Alice made "Kurt" proud!' if alice_total > bob_total else \
-        f'{alice_cnt}, {bob_cnt}: Bob made "Jeff" proud!' if alice_total < bob_total else \
-            '0, 0: that looks like a "draw"! Rock on!'
+    return f'{alice_cnt}, {bob_cnt}: Alice made "Kurt" proud!' if alice_cnt > bob_cnt else \
+        f'{alice_cnt}, {bob_cnt}: Bob made "Jeff" proud!' if bob_cnt > alice_cnt else \
+            f'{alice_cnt}, {bob_cnt}: that looks like a "draw"! Rock on!'
 
 
 print(solve([47, 7, 2], [47, 7, 2]), '0, 0: that looks like a "draw"! Rock on!')
