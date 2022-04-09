d8cb1e2c5657bd35dcdf562659771c482d715b75
Paul <gafgafgarion05@gmail.com>
2022-04-09 13:08:42 +0300

    init

---
+++ 7 kyu Simple Fun #20 First Reverse Try.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Simple Fun #20: First Reverse Try
+
+def first_reverse_try(arr):
+    pass
+
+
+print(first_reverse_try([1, 2, 3, 4, 5]), [5, 2, 3, 4, 1])
+print(first_reverse_try([3, 5, 6, 2]), [2, 5, 6, 3])
+print(first_reverse_try([]), [])
+print(first_reverse_try([111]), [111])
