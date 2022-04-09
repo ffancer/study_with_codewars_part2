235029a9ce2a8e784e75a088ee37f856fc8a9a9a
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:09:37 +0300

    init

---
+++ 7 kyu String Reversing, Changing case, etc..py
@@ -0,0 +1,7 @@
+def reverse_and_mirror(s1, s2):
+    return ""
+
+
+print(reverse_and_mirror('FizZ', 'buZZ'), 'zzUB@@@zZIffIZz')
+print(reverse_and_mirror('String Reversing', 'Changing Case'),
+      'ESAc GNIGNAHc@@@GNISREVEr GNIRTssTRING rEVERSING')
