8e78c97e23438ea9705f482c5ae7e0a2095bcf50
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:12:26 +0300

    2

--- 7 kyu String Reversing, Changing case, etc..py
+++ 7 kyu String Reversing, Changing case, etc..py
@@ -1,6 +1,7 @@
 def reverse_and_mirror(s1, s2):
     right_word = s1[::-1].swapcase() + s1.swapcase()
-    return right_word
+    left_word = s2.swapcase()[::-1] + '@@@'
+    return left_word
 
 
 print(reverse_and_mirror('FizZ', 'buZZ'), 'zzUB@@@zZIffIZz')
