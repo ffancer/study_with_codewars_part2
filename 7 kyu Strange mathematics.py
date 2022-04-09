7e83126e337cb50566abe93353d3955e321b7615
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:57:17 +0300

    1

--- 7 kyu Strange mathematics.py
+++ 7 kyu Strange mathematics.py
@@ -1,6 +1,6 @@
 def strange_math(n, k):
-    pass
-
+    lst = [str(i) for i in range(0, n + 1)]
+    return lst
 
 print(strange_math(11, 2), 4, 'Wrong result for (11,2)')
 print(strange_math(15, 5), 11, 'Wrong result for (15,5)')
