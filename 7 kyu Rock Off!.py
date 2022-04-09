6ba14f532b43b906ddced2f781eaef87907e0bc1
Paul <gafgafgarion05@gmail.com>
2022-04-09 10:16:23 +0300

    init

---
+++ 7 kyu Rock Off!.py
@@ -0,0 +1,7 @@
+def solve(a, b):
+    pass
+
+
+print(solve([47, 7, 2], [47, 7, 2]), '0, 0: that looks like a "draw"! Rock on!')
+print(solve([47, 49, 22], [26, 47, 12]), '3, 0: Alice made "Kurt" proud!')
+print(solve([25, 50, 22], [34, 49, 50]), '1, 2: Bob made "Jeff" proud!')
