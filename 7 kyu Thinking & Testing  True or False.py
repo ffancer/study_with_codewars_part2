f185fb0cc17c710dd82eb802db5b08fab2f56d1a
Paul <gafgafgarion05@gmail.com>
2022-04-09 09:21:43 +0300

    init

---
+++ 7 kyu Thinking & Testing  True or False.py
@@ -0,0 +1,22 @@
+def testit(n):
+    pass
+
+
+# n ?
+print(testit(0), 0)
+print(testit(0), 0)
+# n - 1 ?
+print(testit(2), 1)
+print(testit(3), 2)
+# round(n / 3.0)
+print(testit(4), 1)
+print(testit(5), 2)
+print(testit(6), 2)
+# None of the above
+print(testit(7), 3)
+print(testit(8), 1)
+print(testit(9), 2)
+print(testit(10), 2)
+print(testit(100), 3)
+print(testit(1000), 6)
+print(testit(10000), 5)
