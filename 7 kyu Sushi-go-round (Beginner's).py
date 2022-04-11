53463aaf82fc68fbe00f6e829145a3378a2f494d
Paul <gafgafgarion05@gmail.com>
2022-04-11 09:53:32 +0300

    init

---
+++ 7 kyu Sushi-go-round (Beginner's).py
@@ -0,0 +1,9 @@
+def total_bill(s):
+    pass
+
+
+print(total_bill('rr'), 4)
+print(total_bill('rr rrr'), 8)
+print(total_bill('rr rrr rrr rr'), 16)
+print(total_bill('rrrrrrrrrrrrrrrrrr   rr r'), 34)
+print(total_bill(''), 0)
