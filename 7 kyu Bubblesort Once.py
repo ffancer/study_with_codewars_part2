f5696c7139132db9f1b70e50dcd8cc49f46ffed6
Paul <gafgafgarion05@gmail.com>
2022-04-11 10:13:01 +0300

    my solution

--- 7 kyu Bubblesort Once.py
+++ 7 kyu Bubblesort Once.py
@@ -1,5 +1,12 @@
 def bubblesort_once(l):
-    pass
+    lst = l[:]
+
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[j - 1] > lst[j]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+        return lst
+    return []
 
 
 print(bubblesort_once([9, 7, 5, 3, 1, 2, 4, 6, 8]), [7, 5, 3, 1, 2, 4, 6, 8, 9])
