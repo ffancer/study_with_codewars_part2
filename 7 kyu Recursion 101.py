7e5fe99aa402a54600fce2b6aa032efc80cfcff9
Paul <gafgafgarion05@gmail.com>
2022-04-11 09:38:02 +0300

    1

--- 7 kyu Recursion 101.py
+++ 7 kyu Recursion 101.py
@@ -1,8 +1,10 @@
 def solve(a, b):
-    pass
+    if a == 0 or b == 0:
+        return [a, b]
 
 
 print(solve(6, 19), [6, 7])
 print(solve(2, 1), [0, 1])
 print(solve(22, 5), [0, 1])
 print(solve(2, 10), [2, 2])
+print(solve(0, 10), [0, 10])
