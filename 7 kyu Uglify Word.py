eaaefe26c765f960090cc3ec03d5e50f625b9a0e
Paul <gafgafgarion05@gmail.com>
2022-04-12 09:52:01 +0300

    1

--- 7 kyu Uglify Word.py
+++ 7 kyu Uglify Word.py
@@ -1,6 +1,15 @@
 def uglify_word(s):
-    return
-
+    s_lower, s_upper = s.lower(), s.upper()
+    lst = []
+    if s == s.lower() and '-' in s:
+        s = s.split('-')
+        for i in s:
+            for j in range(len(i)):
+                if j % 2 == 0:
+                    lst.append(i[j].upper())
+                else:
+                    lst.append(i[j].lower())
+    return lst
 
 print(uglify_word("AAA"), "AaA")
 print(uglify_word("AaA"), "AaA")
