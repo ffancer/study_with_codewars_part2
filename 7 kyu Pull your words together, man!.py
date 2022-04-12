0c5d67a78887d5174f2eb4220a77d93113666089
Paul <gafgafgarion05@gmail.com>
2022-04-12 10:18:23 +0300

    init

---
+++ 7 kyu Pull your words together, man!.py
@@ -0,0 +1,8 @@
+def sentencify(words):
+    pass
+
+
+print(sentencify(["i", "am", "an", "AI"]), "I am an AI.")
+print(sentencify(["yes"]), "Yes.")
+print(sentencify(["FIELDS", "of", "CORN", "are", "to", "be", "sown"]), "FIELDS of CORN are to be sown.")
+print(sentencify(["i'm", "afraid", "I", "can't", "let", "you", "do", "that"]), "I'm afraid I can't let you do that.")
