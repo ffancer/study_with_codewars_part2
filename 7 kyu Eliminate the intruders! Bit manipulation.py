c4d2bf411f26b428e2abff6482fd2b952a6a5983
Paul <gafgafgarion05@gmail.com>
2022-04-12 09:09:47 +0300

    init

---
+++ 7 kyu Eliminate the intruders! Bit manipulation.py
@@ -0,0 +1,9 @@
+def eliminate_unset_bits(number):
+    pass
+
+
+print(eliminate_unset_bits("11010101010101"), 255)
+print(eliminate_unset_bits("111"), 7)
+print(eliminate_unset_bits("1000000"), 1)
+print(eliminate_unset_bits("000"), 0)
+
