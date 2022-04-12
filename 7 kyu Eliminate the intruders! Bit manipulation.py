f30e9f64064a661d7f8c5ce6a5192dc174d780f1
Paul <gafgafgarion05@gmail.com>
2022-04-12 09:10:51 +0300

    1

--- 7 kyu Eliminate the intruders! Bit manipulation.py
+++ 7 kyu Eliminate the intruders! Bit manipulation.py
@@ -1,9 +1,10 @@
 def eliminate_unset_bits(number):
-    pass
+    if number.count('1') < 1:
+        return 0
+    return number.replace('0', '')
 
 
 print(eliminate_unset_bits("11010101010101"), 255)
 print(eliminate_unset_bits("111"), 7)
 print(eliminate_unset_bits("1000000"), 1)
 print(eliminate_unset_bits("000"), 0)
-
