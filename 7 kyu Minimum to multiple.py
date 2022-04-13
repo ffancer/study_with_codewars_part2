7528647eb98a797b39d3585efa180ffaca13df2b
Paul <gafgafgarion05@gmail.com>
2022-04-13 10:24:29 +0300

    init

---
+++ 7 kyu Minimum to multiple.py
@@ -0,0 +1,16 @@
+def minimum(a, x):
+    pass
+
+
+print(minimum(1, 1), 0)
+print(minimum(9, 4), 1)
+print(minimum(10, 6), 2)
+print(minimum(60, 45), 15)
+print(minimum(57, 50), 7)
+print(minimum(28, 16), 4)
+print(minimum(84, 80), 4)
+print(minimum(129, 49), 18)
+print(minimum(150, 67), 16)
+print(minimum(121, 46), 17)
+print(minimum(83, 81), 2)
+print(minimum(89, 74), 15)
