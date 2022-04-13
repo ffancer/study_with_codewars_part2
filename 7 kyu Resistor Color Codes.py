e6c8fcdecaea329910af45c012e7bd99fe05bdae
Paul <gafgafgarion05@gmail.com>
2022-04-13 10:15:19 +0300

    init

---
+++ 7 kyu Resistor Color Codes.py
@@ -0,0 +1,7 @@
+def decode_resistor_colors(bands):
+    return ""
+
+
+print(decode_resistor_colors("yellow violet black"), "47 ohms, 20%")
+print(decode_resistor_colors("yellow violet red gold"), "4.7k ohms, 5%")
+print(decode_resistor_colors("brown black green silver"), "1M ohms, 10%")
