60b0fa361bd4aa321af4519a2785412405c43553
Paul <gafgafgarion05@gmail.com>
2022-04-14 10:36:39 +0300

    init

---
+++ 7 kyu Figurate Numbers #2 - Pronic Number.py
@@ -0,0 +1,13 @@
+def is_pronic(n):
+    pass
+
+
+print(is_pronic(0), True, '0 is a Pronic Number')
+print(is_pronic(1), False, '1 is not a Pronic Number')
+print(is_pronic(2), True, '2 is a Pronic Number')
+print(is_pronic(3), False, '3 is not a Pronic Number')
+print(is_pronic(4), False, '4 is not a Pronic Number')
+print(is_pronic(5), False, '5 is not a Pronic Number')
+print(is_pronic(6), True, '6 is a Pronic Number')
+print(is_pronic(-3), False, '-3 is not a Pronic Number')
+print(is_pronic(-27), False, '-27 is not a Pronic Number')
