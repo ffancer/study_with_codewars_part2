58079198b34ecf05d850dc1bf23b17fdda9c83c9
Paul <gafgafgarion05@gmail.com>
2022-04-14 10:41:52 +0300

    init

---
+++ 7 kyu Vowel Changer.py
@@ -0,0 +1,7 @@
+def vowel_change(txt, vow):
+    pass
+
+
+print(vowel_change("hannah hannah bo-bannah banana fanna fo-fannah fee, fy, mo-mannah. hannah!", 'i'),
+      'hinnih hinnih bi-binnih binini finni fi-finnih fii, fy, mi-minnih. hinnih!')
+print(vowel_change('adira wants to go to the park', 'o'), 'odoro wonts to go to tho pork')
