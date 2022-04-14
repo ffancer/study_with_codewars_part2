e30c7be9c7453b5a5e63d905d13e23766500bf13
Paul <gafgafgarion05@gmail.com>
2022-04-14 10:03:29 +0300

    init

---
+++ 7 kyu Ironman Triathlon.py
@@ -0,0 +1,9 @@
+def i_tri(s):
+    pass
+
+
+print(i_tri(36), {'Bike': '104.60 to go!'})
+print(i_tri(103), {'Bike': '37.60 to go!'})
+print(i_tri(0), 'Starting Line... Good Luck!')
+print(i_tri(2), {'Swim': '138.60 to go!'})
+print(i_tri(151), "You're done! Stop running!")
