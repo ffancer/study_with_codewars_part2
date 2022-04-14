437bef39bc9bb7cb897ec300387e1a519d7fe39f
Paul <gafgafgarion05@gmail.com>
2022-04-14 09:42:40 +0300

    init

---
+++ 7 kyu Fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(n: int) -> int:
+    """Given a positive argument n, returns the nth term of the Fibonacci Sequence.
+    """
+    return 0
+
+
+print(fibonacci(0), 0)
+print(fibonacci(1), 1)
+print(fibonacci(2), 1)
+print(fibonacci(3), 2)
+print(fibonacci(4), 3)
+print(fibonacci(5), 5)
+print(fibonacci(6), 8)
+print(fibonacci(34), 5702887)
+print(fibonacci(299), 137347080577163115432025771710279131845700275212767467264610201)
