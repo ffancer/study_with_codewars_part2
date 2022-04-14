4477af5c05a199aea34177e0235f9b25617e58ba
Paul <gafgafgarion05@gmail.com>
2022-04-14 10:35:21 +0300

    my solution

--- 7 kyu Not above the one!.py
+++ 7 kyu Not above the one!.py
@@ -1,5 +1,13 @@
 def binary_cleaner(seq):
-    pass
+    lst1, lst2 = [], []
+
+    for i in range(len(seq)):
+        if seq[i] <= 1:
+            lst1.append(seq[i])
+        else:
+            lst2.append(i)
+
+    return lst1, lst2
 
 
 print(binary_cleaner([0, 1, 2, 1, 0, 2, 1, 1, 1, 0, 4, 5, 6, 2, 1, 1, 0]),
