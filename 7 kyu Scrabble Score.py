ea096ff29c4b955117bee1ed8ff53f731c56c13a
Paul <gafgafgarion05@gmail.com>
2022-04-15 13:34:49 +0300

    my 1 line solution

--- 7 kyu Scrabble Score.py
+++ 7 kyu Scrabble Score.py
@@ -3,13 +3,8 @@ def scrabble_score(st):
     dict_scores = {'E': 1, 'A': 1, 'I': 1, 'O': 1, 'N': 1, 'R': 1, 'T': 1, 'L': 1, 'S': 1, 'U': 1, 'D': 2, 'G': 2,
                    'B': 3, 'C': 3,
                    'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10}
-    total = 0
 
-    for i in st.upper():
-        if i in dict_scores.keys():
-            total += dict_scores[i]
-
-    return total
+    return sum(dict_scores[i] for i in st.upper() if i in dict_scores.keys())
 
 
 print(scrabble_score(""), 0, "returns 0 for ''")
