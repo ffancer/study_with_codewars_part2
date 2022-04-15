16ea54228335e1c9e812ac6e13385d7edb68090a
Paul <gafgafgarion05@gmail.com>
2022-04-15 13:09:14 +0300

    init

---
+++ 7 kyu Linked Lists - Append.py
@@ -0,0 +1,8 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def append(listA, listB):
+    # Your code goes here.
+    # Remember to return the head of the list.
