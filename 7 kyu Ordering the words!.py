cecb2929eae737266a246b7d97f319cb5a05fd0b
Paul <gafgafgarion05@gmail.com>
2022-04-15 13:23:06 +0300

    1

--- 7 kyu Ordering the words!.py
+++ 7 kyu Ordering the words!.py
@@ -1,7 +1,7 @@
 def order_word(s):
-    return ""
+    return ''.join(sorted(s))
 
 
 print(order_word("Hello, World!"), " !,HWdellloor")
 print(order_word("bobby"), "bbboy")
-print(order_word(""), "Invalid String!");
+print(order_word(""), "Invalid String!")
