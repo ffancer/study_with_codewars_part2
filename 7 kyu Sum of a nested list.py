23b049a1b2b74f2d64ccd1ff7be3a41370eb6a18
Paul <gafgafgarion05@gmail.com>
2022-04-16 09:16:00 +0300

    my solution

--- 7 kyu Sum of a nested list.py
+++ 7 kyu Sum of a nested list.py
@@ -1,8 +1,15 @@
 def sum_nested(lst):
-    flat_list = [item for sublist in lst for item in sublist]
-    return flat_list
+    total = 0
 
+    for i in lst:
+        if not i:
+            continue
+        if type(i) == list:
+            total += sum_nested(i)
+        else:
+            total += i  # coz type(i) == int
 
+    return total
 
 
 print(sum_nested([1]), 1)
