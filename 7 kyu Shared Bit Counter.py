05afe337959796b2a5c5d765a346a782b6fb4333
Paul <gafgafgarion05@gmail.com>
2022-04-16 09:19:30 +0300

    init

---
+++ 7 kyu Shared Bit Counter.py
@@ -0,0 +1,12 @@
+def shared_bits(a, b):
+    return
+
+
+print(shared_bits(1, 2), False)
+print(shared_bits(16, 8), False)
+print(shared_bits(1, 1), False)
+print(shared_bits(2, 3), False)
+print(shared_bits(7, 10), False)
+print(shared_bits(43, 77), True)
+print(shared_bits(7, 15), True)
+print(shared_bits(23, 7), True)
