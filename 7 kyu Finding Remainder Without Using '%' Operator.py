afade20566444a0387ad9aba0eca2f38396e3bf2
Paul <gafgafgarion05@gmail.com>
2022-04-16 08:27:45 +0300

    init

---
+++ 7 kyu Finding Remainder Without Using '%' Operator.py
@@ -0,0 +1,9 @@
+def remainder(dividend, divisor):
+    pass
+
+
+print(remainder(3, 2), 1)
+print(remainder(19, 2), 1)
+print(remainder(10, 2), 0)
+print(remainder(34, 7), 6)
+print(remainder(27, 5), 2)
