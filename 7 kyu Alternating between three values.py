9ad83bc95bfd28fc83fd7beb5c8135ca1c6bf6c2
Paul <gafgafgarion05@gmail.com>
2022-04-16 08:55:45 +0300

    my solution

--- 7 kyu Alternating between three values.py
+++ 7 kyu Alternating between three values.py
@@ -1,6 +1,11 @@
 def f(x, a, b, c):
-    a, b, c = b, c, a
-    return a, b, c
+    if x == a:
+        return b
+    elif x == b:
+        return c
+    elif x == c:
+        return a
+
 
 print(f(3, a=3, b=4, c=5), 4)
 print(f(5, a=3, b=4, c=5), 3)
