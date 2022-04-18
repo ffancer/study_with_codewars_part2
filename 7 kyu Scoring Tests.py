4cd3b3344665c9902c905bafb522b3e0d5cf1e40
Paul <gafgafgarion05@gmail.com>
2022-04-18 10:31:26 +0300

    best solution

--- 7 kyu Scoring Tests.py
+++ 7 kyu Scoring Tests.py
@@ -1,15 +1,6 @@
 def score_test(tests, right, omit, wrong):
-    total = 0
-
-    for i in tests:
-        if i == 0:
-            total += right
-        elif i == 1:
-            total += omit
-        elif i == 2:
-            total -= wrong
-
-    return total
+    points = (right, omit, -wrong)
+    return sum(points[test] for test in tests)
 
 
 print(score_test([0, 0, 0, 0, 2, 1, 0], 2, 0, 1), 9)
