9971d10b99694d737366f2da3319bb859bbf1521
Paul <gafgafgarion05@gmail.com>
2022-04-18 13:08:31 +0300

    init

---
+++ 7 kyu Pizza Payments.py
@@ -0,0 +1,12 @@
+def michael_pays(costs):
+    pass
+
+
+print(michael_pays(15), 10)
+print(michael_pays(4), 4)
+print(michael_pays(30), 20)
+print(michael_pays(80), 70)
+print(michael_pays(22), 14.67)
+print(michael_pays(5.9181), 3.95)
+print(michael_pays(28.789), 19.19)
+print(michael_pays(4.325), 4.33)
