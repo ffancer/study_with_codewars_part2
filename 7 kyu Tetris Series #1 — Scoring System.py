890a837e89991dcd5d9e786f63f80d5ea7e70cf2
Paul <gafgafgarion05@gmail.com>
2022-04-19 09:52:02 +0300

    init

---
+++ 7 kyu Tetris Series #1 — Scoring System.py
@@ -0,0 +1,9 @@
+def get_score(arr) -> int:
+    return 0
+
+
+print(get_score([0, 1, 2, 3, 4]), 1640)
+print(get_score([0, 1, 1, 3, 0, 2, 1, 2]), 620)
+print(get_score([2, 0, 4, 2, 2, 3, 0, 0, 3, 3]), 3300)
+print(get_score([0]), 0)
+print(get_score([]), 0)
