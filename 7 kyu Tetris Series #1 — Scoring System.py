cd54d144d620b57af7efa084328173d342d879ca
Paul <gafgafgarion05@gmail.com>
2022-04-19 10:00:40 +0300

    solution 1

--- 7 kyu Tetris Series #1 — Scoring System.py
+++ 7 kyu Tetris Series #1 — Scoring System.py
@@ -1,5 +1,25 @@
-def get_score(arr) -> int:
+def score_per_lines_cleared(lines_cleared, level):
+    level += 1
+    if lines_cleared == 1:
+        return 40 * level
+    elif lines_cleared == 2:
+        return 100 * level
+    elif lines_cleared == 3:
+        return 300 * level
+    elif lines_cleared == 4:
+        return 1200 * level
     return 0
+
+
+def get_score(arr):
+    ret = 0
+    level = 0
+    total_lines = 0
+    for lines_cleared in arr:
+        ret += score_per_lines_cleared(lines_cleared, level)
+        total_lines += lines_cleared
+        level = total_lines // 10
+    return ret
 
 
 print(get_score([0, 1, 2, 3, 4]), 1640)
