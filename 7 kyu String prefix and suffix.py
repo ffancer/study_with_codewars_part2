09a699f1591e219c82055f6c337d06d412e044ad
Paul <gafgafgarion05@gmail.com>
2022-04-20 14:30:41 +0300

    init

---
+++ 7 kyu String prefix and suffix.py
@@ -0,0 +1,14 @@
+def solve(st):
+    pass
+
+
+print(solve("abcd"), 0)
+print(solve("abcda"), 1)
+print(solve("abcdabc"), 3)
+print(solve("abcabc"), 3)
+print(solve("abcabca"), 1)
+print(solve("aaaaa"), 2)
+print(solve("aaaa"), 2)
+print(solve("aaa"), 1)
+print(solve("aa"), 1)
+print(solve("a"), 0)
