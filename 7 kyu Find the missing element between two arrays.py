f64c386c2d1e0f10c556aa775094ca6aec312821
Paul <gafgafgarion05@gmail.com>
2022-04-21 13:53:46 +0300

    init

---
+++ 7 kyu Find the missing element between two arrays.py
@@ -0,0 +1,9 @@
+def find_missing(arr1, arr2):
+    pass
+
+
+print(find_missing([1, 2, 3], [1, 3]), 2)
+print(find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]), 8)
+print(find_missing([7], []), 7)
+print(find_missing([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4]), 3)
+print(find_missing([0, 0, 0, 0, 0], [0, 0, 0, 0]), 0)
