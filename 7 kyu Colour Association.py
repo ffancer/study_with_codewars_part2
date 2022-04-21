509e5826f15f52dee2be83a79b3012484aae0673
Paul <gafgafgarion05@gmail.com>
2022-04-21 18:23:07 +0300

    init

---
+++ 7 kyu Colour Association.py
@@ -0,0 +1,13 @@
+def colour_association(arr):
+    pass
+
+
+print(colour_association([["white", "goodness"], ["blue", "tranquility"]]),
+      [{'white': "goodness"}, {'blue': "tranquility"}])
+print(colour_association([["red", "energy"], ["yellow", "creativity"], ["brown", "friendly"], ["green", "growth"]]),
+      [{'red': "energy"}, {'yellow': "creativity"}, {'brown': "friendly"}, {'green': "growth"}])
+print(colour_association([["pink", "compassion"], ["purple", "ambition"]]),
+      [{'pink': "compassion"}, {'purple': "ambition"}])
+print(colour_association([["gray", "intelligence"], ["black", "classy"]]),
+      [{'gray': "intelligence"}, {'black': "classy"}])
+print(colour_association([["white", "goodness"], ["blue", "goodness"]]), [{'white': 'goodness'}, {'blue': 'goodness'}])
