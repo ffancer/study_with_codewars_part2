72020a4c74eeea687f77034742c465ce2bbab470
Paul <gafgafgarion05@gmail.com>
2022-04-22 11:14:29 +0300

    my solution

--- 7 kyu Keypad horror.py
+++ 7 kyu Keypad horror.py
@@ -1,7 +1,14 @@
 def computer_to_phone(numbers):
+    s = ''
     dct = {'1': '7', '2': '8', '3': '9', '7': '1', '8': '2', '9': '3'}
 
+    for i in numbers:
+        if i in dct.keys():
+            s += dct[i]
+        else:
+            s += i
 
+    return s
 
 
 print(computer_to_phone("0789456123"), "0123456789")
