f0cf7c2cc2faf6fc6020cec919f2710e2d9aa6b1
Paul <gafgafgarion05@gmail.com>
2022-04-22 14:43:19 +0300

    1

--- 7 kyu The Skiponacci Sequence.py
+++ 7 kyu The Skiponacci Sequence.py
@@ -1,5 +1,14 @@
 def skiponacci(n):
-    return
+    fib1, fib2 = 1, 1
+    s = ''
+    i = 0
+    while i < n - 2:
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        i = i + 1
+        s += str(fib2) + ' '
+    return s
 
 
 print(skiponacci(1), "1")
