c8f060c30928ebca0b80ad4028c23cdef8d11149
Paul <gafgafgarion05@gmail.com>
2022-04-22 11:08:32 +0300

    init

---
+++ 7 kyu Keypad horror.py
@@ -0,0 +1,7 @@
+def computer_to_phone(numbers):
+    pass
+
+
+print(computer_to_phone("0789456123"), "0123456789")
+print(computer_to_phone("000"), "000")
+print(computer_to_phone("94561"), "34567")
