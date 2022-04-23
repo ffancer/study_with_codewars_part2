4b7026385b6391e420e33a054b5b9476532e5e3a
Paul <gafgafgarion05@gmail.com>
2022-04-23 17:02:48 +0300

    my solution

--- 7 kyu Power of 4.py
+++ 7 kyu Power of 4.py
@@ -2,7 +2,7 @@ from math import log
 
 
 def powerof4(n):
-    return log(n) / log(4)
+    return log(n) / log(4) % 1 == 0 if type(n) is int and n > 0 else False
 
 
 print(powerof4(4), True, "Wrong result for 4")
