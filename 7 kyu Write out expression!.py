349ff9784db7f94a63852dd1a6bc9b8ee5322c8e
Paul <gafgafgarion05@gmail.com>
2022-04-24 11:48:26 +0300

    init

---
+++ 7 kyu Write out expression!.py
@@ -0,0 +1,12 @@
+def expression_out(exp):
+    # Your amazing code below!
+    return exp + exp
+
+
+print(expression_out('1 + 3'), 'One Plus Three')
+print(expression_out('2 - 10'), 'Two Minus Ten')
+print(expression_out('6 ** 9'), 'Six To The Power Of Nine')
+print(expression_out('5 = 5'), 'Five Equals Five')
+print(expression_out('7 * 4'), 'Seven Times Four')
+print(expression_out('2 / 2'), 'Two Divided By Two')
+print(expression_out('8 != 5'), 'Eight Does Not Equal Five')
