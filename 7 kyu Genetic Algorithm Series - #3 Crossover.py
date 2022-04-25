d0099d440d16b8506fea370ce5fb9c693c4d9956
Paul <gafgafgarion05@gmail.com>
2022-04-25 10:57:13 +0300

    my solution

--- 7 kyu Genetic Algorithm Series - #3 Crossover.py
+++ 7 kyu Genetic Algorithm Series - #3 Crossover.py
@@ -1,7 +1,5 @@
 def crossover(chromosome1, chromosome2, index):
-    if len(chromosome1) % 2 == 0:
-
-        return [chromosome1[:index]+chromosome2[index:], chromosome1[index:]+chromosome2[:index]]
+    return [chromosome1[:index]+chromosome2[index:], chromosome2[:index]+chromosome1[index:]]
 
 
 print(crossover("110", "001", 2), ["111", "000"])
