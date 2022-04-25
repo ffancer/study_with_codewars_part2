82a8f62c87a3fcf125392e62b22f62058538088e
Paul <gafgafgarion05@gmail.com>
2022-04-25 11:57:12 +0300

    my solution

--- 7 kyu Concatenated Sum.py
+++ 7 kyu Concatenated Sum.py
@@ -1,11 +1,13 @@
 def check_concatenated_sum(num, n):
     lst = []
+    try:
+        for i in str(abs(num)):
+            lst.append(i * n)
+        lst = list(map(int, lst))
 
-    for i in str(abs(num)):
-        lst.append(i * n)
-    lst = list(map(int, lst))
-
-    return sum(lst) == abs(num)
+        return sum(lst) == abs(num)
+    except:
+        return False
 
 
 print(check_concatenated_sum(2997, 3), True)
