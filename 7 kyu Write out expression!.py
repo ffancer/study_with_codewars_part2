0b1feaa8cc5c2dd1e9fc1a43bf7f1581de0a5f1e
Paul <gafgafgarion05@gmail.com>
2022-04-25 10:33:30 +0300

    my solution

--- 7 kyu Write out expression!.py
+++ 7 kyu Write out expression!.py
@@ -1,16 +1,21 @@
 def expression_out(exp):
     exp = exp.split()
     lst = []
-    dct = {'0': 'Zero', '1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven', '8': 'Eight',
-           '9': 'Nine', '10': 'Ten', "+": "Plus", "-": "Minus", "*": "Times", "/": "Divided By", "**": "To The Power Of",
-           "=": "Equals", "!=": "Does Not Equal"}
+    dct_num = {'0': 'Zero', '1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven',
+               '8': 'Eight',
+               '9': 'Nine', '10': 'Ten'}
+    dct_operator = {"+": "Plus", "-": "Minus", "*": "Times", "/": "Divided By", "**": "To The Power Of",
+                    "=": "Equals", "!=": "Does Not Equal"}
 
-    if exp[1] not in dct.keys():
+    if exp[1] not in dct_operator.keys():
         return "That's not an operator!"
 
     for i in exp:
-        if i in dct.keys():
-            lst.append(dct[i])
+        if i in dct_num.keys():
+            lst.append(dct_num[i])
+        if i in dct_operator:
+            lst.append(dct_operator[i])
+
     return ' '.join(lst)
 
 
