dad1d229f1939108e97020b120181f41c2ba5611
Paul <gafgafgarion05@gmail.com>
2022-04-25 11:34:41 +0300

    init

---
+++ 7 kyu String Reordering.py
@@ -0,0 +1,10 @@
+def sentence(List):
+    pass
+
+
+List = [
+    {'4': 'dog'}, {'2': 'took'}, {'3': 'his'},
+    {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+]
+
+print(sentence(List), 'Vatsan took his dog for a spin')
