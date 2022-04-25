5d46ec4a34261180c72e17dd533583911dc3edd6
Paul <gafgafgarion05@gmail.com>
2022-04-25 11:52:19 +0300

    1 all ok with positive num

--- 7 kyu Concatenated Sum.py
+++ 7 kyu Concatenated Sum.py
@@ -1,6 +1,9 @@
-def check_concatenated_sum(num1, num2):
-    pass
+def check_concatenated_sum(num, n):
+    lst = []
+    for i in str(num):
+        lst.append(int(i * n))
+    return sum(lst) == num
 
 
 print(check_concatenated_sum(2997, 3), True)
-print(check_concatenated_sum(-2997, 3), True)
+# print(check_concatenated_sum(-2997, 3), True)
