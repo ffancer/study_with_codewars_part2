998badec94426bda75c8ebfdc8d50bc92c09d3a9
Paul <gafgafgarion05@gmail.com>
2022-04-25 11:56:12 +0300

    2

--- 7 kyu Concatenated Sum.py
+++ 7 kyu Concatenated Sum.py
@@ -1,9 +1,12 @@
 def check_concatenated_sum(num, n):
     lst = []
-    for i in str(num):
-        lst.append(int(i * n))
-    return sum(lst) == num
+
+    for i in str(abs(num)):
+        lst.append(i * n)
+    lst = list(map(int, lst))
+
+    return sum(lst) == abs(num)
 
 
 print(check_concatenated_sum(2997, 3), True)
-# print(check_concatenated_sum(-2997, 3), True)
+print(check_concatenated_sum(-2997, 3), True)
