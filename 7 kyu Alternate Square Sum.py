10cbd599fdbfed62700a266652d72434c264615f
Paul <gafgafgarion05@gmail.com>
2022-04-26 11:12:57 +0300

    init

---
+++ 7 kyu Alternate Square Sum.py
@@ -0,0 +1,8 @@
+def alternate_sq_sum(arr):
+    return
+
+
+print(alternate_sq_sum([]), 0)
+print(alternate_sq_sum([-1, 0, -3, 0, -5, 3]), 0)
+print(alternate_sq_sum([-1, 2, -3, 4, -5]), 11)
+print(alternate_sq_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 245)
