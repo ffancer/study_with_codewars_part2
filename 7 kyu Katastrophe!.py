a11ff4e922bee8f32322d186719e92461ee09e8f
Paul <gafgafgarion05@gmail.com>
2022-04-26 10:49:42 +0300

    init

---
+++ 7 kyu Katastrophe!.py
@@ -0,0 +1,8 @@
+def strong_enough(earthquake, age):
+    # ...
+    return "?"
+
+
+print(strong_enough([[2, 3, 1], [3, 1, 1], [1, 1, 2]], 2), "Safe!")
+print(strong_enough([[5, 8, 7], [3, 3, 1], [4, 1, 2]], 2), "Safe!")
+print(strong_enough([[5, 8, 7], [3, 3, 1], [4, 1, 2]], 3), "Needs Reinforcement!")
