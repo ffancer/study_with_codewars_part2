370527bd8501e003d667790bdc393f4c4d56b4cf
Paul <gafgafgarion05@gmail.com>
2022-04-26 10:15:49 +0300

    solution

--- 7 kyu Simple elevator.py
+++ 7 kyu Simple elevator.py
@@ -1,7 +1,14 @@
+levels = [0, 1, 2, 3]
+buttons = ['0', '1', '2', '3']
+
+
 def goto(level, button):
-    pass
+    if level not in levels or button not in buttons:
+        return 0
+    else:
+        return int(button) - level
 
 
 print(goto(0, '2'), 2)
-# print(3+goto(3,'1'),1)
-# print(2+goto(2,'2'),2)
+print(3 + goto(3, '1'), 1)
+print(2 + goto(2, '2'), 2)
