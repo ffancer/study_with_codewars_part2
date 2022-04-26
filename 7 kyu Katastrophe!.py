bc2653e62fa06d6bf4c5781dcd26814244a6db63
Paul <gafgafgarion05@gmail.com>
2022-04-26 10:53:16 +0300

    2

--- 7 kyu Katastrophe!.py
+++ 7 kyu Katastrophe!.py
@@ -1,8 +1,11 @@
 def strong_enough(earthquake, age):
     total = 1
+
     for i in earthquake:
         total *= sum(i)
-    print(total)
+
+    return "Safe!" if total <= 1000 else "Needs Reinforcement!"
+
 
 print(strong_enough([[2, 3, 1], [3, 1, 1], [1, 1, 2]], 2), "Safe!")
 print(strong_enough([[5, 8, 7], [3, 3, 1], [4, 1, 2]], 2), "Safe!")
