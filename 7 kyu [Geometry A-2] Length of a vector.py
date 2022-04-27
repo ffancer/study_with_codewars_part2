4c4a03e8844d8f132585ee2a848e60e147cf2b09
Paul <gafgafgarion05@gmail.com>
2022-04-27 15:01:40 +0300

    my solution

--- 7 kyu [Geometry A-2] Length of a vector.py
+++ 7 kyu [Geometry A-2] Length of a vector.py
@@ -5,9 +5,9 @@ from math import hypot
 
 
 def vector_length(vector):
-    # x1, x2, y1, y2 = vector[0], vector[2], vector[1], vector[3]
-    # return hypot(x1)
-    return vector[1]
+    x1, x2, y1, y2 = vector[0][0], vector[1][0], vector[0][1], vector[1][1]
+    return hypot((x2-x1),(y2-y1))
+
 
 print(vector_length([[0, 1], [0, 0]]), 1)
 print(vector_length([[0, 3], [4, 0]]), 5)
