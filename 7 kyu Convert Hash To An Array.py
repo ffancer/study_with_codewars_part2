40599918c0f004a50948473cb875072545383a6d
Paul <gafgafgarion05@gmail.com>
2022-04-27 10:26:46 +0300

    init

---
+++ 7 kyu Convert Hash To An Array.py
@@ -0,0 +1,11 @@
+def convert_hash_to_array(hash):
+    pass
+
+
+print(convert_hash_to_array({"name": "Jeremy"}), [["name", "Jeremy"]])
+print(convert_hash_to_array({"name": "Jeremy", "age": 24}), [["age", 24], ["name", "Jeremy"]])
+print(convert_hash_to_array({"name": "Jeremy", "age": 24, "role": "Software Engineer"}),
+      [["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]])
+print(convert_hash_to_array({"product": "CodeWars", "power_level_over": 9000}),
+      [["power_level_over", 9000], ["product", "CodeWars"]])
+print(convert_hash_to_array({}), [])
