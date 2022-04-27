49c11c865854ecd3cf191dfbd32401be32ade7e7
Paul <gafgafgarion05@gmail.com>
2022-04-27 10:39:55 +0300

    init

---
+++ 7 kyu Email Address Obfuscator.py
@@ -0,0 +1,6 @@
+def obfuscate(email):
+    pass
+
+
+print(obfuscate('test@123.com'), 'test [at] 123 [dot] com')
+print(obfuscate('Code_warrior@foo.ac.uk'), 'Code_warrior [at] foo [dot] ac [dot] uk')
