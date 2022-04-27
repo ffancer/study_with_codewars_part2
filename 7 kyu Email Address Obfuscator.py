e17c030ea2ec32c9ac1ebd10432f97290ee57410
Paul <gafgafgarion05@gmail.com>
2022-04-27 10:43:04 +0300

    my solution

--- 7 kyu Email Address Obfuscator.py
+++ 7 kyu Email Address Obfuscator.py
@@ -1,5 +1,16 @@
 def obfuscate(email):
-    pass
+    lst = []
+
+    for i in email:
+        if i == '@':
+            lst.append(' [at] ')
+        elif i == '.':
+            lst.append(' [dot] ')
+        else:
+            lst.append(i)
+
+    return ''.join(lst)
+
 
 
 print(obfuscate('test@123.com'), 'test [at] 123 [dot] com')
