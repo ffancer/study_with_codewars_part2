4370d174546c0baab375faa470927cb0446a7b55
Paul <gafgafgarion05@gmail.com>
2022-04-27 14:29:58 +0300

    init

---
+++ 7 kyu [Geometry A-2] Length of a vector.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# [Geometry A-2]: Length of a vector
+
+
+def vector_length(vector):
+    return -1
+
+
+print(vector_length([[0, 1], [0, 0]]), 1)
+print(vector_length([[0, 3], [4, 0]]), 5)
+print(vector_length([[1, -1], [1, -1]]), 0)
