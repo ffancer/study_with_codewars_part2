9b617e75d2304c7549684567f8eb3c4141e98be8
Paul <gafgafgarion05@gmail.com>
2022-04-28 09:44:44 +0300

    init

---
+++ 7 kyu Finding length of the sequence.py
@@ -0,0 +1,10 @@
+def length_of_sequence(arr, n):
+    pass
+
+
+print(length_of_sequence([1, 1], 1), 2, 'Returns two when there are only two elements in the array')
+print(length_of_sequence([1, 2, 3, 1], 1), 4,
+      'Returns four for an array of length four and the number to be searched at the boundaries')
+print(length_of_sequence([-7, 5, 0, 2, 9, 5], 10), 0, 'Returns zero if element is missing from the array')
+print(length_of_sequence([-7, 5, 0, 2, 9, 5], 5), 5, 'Returns five')
+print(length_of_sequence([-7, 6, 2, -7, 4], -7), 4, 'Returns four')
