f511b35fab8b1c9666d0cbc65f58bee3a1cac552
Paul <gafgafgarion05@gmail.com>
2022-04-28 10:18:34 +0300

    init

---
+++ 7 kyu Find an employees role in the company.py
@@ -0,0 +1,7 @@
+def find_employees_role(name):
+    pass
+
+
+print(find_employees_role("Dipper Pines"), "Does not work here!")
+print(find_employees_role("Morty Smith"), "Truck Driver")
+print(find_employees_role("Anna Bell"), "Admin")
