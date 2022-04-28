d284fe78e2d79ca01ebfd411c1f21cc3501cadf9
Paul <gafgafgarion05@gmail.com>
2022-04-28 10:22:09 +0300

    my solution

--- 7 kyu Find an employees role in the company.py
+++ 7 kyu Find an employees role in the company.py
@@ -1,5 +1,16 @@
 def find_employees_role(name):
-    pass
+    for employee in employees:
+        first_name, last_name, role = None, None, None
+        for i in employee.keys():
+            if i == 'first_name':
+                first_name = employee[i]
+            elif i == 'last_name':
+                last_name = employee[i]
+            elif i == 'role':
+                role = employee[i]
+        if name == " ".join([first_name, last_name]):
+            return role
+    return "Does not work here!"
 
 
 print(find_employees_role("Dipper Pines"), "Does not work here!")
