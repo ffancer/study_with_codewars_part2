c03312a4844c2e98c9b24e79a90117973c376912
Paul <gafgafgarion05@gmail.com>
2022-04-28 09:22:04 +0300

    my solution

--- 7 kyu SillyCASE.py
+++ 7 kyu SillyCASE.py
@@ -1,11 +1,9 @@
 from math import ceil
 
+
 def sillycase(silly):
     mid = ceil(len(silly) / 2)
-    # if len(silly) % 2 != 0:
-    #     return silly[:(len(silly)) // 2].lower() + silly[(len(silly) - 1)// 2:].upper()
-    # return silly[:len(silly) // 2].lower() + silly[len(silly) // 2:].upper()
-    return silly[:mid], silly[mid:]
+    return silly[:mid].lower() + silly[mid:].upper()
 
 
 print(sillycase('foobar'), "fooBAR")
