d00413a1c25c79ce3aef05009ba6d29ec221de17
Paul <gafgafgarion05@gmail.com>
2022-04-28 09:20:33 +0300

    3

--- 7 kyu SillyCASE.py
+++ 7 kyu SillyCASE.py
@@ -1,7 +1,11 @@
+from math import ceil
+
 def sillycase(silly):
-    if len(silly) % 2 != 0:
-        return silly[:(len(silly) // 2)].lower() + silly[len(silly) // 2:].upper()
-    return silly[:len(silly) // 2].lower() + silly[len(silly) // 2:].upper()
+    mid = ceil(len(silly) / 2)
+    # if len(silly) % 2 != 0:
+    #     return silly[:(len(silly)) // 2].lower() + silly[(len(silly) - 1)// 2:].upper()
+    # return silly[:len(silly) // 2].lower() + silly[len(silly) // 2:].upper()
+    return silly[:mid], silly[mid:]
 
 
 print(sillycase('foobar'), "fooBAR")
