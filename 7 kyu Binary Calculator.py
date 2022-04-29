388b18a5efd43fc2b58c2592e78fbf6d7ff69a77
Paul <gafgafgarion05@gmail.com>
2022-04-29 13:44:21 +0300

    4 problems with -

--- 7 kyu Binary Calculator.py
+++ 7 kyu Binary Calculator.py
@@ -1,13 +1,13 @@
 def calculate(n1, n2, o):
-    # return {
-    #     'add': bin(int(n1) + int(n2))[2:],
-    #     'subtract': bin(int(n1) - int(n2))[2:],
-    #     'multiply': bin(int(n1) * int(n2))[2:]
-    # }[o]
-    return bin(int(n1, 2) * int(n2, 2))[2:]
+    return {
+        'add': bin(int(n1, 2) + int(n2, 2))[2:],
+        'subtract': bin(int(n1, 2) - int(n2, 2))[2:],
+        'multiply': bin(int(n1, 2) * int(n2, 2))[2:]
+    }[o]
 
 
-# print(calculate('1', '1', 'add'), '10')
-# print(calculate('1', '1', 'subtract'), '0')
-# print(calculate('1', '1', 'multiply'), '1')
+
+print(calculate('1', '1', 'add'), '10')
+print(calculate('1', '1', 'subtract'), '0')
+print(calculate('1', '1', 'multiply'), '1')
 print(calculate('10101101', '1011011101', 'multiply'), '11110111101011001')
