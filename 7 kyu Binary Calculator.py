e69eb64cd5b6d636aec2223486426d66e1ae9a0c
Paul <gafgafgarion05@gmail.com>
2022-04-29 13:49:37 +0300

    my solution with lambda

--- 7 kyu Binary Calculator.py
+++ 7 kyu Binary Calculator.py
@@ -1,10 +1,6 @@
 def calculate(n1, n2, o):
-    return {
-        'add': bin(int(n1, 2) + int(n2, 2))[2:],
-        'subtract': bin(int(n1, 2) - int(n2, 2))[2:],
-        'multiply': bin(int(n1, 2) * int(n2, 2))[2:]
-    }[o]
-
+    ops = {'add': lambda x, y: x + y, 'subtract': lambda x, y: x - y, 'multiply': lambda x, y: x * y}
+    return "{0:b}".format(ops[o](int(n1, 2), int(n2, 2)))
 
 
 print(calculate('1', '1', 'add'), '10')
