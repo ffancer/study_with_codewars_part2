f430917b097d945f811e17bc5b9ab3b5ab79eece
Paul <gafgafgarion05@gmail.com>
2022-04-29 10:19:21 +0300

    best solution and this is work with random tests

--- 7 kyu Recursive Replication.py
+++ 7 kyu Recursive Replication.py
@@ -1,5 +1,8 @@
+@countcalls
 def replicate(times, number):
-    return [] if times <=0 else [number] * times
+    if times > 0:
+        return [number] + replicate(times - 1, number)
+    return []
 
 
 print(replicate(3, 5), [5, 5, 5])
