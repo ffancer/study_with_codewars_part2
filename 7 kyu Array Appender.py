408cd36e71f7ce50d7a8753ac42cc157db175663
Paul <gafgafgarion05@gmail.com>
2022-04-29 10:11:15 +0300

    init

---
+++ 7 kyu Array Appender.py
@@ -0,0 +1,10 @@
+def append_arrays(seq1, seq2):
+    # your code here
+    pass
+
+
+print(append_arrays([1, 2], [2, 4]), [1, 2, 2, 4])
+print(append_arrays([1, 2], [3, 4]), [1, 2, 3, 4])
+print(append_arrays(['this'], ['that']), ['this', 'that'])
+print(append_arrays(['a', 'B'], ['c', 'D']), ['a', 'B', 'c', 'D'])
+print(append_arrays([1, 2], [1]), [1, 2, 1])
