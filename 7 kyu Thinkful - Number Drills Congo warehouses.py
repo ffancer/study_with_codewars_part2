c7945361326be692ea7ce7316a2d13c97d9f7640
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:56:00 +0300

    init

---
+++ 7 kyu Thinkful - Number Drills Congo warehouses.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - Number Drills: Congo warehouses
+
+
+def box_capacity(length, width, height):
+    pass
+
+
+print(box_capacity(32, 64, 16), 13824)
+print(box_capacity(20, 20, 20), 3375)
+print(box_capacity(80, 40, 20), 27000)
