e9448aa3efb2bc86d7879cb3947ce345b78d0aa1
Paul <gafgafgarion05@gmail.com>
2022-04-30 10:37:07 +0300

    1

--- 7 kyu EAN Validation.py
+++ 7 kyu EAN Validation.py
@@ -1,7 +1,15 @@
 def validate_ean(code):
-    pass
+    total = 0
+    lst = []
+    for i, j in enumerate(code):
+        if i % 2 == 0:
+            total += int(j) * 3
+        else:
+            total += i
+    return total
 
 
+print(validate_ean("400330101839"), True)
 print(validate_ean("9783815820865"), True)
 print(validate_ean("9783815820864"), False)
 print(validate_ean("9783827317100"), True)
