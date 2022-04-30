84b0e13aab7719300848ce2696e50f4782e2cd78
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:29:02 +0300

    solution

--- 7 kyu Target Date.py
+++ 7 kyu Target Date.py
@@ -1,5 +1,15 @@
+from datetime import datetime
+from datetime import timedelta
+
 def date_nb_days(a0, a, p):
-    pass
+    day = 0
+    start = '2016/01/01'
+    while a0 < a:
+        a0 = a0 + (a0*p/100/360)
+        day += 1
+    date = datetime.strptime(start, '%Y/%m/%d')
+    date += timedelta(days=day)
+    return datetime.strftime(date, '%Y-%m-%d')
 
 
 print(date_nb_days(4281, 5087, 2), "2024-07-03")
