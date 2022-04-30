28d13f8d0b5e39aa827cedca98ddb6bbb1ba41d2
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:53:35 +0300

    my solution

--- 7 kyu Batman Quotes.py
+++ 7 kyu Batman Quotes.py
@@ -2,7 +2,7 @@ class BatmanQuotes(object):
 
     @staticmethod
     def get_quote(quotes, hero):
-        return "Who" + ": " + "said what?"
+        return f"{('Batman', 'Joker', 'Robin')['BJR'.index(hero[0])]}: {quotes[int(min(hero))]}"
 
 
 quotes = ["WHERE IS SHE?!", "Holy haberdashery, Batman!", "Let's put a smile on that faaaceee!"]
