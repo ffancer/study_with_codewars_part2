9b298aa6d1669583d44250a36b0f00b117d98377
Paul <gafgafgarion05@gmail.com>
2022-04-30 10:01:11 +0300

    1

--- 7 kyu Thinkful - Number Drills Congo warehouses.py
+++ 7 kyu Thinkful - Number Drills Congo warehouses.py
@@ -3,7 +3,10 @@
 
 
 def box_capacity(length, width, height):
-    pass
+    boxes_len = 1.3 * length
+    boxes_wid = 1.3 * width
+    boxes_hgt = 1.3 * height
+    return boxes_len, boxes_wid, boxes_hgt
 
 
 print(box_capacity(32, 64, 16), 13824)
