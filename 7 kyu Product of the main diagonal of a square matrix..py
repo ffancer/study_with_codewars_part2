666c8b4684d3f74aaa72432cfedf3ff7a7a19c4d
Paul <gafgafgarion05@gmail.com>
2022-04-30 16:45:21 +0300

    init

---
+++ 7 kyu Product of the main diagonal of a square matrix..py
@@ -0,0 +1,12 @@
+def main_diagonal_product(mat):
+    pass
+
+
+res1 = main_diagonal_product([[1, 0], [0, 1]])
+res2 = main_diagonal_product([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+print("main_diagonal_product([1,0],[0,1]) => 1")
+print(res1, 1)
+
+print("main_diagonal_product([1,2,3],[4,5,6],[7,8,9]) => 45")
+print(res2, 45)
