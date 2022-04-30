f8f34c00200645e91d1da9c36b1a82b1a2ef14a2
Paul <gafgafgarion05@gmail.com>
2022-04-30 16:29:31 +0300

    1

--- 7 kyu Regex Failure - Bug Fixing #2.py
+++ 7 kyu Regex Failure - Bug Fixing #2.py
@@ -1,7 +1,10 @@
 from re import sub
+
+
 def filter_words(phrase):
-    return sub("(bad|mean|ugly|horrible|hideous)","awesome",phrase)
+    phrase = sub("(bad|mean|ugly|horrible|hideous)", "awesome", phrase.lower())
+    return phrase[0].upper() + phrase[1:]
 
 
-print(filter_words("You're Bad! timmy!"),"You're awesome! timmy!")
-print(filter_words("You're MEAN! timmy!"),"You're awesome! timmy!")
+print(filter_words("You're Bad! timmy!"), "You're awesome! timmy!")
+print(filter_words("You're MEAN! timmy!"), "You're awesome! timmy!")
