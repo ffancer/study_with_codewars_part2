cc3c997f0eb0888309eceb766145de285e92c263
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:35:32 +0300

    my solution

--- 7 kyu Complete The Pattern #6 - Odd Ladder.py
+++ 7 kyu Complete The Pattern #6 - Odd Ladder.py
@@ -1,14 +1,14 @@
 def pattern(n):
     s = ''
 
-    for i in range(1, n, 2):
+    for i in range(1, n+1, 2):
         s += str(i) * i + '\n'
 
-    return s
+    return s[:-1]
 
 print(pattern(4), "1\n333")
-# print(pattern(1), "1")
-# print(pattern(5), "1\n333\n55555")
-# print(pattern(5), "1\n333\n55555")
-# print(pattern(0), "")
-# print(pattern(-25), "")
+print(pattern(1), "1")
+print(pattern(5), "1\n333\n55555")
+print(pattern(5), "1\n333\n55555")
+print(pattern(0), "")
+print(pattern(-25), "")
