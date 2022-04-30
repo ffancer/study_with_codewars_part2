531d33cf0a0a80a21d361eadf28c41d217c3e4b9
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:30:59 +0300

    init

---
+++ 7 kyu Complete The Pattern #6 - Odd Ladder.py
@@ -0,0 +1,10 @@
+def pattern(n):
+    pass
+
+
+print(pattern(4), "1\n333")
+print(pattern(1), "1")
+print(pattern(5), "1\n333\n55555")
+print(pattern(5), "1\n333\n55555")
+print(pattern(0), "")
+print(pattern(-25), "")
