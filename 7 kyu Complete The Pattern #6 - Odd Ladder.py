bbf8ceef20d943724510ce0faa54fcbfd6413fc9
Paul <gafgafgarion05@gmail.com>
2022-04-30 09:35:11 +0300

    1

--- 7 kyu Complete The Pattern #6 - Odd Ladder.py
+++ 7 kyu Complete The Pattern #6 - Odd Ladder.py
@@ -1,10 +1,14 @@
 def pattern(n):
-    pass
+    s = ''
 
+    for i in range(1, n, 2):
+        s += str(i) * i + '\n'
+
+    return s
 
 print(pattern(4), "1\n333")
-print(pattern(1), "1")
-print(pattern(5), "1\n333\n55555")
-print(pattern(5), "1\n333\n55555")
-print(pattern(0), "")
-print(pattern(-25), "")
+# print(pattern(1), "1")
+# print(pattern(5), "1\n333\n55555")
+# print(pattern(5), "1\n333\n55555")
+# print(pattern(0), "")
+# print(pattern(-25), "")
