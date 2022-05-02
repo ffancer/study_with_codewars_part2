dfe8976d49d4cb6c33479d7a9d7401dc59026cbd
Paul <gafgafgarion05@gmail.com>
2022-05-02 10:49:29 +0300

    1

--- 7 kyu Simple Maths Test.py
+++ 7 kyu Simple Maths Test.py
@@ -1,6 +1,18 @@
 def number_property(n):
-    pass
+    lst = []
 
+    def is_prime(n):
+        if n > 1:
+            for i in range(2, int(n / 2) + 1):
+                if (n % i) == 0:
+                    lst.append(False)
+                    break
+            else:
+                lst.append(True)
+        else:
+            print(lst.append(False))
+    is_prime(n)
+    return lst
 
 print(number_property(-10), [False, True, True])
 print(number_property(2), [True, True, False])
