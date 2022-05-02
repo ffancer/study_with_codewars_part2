f50a77e0c27fae75c29084dd618046a1eec17371
Paul <gafgafgarion05@gmail.com>
2022-05-02 13:31:46 +0300

    init

---
+++ 7 kyu CompoundArray.py
@@ -0,0 +1,7 @@
+def compound_array(a, b):
+    # You code goes here...
+    return []
+
+
+print(compound_array([1, 2, 3, 4, 5, 6], [9, 8, 7, 6]), [1, 9, 2, 8, 3, 7, 4, 6, 5, 6])
+print(compound_array([0, 1, 2], [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), [0, 9, 1, 8, 2, 7, 6, 5, 4, 3, 2, 1, 0])
