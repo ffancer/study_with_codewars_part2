dd569a61cff120895091381cfcd60b132d28b192
Paul <gafgafgarion05@gmail.com>
2022-05-02 10:43:56 +0300

    init

---
+++ 7 kyu Simple Maths Test.py
@@ -0,0 +1,8 @@
+def number_property(n):
+    pass
+
+
+print(number_property(-10), [False, True, True])
+print(number_property(2), [True, True, False])
+print(number_property(120), [False, True, True])
+print(number_property(125), [False, False, False])
