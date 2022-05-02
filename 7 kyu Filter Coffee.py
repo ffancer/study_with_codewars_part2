fa1a222e8dab3233a5392e6fee7e19210a89dcb9
Paul <gafgafgarion05@gmail.com>
2022-05-02 13:23:58 +0300

    init

---
+++ 7 kyu Filter Coffee.py
@@ -0,0 +1,9 @@
+def search(budget, prices):
+    pass
+
+
+print(search(3, [6, 1, 2, 9, 2]), "1,2,2")
+print(search(14, [7, 3, 23, 9, 14, 20, 7]), "3,7,7,9,14")
+print(search(0, [6, 1, 2, 9, 2]), "")
+print(search(10, []), "")
+print(search(24, [24, 0, 100, 2, 5]), "0,2,5,24")
