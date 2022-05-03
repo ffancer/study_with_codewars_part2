7f72202ddb134a50ee61bf9267843edf3f85c729
Paul <gafgafgarion05@gmail.com>
2022-05-03 14:57:57 +0300

    init

---
+++ 7 kyu Hamming Distance - Part 1 Binary codes.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Hamming Distance - Part 1: Binary codes
+
+
+def hamming_distance(a, b):
+    pass
+
+
+print(hamming_distance('100101', '101001'), 2)
+print(hamming_distance('1010', '0101'), 4)
+print(hamming_distance('100101011011010010010', '101100010110010110101'), 9)
