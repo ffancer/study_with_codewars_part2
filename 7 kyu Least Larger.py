87698afeff35c5de20f51a95c9116ac6d015266a
Paul <gafgafgarion05@gmail.com>
2022-05-03 10:34:19 +0300

    my solution

--- 7 kyu Least Larger.py
+++ 7 kyu Least Larger.py
@@ -1,6 +1,13 @@
 def least_larger(a, i):
-    # your code here
-    pass
+    if max(a) == a[i]:
+        return -1
+
+    num = a[i]
+    lst = sorted(a)
+    for i, j in enumerate(lst):
+        if j == num:
+            if lst[i + 1] > num:
+                return a.index(lst[i + 1])
 
 
 print(least_larger([4, 1, 3, 5, 6], 0), 3)
