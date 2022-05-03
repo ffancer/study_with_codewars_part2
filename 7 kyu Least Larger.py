35c86d37d469531532945c40ad91c156148e5965
Paul <gafgafgarion05@gmail.com>
2022-05-03 10:26:59 +0300

    init

---
+++ 7 kyu Least Larger.py
@@ -0,0 +1,8 @@
+def least_larger(a, i):
+    # your code here
+    pass
+
+
+print(least_larger([4, 1, 3, 5, 6], 0), 3)
+print(least_larger([4, 1, 3, 5, 6], 4), -1)
+print(least_larger([1, 3, 5, 2, 4], 0), 3)
