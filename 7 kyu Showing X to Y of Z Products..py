9de2ff1726c8eb76d0ea9d5fcaefe5f16d992bba
Paul <gafgafgarion05@gmail.com>
2022-05-03 14:21:16 +0300

    init

---
+++ 7 kyu Showing X to Y of Z Products..py
@@ -0,0 +1,11 @@
+def pagination_text(page_number, page_size, total_products):
+    pass
+
+
+print(pagination_text(1, 10, 30), "Showing 1 to 10 of 30 Products.")
+print(pagination_text(3, 10, 26), "Showing 21 to 26 of 26 Products.")
+print(pagination_text(1, 10, 8), "Showing 1 to 8 of 8 Products.")
+print(pagination_text(2, 30, 350), "Showing 31 to 60 of 350 Products.")
+print(pagination_text(1, 23, 30), "Showing 1 to 23 of 30 Products.")
+print(pagination_text(2, 23, 30), "Showing 24 to 30 of 30 Products.")
+print(pagination_text(43, 15, 3456), "Showing 631 to 645 of 3456 Products.")
