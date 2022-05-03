027c8fa157a65c7618608efde9faf306e2dbb3f5
Paul <gafgafgarion05@gmail.com>
2022-05-03 11:09:51 +0300

    my solution

--- 7 kyu HTML Generator.py
+++ 7 kyu HTML Generator.py
@@ -2,6 +2,27 @@ class HTMLGen:
     def a(self, text):
         return f'<a>{text}</a>'
 
+    def b(self, text):
+        return f'<b>{text}</b>'
+
+    def p(self, text):
+        return f'<p>{text}</p>'
+
+    def body(self, text):
+        return f'<body>{text}</body>'
+
+    def div(self, text):
+        return f'<div>{text}</div>'
+
+    def span(self, text):
+        return f'<span>{text}</span>'
+
+    def title(self, text):
+        return f'<title>{text}</title>'
+
+    def comment(self, text):
+        return f'<!--{text}-->'
+
 
 htmlGen = HTMLGen()
 print(htmlGen.a('test'), '<a>test</a>')
