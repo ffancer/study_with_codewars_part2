c0844d919b3ca27fac500d034a20105fd481c09c
Paul <gafgafgarion05@gmail.com>
2022-05-04 10:49:31 +0300

    init

---
+++ 7 kyu Ghostbusters (whitespace removal).py
@@ -0,0 +1,8 @@
+def ghostbusters(building):
+    pass
+
+
+print(ghostbusters("Factor y"), "Factory", 'Nope, there may still be a ghost in the building. Try again.')
+print(ghostbusters("O  f fi ce"), "Office", 'Nope, there may still be a ghost in the building. Try again.')
+print(ghostbusters("BusStation"), "You just wanted my autograph didn't you?",
+      'Nope, as there were no ghosts in the BusStation you need to return a witty retort.')
