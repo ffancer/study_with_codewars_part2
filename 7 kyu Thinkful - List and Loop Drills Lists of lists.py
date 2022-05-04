26a8ddeb73ae53a19cbe75ab4d6775ff0ccdb8fa
Paul <gafgafgarion05@gmail.com>
2022-05-04 14:27:22 +0300

    init

---
+++ 7 kyu Thinkful - List and Loop Drills Lists of lists.py
@@ -0,0 +1,10 @@
+# 7 kyu
+# Thinkful - List and Loop Drills: Lists of lists
+
+
+def process_data(data):
+    pass
+
+
+print(process_data([[2, 5], [3, 4], [8, 7]]), 3)
+print(process_data([[2, 9], [2, 4], [7, 5]]), 28)
