05a501ecefcb85d07ddb64354ea4c891c9f7047d
Paul <gafgafgarion05@gmail.com>
2022-05-04 10:30:34 +0300

    1

--- 7 kyu Pairs of integers from m to n.py
+++ 7 kyu Pairs of integers from m to n.py
@@ -1,7 +1,9 @@
 def generate_pairs(m, n):
-    return []
+    for i in range(m, n+1):
+        for j in range(m, n+1):
+            print(i, j)
 
 
 print(generate_pairs(2, 4), [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)])
-print(generate_pairs(0, 1), [(0, 0), (0, 1), (1, 1)])
-print(generate_pairs(0, 0), [(0, 0)])
+# print(generate_pairs(0, 1), [(0, 0), (0, 1), (1, 1)])
+# print(generate_pairs(0, 0), [(0, 0)])
