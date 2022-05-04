fc4ac978e5f1b03422f1667770781a313410cdf8
Paul <gafgafgarion05@gmail.com>
2022-05-04 10:35:05 +0300

    my 1 line solution

--- 7 kyu Pairs of integers from m to n.py
+++ 7 kyu Pairs of integers from m to n.py
@@ -1,11 +1,5 @@
 def generate_pairs(m, n):
-    lst = []
-
-    for i in range(m, n+1):
-        for j in range(i, n+1):
-            lst.append((i, j))
-
-    return lst
+    return [(i, j) for i in range(m, n + 1) for j in range(i, n + 1)]
 
 
 print(generate_pairs(2, 4), [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (4, 4)])
