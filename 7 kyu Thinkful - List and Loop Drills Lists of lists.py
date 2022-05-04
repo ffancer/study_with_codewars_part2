31aa08c125ea58f88cfee5a5642919735036b547
Paul <gafgafgarion05@gmail.com>
2022-05-04 14:35:59 +0300

    2

--- 7 kyu Thinkful - List and Loop Drills Lists of lists.py
+++ 7 kyu Thinkful - List and Loop Drills Lists of lists.py
@@ -1,14 +1,14 @@
 # 7 kyu
 # Thinkful - List and Loop Drills: Lists of lists
 
-# from math import
+from math import prod
 def process_data(data):
 
     def diffr(items):
         return items[0] - sum(items[1:])
 
     for i in data:
-        print(diffr(i))
+        print(prod(diffr(i)))
 
 
 print(process_data([[2, 5], [3, 4], [8, 7]]), 3)
