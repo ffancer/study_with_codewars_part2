87aaa79d13251acd8699bce154e4d093900671cb
Paul <gafgafgarion05@gmail.com>
2022-05-05 16:52:03 +0300

    2

--- 7 kyu International Morse Code Encryption.py
+++ 7 kyu International Morse Code Encryption.py
@@ -1,7 +1,10 @@
 # CHAR_TO_MORSE preloaded to convert characters into Morse code
 def encryption(string):
-    for i in CHAR_TO_MORSE.values():
-        print(i)
+    s = ''
+    for i in string.split():
+        if i in CHAR_TO_MORSE.values():
+            s += CHAR_TO_MORSE[i]
+    return s
 
 
 print(encryption("HELLO WORLD"), ".... . .-.. .-.. ---   .-- --- .-. .-.. -..")
