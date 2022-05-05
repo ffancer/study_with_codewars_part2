730d8c3bc4f8469164a9331135ff3d18da0697cd
Paul <gafgafgarion05@gmail.com>
2022-05-05 17:32:55 +0300

    2

--- 7 kyu Diagonals sum.py
+++ 7 kyu Diagonals sum.py
@@ -1,6 +1,7 @@
 def sum_diagonals(matrix):
+    total = 0
     for i, j in enumerate(matrix):
-        print(i, j)
-
+        total += j[i]
+    return total
 
 print(sum_diagonals([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 1 + 5 + 9 + 3 + 5 + 7)
