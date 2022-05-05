0c8623f4f9f45733e7ff1b8af0f333b00328ec28
Paul <gafgafgarion05@gmail.com>
2022-05-05 16:39:50 +0300

    init

---
+++ 7 kyu Thinkful - Number Drills Rømer temperature.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - Number Drills: Rømer temperature
+
+
+def celsius_to_romer(temp):
+    pass
+
+
+print(celsius_to_romer(24), 20.1)
+print(celsius_to_romer(8), 11.7)
+print(celsius_to_romer(29), 22.725)
