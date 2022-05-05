032d80bd0f921f1a01594e7e2c15c9449303d95f
Paul <gafgafgarion05@gmail.com>
2022-05-05 17:16:17 +0300

    init

---
+++ 7 kyu Thinkful - String Drills Areacode extractor.py
@@ -0,0 +1,7 @@
+def area_code(text):
+    pass
+
+
+print(area_code("The supplier's phone number is (555) 867-5309"), '555')
+print(area_code("Grae's cell number used to be (123) 456-7890"), '123')
+print(area_code("The 102nd district court's fax line is (124) 816-3264"), '124')
