fc0cc9aeb48060548e50d5015416f2a9327fcbf5
Paul <gafgafgarion05@gmail.com>
2022-05-05 18:01:59 +0300

    6

--- 7 kyu Diagonals sum.py
+++ 7 kyu Diagonals sum.py
@@ -1,9 +1,12 @@
 def sum_diagonals(matrix):
+    if len(matrix) == 1:
+        return matrix[0] * 2
+
     # first diagonal
     first_dgnl = sum(matrix[i][i] for i in range(len(matrix)))
 
     # second diagonal
-    second_dgnl = sum(matrix[len(matrix) - i - 1][len(matrix) - i - 1] for i in range(len(matrix)))
+    second_dgnl = sum(matrix[i][len(matrix) - i - 1] for i in range(len(matrix)))
 
     return first_dgnl + second_dgnl
 
