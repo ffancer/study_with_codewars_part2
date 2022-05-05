be66b94547dc5f4a72b0e528cb67acf0b9bc6503
Paul <gafgafgarion05@gmail.com>
2022-05-05 18:07:08 +0300

    solution

--- 7 kyu Diagonals sum.py
+++ 7 kyu Diagonals sum.py
@@ -1,15 +1,13 @@
 def sum_diagonals(matrix):
-    if len(matrix) == 1:
-        return matrix[0] * 2
+    matrix_len = len(matrix[0])
 
-    # first diagonal
-    first_dgnl = sum(matrix[i][i] for i in range(len(matrix)))
+    m1 = [matrix[x][x] for x in range(matrix_len)]
+    m2 = [matrix[x][-x - 1] for x in range(matrix_len)]
 
-    # second diagonal
-    second_dgnl = sum(matrix[i][len(matrix) - i - 1] for i in range(len(matrix)))
-
-    return first_dgnl + second_dgnl
+    return sum(m1) + sum(m2)
 
 
 print(sum_diagonals([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 1 + 5 + 9 + 3 + 5 + 7)
 print(sum_diagonals([]))
+print(sum_diagonals([4]))
+print(sum_diagonals([[4], [4]]))
