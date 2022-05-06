eeb5dd91792195e70c6c587fc9877f3919c06349
Paul <gafgafgarion05@gmail.com>
2022-05-06 16:51:32 +0300

    init

---
+++ 7 kyu Point in a unit circle.py
@@ -0,0 +1,9 @@
+def point_in_circle(x, y):
+    pass
+
+
+print(point_in_circle(0, 0), True)
+print(point_in_circle(2, 0), False)
+print(point_in_circle(-1.1, 0), False)
+print(point_in_circle(0, 0.9), True)
+print(point_in_circle(1, 0), False)
