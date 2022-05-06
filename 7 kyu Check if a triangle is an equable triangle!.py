111f7fc372cb7a2d7e24fb05601427f14019483a
Paul <gafgafgarion05@gmail.com>
2022-05-06 14:32:36 +0300

    init

---
+++ 7 kyu Check if a triangle is an equable triangle!.py
@@ -0,0 +1,19 @@
+def equable_triangle(a, b, c):
+    pass
+
+
+print(equable_triangle(5, 12, 13), True)
+print(equable_triangle(2, 3, 4), False)
+print(equable_triangle(6, 8, 10), True)
+print(equable_triangle(7, 15, 20), True)
+print(equable_triangle(17, 17, 30), False)
+print(equable_triangle(7, 10, 12), False)
+print(equable_triangle(6, 11, 12), False)
+print(equable_triangle(25, 25, 45), False)
+print(equable_triangle(13, 37, 30), False)
+print(equable_triangle(6, 25, 29), True)
+print(equable_triangle(10, 11, 18), False)
+print(equable_triangle(73, 9, 80), False)
+print(equable_triangle(12, 35, 37), False)
+print(equable_triangle(120, 109, 13), False)
+print(equable_triangle(9, 10, 17), True)
