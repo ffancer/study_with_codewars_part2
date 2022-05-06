803beae2cce9d8507319364ee8117f345cf44816
Paul <gafgafgarion05@gmail.com>
2022-05-06 16:36:58 +0300

    2

--- 7 kyu Transpose two strings in an array.py
+++ 7 kyu Transpose two strings in an array.py
@@ -1,13 +1,21 @@
 def transpose_two_strings(arr):
     first, second = arr[0], arr[1]
-    s = ''
+    lst = []
 
     for i in range(max(len(first), len(second))):
-        s +=
+        if first[i] == '':
+            lst.append('  ' + second[i])
+        if second[i] == '':
+            lst.append(first[i])
+        else:
+            lst.append(first[i] + ' ' + second[i])
 
 
-print(transpose_two_strings(["Hello", "World"]), "H W\ne o\nl r\nl l\no d")
-print(transpose_two_strings(["joey", "louise"]), "j l\no o\ne u\ny i\n  s\n  e")
-print(transpose_two_strings(["a", "cat"]), "a c\n  a\n  t")
+    return '\n'.join(lst)
+
+
+# print(transpose_two_strings(["Hello", "World"]))  # , "H W\ne o\nl r\nl l\no d"
+# print(transpose_two_strings(["joey", "louise"]), "j l\no o\ne u\ny i\n  s\n  e")
+# print(transpose_two_strings(["a", "cat"]), "a c\n  a\n  t")
 print(transpose_two_strings(["cat", ""]), "c  \na  \nt  ")
-print(transpose_two_strings(["!a!a!", "?b?b"]), "! ?\na b\n! ?\na b\n!  ")
+# print(transpose_two_strings(["!a!a!", "?b?b"]), "! ?\na b\n! ?\na b\n!  ")
