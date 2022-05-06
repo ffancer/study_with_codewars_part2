d6c6d1ce95fb9e85ac9ea2e73fd8dd1c9d6d3fa6
Paul <gafgafgarion05@gmail.com>
2022-05-06 14:20:15 +0300

    init

---
+++ 7 kyu Negative Connotation.py
@@ -0,0 +1,9 @@
+def connotation(strng):
+    return
+
+
+print(connotation("A big brown fox caught a bad bunny"), True)
+print(connotation("Xylophones can obtain Xenon."), False)
+print(connotation("CHOCOLATE MAKES A GREAT SNACK"), True)
+print(connotation("All FOoD tAsTEs NIcE for someONe"), True)
+print(connotation("Is  this the  best  Kata?"), True)
