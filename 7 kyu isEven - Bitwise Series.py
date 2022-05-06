5d2d45393e9b8eac027f51525f63e1c7627c2302
Paul <gafgafgarion05@gmail.com>
2022-05-06 14:13:57 +0300

    init

---
+++ 7 kyu isEven - Bitwise Series.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# isEven? - Bitwise Series
+
+
+def is_even(n):
+    pass
+
+
+print(is_even(2), True, "Expected 'True'")
+print(is_even(3), False, "Expected 'False'")
+print(is_even(14), True, "Expected 'True'")
+print(is_even(15), False, "Expected 'False'")
+print(is_even(26), True, "Expected 'True'")
+print(is_even(27), False, "Expected 'False'")
