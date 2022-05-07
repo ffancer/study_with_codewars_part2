cf1514d20d4dc6b17dca2f62a6a8e8c4c8faced9
Paul <gafgafgarion05@gmail.com>
2022-05-07 09:38:19 +0300

    init

---
+++ 7 kyu Alan Partridge III - London.py
@@ -0,0 +1,14 @@
+def alan(arr):
+    pass
+
+
+print(
+    alan(["Norwich", "Rejection", "Disappointment", "Backstabbing Central", "Shattered Dreams Parkway", "London"]),
+    "Smell my cheese you mother!")
+print(alan(["London", "Norwich"]), "No, seriously, run. You will miss it.")
+print(alan(["Norwich", "Tooting", "Bank", "Rejection", "Disappointment", "Backstabbing Central", "Exeter",
+            "Shattered Dreams Parkway", "Belgium", "London"]), "Smell my cheese you mother!")
+print(alan(["London", "Norwich"]), "No, seriously, run. You will miss it.")
+print(
+    alan(["London", "Shattered Dreams Parkway", "Backstabbing Central", "Disappointment", "Rejection", "Norwich"]),
+    "Smell my cheese you mother!")
