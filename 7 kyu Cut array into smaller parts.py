621d2bc8ebceff8ba2887903ff627319d72dafb9
Paul <gafgafgarion05@gmail.com>
2022-05-07 10:40:15 +0300

    init

---
+++ 7 kyu Cut array into smaller parts.py
@@ -0,0 +1,7 @@
+def make_parts(arr, chunkSize):
+    return []
+
+
+print(make_parts([1, 2, 3, 4, 5], 2), [[1, 2], [3, 4], [5]])
+print(make_parts([1, 2, 3], 1), [[1], [2], [3]])
+print(make_parts([1, 2, 3, 4, 5], 10), [[1, 2, 3, 4, 5]])
