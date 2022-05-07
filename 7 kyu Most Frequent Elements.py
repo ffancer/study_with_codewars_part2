36776fc3f0a8242d711fb39c8c8d872add2c66a2
Paul <gafgafgarion05@gmail.com>
2022-05-07 16:13:50 +0300

    1

--- 7 kyu Most Frequent Elements.py
+++ 7 kyu Most Frequent Elements.py
@@ -1,6 +1,10 @@
 def find_most_frequent(l):
-    # Write your code here.
-    return set()
+    dct = {}
+
+    for i in l:
+        dct.update({i: l.count(i)})
+
+    return dct
 
 
 print(find_most_frequent([1, 1, 2, 3]), set([1]), 'one most frequent element')
