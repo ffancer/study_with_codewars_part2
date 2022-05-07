6746a790d5e770c1d0e279997f57e7488d7e2288
Paul <gafgafgarion05@gmail.com>
2022-05-07 11:29:10 +0300

    init

---
+++ 7 kyu Matrix creation.py
@@ -0,0 +1,9 @@
+def get_matrix(n):
+    pass
+
+
+print(get_matrix(0), [])
+print(get_matrix(1), [[1]])
+print(get_matrix(2), [[1, 0], [0, 1]])
+print(get_matrix(3), [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+print(get_matrix(5), [[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
