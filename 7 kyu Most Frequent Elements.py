a15356c60afe4bcc518040ceef058a0d5328ef07
Paul <gafgafgarion05@gmail.com>
2022-05-07 16:06:27 +0300

    init

---
+++ 7 kyu Most Frequent Elements.py
@@ -0,0 +1,8 @@
+def find_most_frequent(l):
+    # Write your code here.
+    return set()
+
+
+print(find_most_frequent([1, 1, 2, 3]), set([1]), 'one most frequent element')
+print(find_most_frequent([1, 1, 2, 2, 3]), set([1, 2]), 'two most frequent element')
+print(find_most_frequent([]), set([]), 'empty')
