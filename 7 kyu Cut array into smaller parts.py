06ec0ab1aa5ee868559fcf2b762b3085df04cad2
Paul <gafgafgarion05@gmail.com>
2022-05-07 10:41:50 +0300

    my solution

--- 7 kyu Cut array into smaller parts.py
+++ 7 kyu Cut array into smaller parts.py
@@ -1,5 +1,11 @@
 def make_parts(arr, chunkSize):
-    return []
+    lst = []
+
+    while arr:
+        lst.append(arr[:chunkSize])
+        arr = arr[chunkSize:]
+
+    return lst
 
 
 print(make_parts([1, 2, 3, 4, 5], 2), [[1, 2], [3, 4], [5]])
