270fd6e0ed9355f324e9ccc325adcf7687313352
Paul <gafgafgarion05@gmail.com>
2022-05-09 10:45:40 +0300

    init

---
+++ 7 kyu Thinkful - Logic Drills Umbrella decider.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - Logic Drills: Umbrella decider
+
+
+def take_umbrella(weather, rain_chance):
+    pass
+
+
+print(take_umbrella('sunny', 0.40), False)
+print(take_umbrella('rainy', 0.0), True)
+print(take_umbrella('cloudy', 0.20), False)
