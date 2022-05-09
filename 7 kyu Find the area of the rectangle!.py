822fbc51127e8f175f01255be521ef77a0e2606d
Paul <gafgafgarion05@gmail.com>
2022-05-09 13:52:01 +0300

    init

---
+++ 7 kyu Find the area of the rectangle!.py
@@ -0,0 +1,11 @@
+def area(d, l):
+    # your code here
+    pass
+
+
+print(area(5, 4), 12)
+print(area(10, 6), 48)
+print(area(13, 5), 60)
+print(area(12, 5), 54.54)
+print(area(5, 5), "Not a rectangle")
+print(area(3, 5), "Not a rectangle")
