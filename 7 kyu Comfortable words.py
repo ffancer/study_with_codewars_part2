6ee2026c919fa6a03b7f543872532607a8263349
Paul <gafgafgarion05@gmail.com>
2022-05-10 14:53:47 +0300

    solution

--- 7 kyu Comfortable words.py
+++ 7 kyu Comfortable words.py
@@ -1,4 +1,11 @@
-def comfortable_word(word):
+def comfortable_word(w):
+    l, r, w = 'qwertasdfgzxcvb', 'yuiophjklnm', w.lower()
+    x = 0 if w[0] in l else 1
+    for i in range(len(w)):
+        if i % 2 == 0 and w[i] not in (l if x == 0 else r):
+            return False
+        if i % 2 == 1 and w[i] not in (r if x == 0 else l):
+            return False
     return True
 
 
