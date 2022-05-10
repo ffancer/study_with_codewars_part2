3f14b14e1b6b39a68bfc424b2828848b756fce98
Paul <gafgafgarion05@gmail.com>
2022-05-10 14:42:42 +0300

    init

---
+++ 7 kyu Mispelled word.py
@@ -0,0 +1,10 @@
+def mispelled(word1, word2):
+    # write your code here
+    pass
+
+
+print(mispelled('versed', 'xersed'), True)
+print(mispelled('versed', 'applb'), False)
+print(mispelled('versed', 'v5rsed'), True)
+print(mispelled('1versed', 'versed'), True)
+print(mispelled('versed', 'versed'), True)
