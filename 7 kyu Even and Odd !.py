a516efc1dff98ea345fc2df3b01007021345e90a
Paul <gafgafgarion05@gmail.com>
2022-05-11 10:36:59 +0300

    my solution

--- 7 kyu Even and Odd !.py
+++ 7 kyu Even and Odd !.py
@@ -7,9 +7,15 @@ def even_and_odd(n):
         else:
             odd += i
 
+    if even == '':
+        even = '0'
+    elif odd == '':
+        odd = '0'
+
     return int(even), int(odd)
 
 
 print(even_and_odd(126453), (264, 153))
 print(even_and_odd(3012), (2, 31))
 print(even_and_odd(4628), (4628, 0))
+
