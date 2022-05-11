b47030082ca6dd2061fac1df29358ad77dd65d00
Paul <gafgafgarion05@gmail.com>
2022-05-11 10:32:05 +0300

    1

--- 7 kyu Even and Odd !.py
+++ 7 kyu Even and Odd !.py
@@ -1,7 +1,13 @@
 def even_and_odd(n):
-    # your code here
-    pass
+    even, odd = '', ''
 
+    for i in str(n):
+        if int(i) % 2 == 0:
+            even += i
+        else:
+            odd += i
+
+    return even, odd
 
 print(even_and_odd(126453), (264, 153))
 print(even_and_odd(3012), (2, 31))
