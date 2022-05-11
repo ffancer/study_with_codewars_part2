632bb0d6d6aef5e4fa5394aba4226bb53498c083
Paul <gafgafgarion05@gmail.com>
2022-05-11 10:21:10 +0300

    2 was boring to make this dict

--- 7 kyu Decoding a message.py
+++ 7 kyu Decoding a message.py
@@ -1,5 +1,9 @@
 def decode(message):
-    dct = {'a''b''c''d''e''f''g''h''i''j''k''l''m''n''o''p''q''r''s''t''u''v''w''x''y''z'}
+    dct = {'a': 'z','b': 'y', 'c': 'x', 'd': 'w', 'e': 'v',
+           'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q',
+           'k': 'p', 'l': 'o', 'm': 'n', 'n': 'm', 'o': 'l',
+           'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g',
+           'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'}
 
 
 print(decode("sr"), "hi")
