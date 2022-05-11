6dfc71ca161c1c9137bff0b91e20b395383de185
Paul <gafgafgarion05@gmail.com>
2022-05-11 14:55:07 +0300

    1

--- 7 kyu Is There an Odd Bit.py
+++ 7 kyu Is There an Odd Bit.py
@@ -3,8 +3,13 @@
 
 
 def any_odd(x):
-    # Write code here...
-    return
+    x = bin(x)[2:]
+    for i in range(len(x)):
+        if i % 2 != 0:
+            if x[i] == '1':
+                return True
+    return False
+
 
 
 print(any_odd(5), 0)
