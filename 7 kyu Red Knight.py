0fe72c00f8d108ea05d0c83b2a163c36a8e657b7
Paul <gafgafgarion05@gmail.com>
2022-05-11 14:24:20 +0300

    init

---
+++ 7 kyu Red Knight.py
@@ -0,0 +1,8 @@
+def red_knight(N, P):
+    pass
+
+
+print(red_knight(0, 8), ('White', 16))
+print(red_knight(0, 7), ('Black', 14))
+print(red_knight(1, 6), ('Black', 12))
+print(red_knight(1, 5), ('White', 10))
