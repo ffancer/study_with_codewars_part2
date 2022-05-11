259715817e58ea2d60176588bd1b0cbf1295c2f6
Paul <gafgafgarion05@gmail.com>
2022-05-11 10:50:40 +0300

    my solution

--- 7 kyu Chicken Sexing.py
+++ 7 kyu Chicken Sexing.py
@@ -4,8 +4,11 @@ def correctness(bobs_decisions, expert_decisions):
     for i in range(len(bobs_decisions)):
         if bobs_decisions[i] == expert_decisions[i]:
             cnt += 1
+        if (bobs_decisions[i] != expert_decisions[i]) and (bobs_decisions[i] == '?' or expert_decisions[i] == '?'):
+            cnt += .5
 
     return cnt
+
 
 print(correctness(('M', 'F', '?'), ('M', 'F', '?')), 3)
 print(correctness(('M', '?', 'M'), ('M', 'F', '?')), 2)
