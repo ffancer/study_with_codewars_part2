d6b4417a3a8307adef23603224996b73c34c044b
Paul <gafgafgarion05@gmail.com>
2022-05-12 10:38:55 +0300

    my 1 line solution

--- 7 kyu Odd or Even Determine that!.py
+++ 7 kyu Odd or Even Determine that!.py
@@ -3,11 +3,8 @@
 
 
 def odd_or_even(n):
-    if n % 2 == 1:
-        return 'Either'
-    elif (n / 2) % 2 == 0:
-        return 'Even'
-    return 'Odd'
+    return 'Either' if n % 2 == 1 else 'Even' if (n / 2) % 2 == 0 else 'Odd'
+
 
 print(odd_or_even(1), "Either")
 print(odd_or_even(3), "Either")
