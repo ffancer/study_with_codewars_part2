ca7975b8b5c923de5cf8f6e2493c6b2c472cd63a
Paul <gafgafgarion05@gmail.com>
2022-05-12 13:10:45 +0300

    init

---
+++ 7 kyu Suitcase packing.py
@@ -0,0 +1,9 @@
+def fit_in(a, b, m, n):
+    # You may code here
+    pass
+
+
+print(fit_in(1, 2, 3, 2), True)
+print(fit_in(1, 2, 2, 1), False)
+print(fit_in(3, 2, 3, 2), False)
+print(fit_in(1, 2, 1, 2), False)
