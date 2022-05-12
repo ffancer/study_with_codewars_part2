8f960e7d63092c92f4999a725e981bdf42d902fa
Paul <gafgafgarion05@gmail.com>
2022-05-12 14:03:28 +0300

    2 forget about float

--- 7 kyu reverseIt.py
+++ 7 kyu reverseIt.py
@@ -1,6 +1,9 @@
 def reverse_it(data):
-    return str(data)[::-1] if type(data) in [str, int] else data
-
+    if type(data) is str:
+        return data[::-1]
+    elif type(data) is int:
+        return int(str(data)[::-1])
+    return data
 
 print(reverse_it('Hello'), "olleH", 'Not quite')
 print(reverse_it(314159), 951413, 'Not quite')
