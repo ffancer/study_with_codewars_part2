c877bc081602e35b3dcb2742748674c74ab2bec5
Paul <gafgafgarion05@gmail.com>
2022-05-12 10:33:03 +0300

    init

---
+++ 7 kyu Odd or Even Determine that!.py
@@ -0,0 +1,12 @@
+# 7 kyu
+# Odd or Even? Determine that!
+
+
+def odd_or_even(n):
+    return "Even"
+
+
+print(odd_or_even(1), "Either")
+print(odd_or_even(3), "Either")
+print(odd_or_even(6), "Odd")
+print(odd_or_even(8), "Even")
