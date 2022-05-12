44e7d59ec3d806bc12a258c91a80e204b1a4d986
Paul <gafgafgarion05@gmail.com>
2022-05-12 11:04:01 +0300

    2

--- 7 kyu Hero's root.py
+++ 7 kyu Hero's root.py
@@ -1,6 +1,10 @@
 def int_rac(n, guess):
-    return (n + guess / n) / 2
-
+    # return (n + guess / n) / 2
+    cnt = 0
+    while n > 0:
+        cnt += 1
+        n = (n + guess / n) / 2
+    return cnt
 
 print(int_rac(25, 1), 4)
 print(int_rac(125348, 300), 3)
