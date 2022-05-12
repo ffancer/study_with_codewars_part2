509ce2ebd160f63ec360bda74629b5e6ab65a81c
Paul <gafgafgarion05@gmail.com>
2022-05-12 13:24:49 +0300

    solution

--- 7 kyu Shift Left.py
+++ 7 kyu Shift Left.py
@@ -1,6 +1,10 @@
 def shift_left(a, b):
-    return
-
+    r = len(a) + len(b)
+    for i in range(-1, -min(len(a), len(b)) - 1, -1):
+        if a[i] != b[i]:
+            break
+        r -= 2
+    return r
 
 print(shift_left("test", "west"), 2)
 print(shift_left("test", "yes"), 7)
