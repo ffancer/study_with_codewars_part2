42b2b1acfea650a6c8067b0bc3729f940eec36c3
Paul <gafgafgarion05@gmail.com>
2022-05-12 11:17:29 +0300

    solution

--- 7 kyu Hero's root.py
+++ 7 kyu Hero's root.py
@@ -1,8 +1,13 @@
 def int_rac(n, guess):
-    # return (n + guess / n) / 2
-    x = (guess + n / guess) / 2
-    if abs(x - guess) < 1:
-        return 1
+    """Integer Square Root of an Integer"""
+    x = guess
+    cnt = 1
+    while True:
+        newx = (x + n // x) // 2
+        if abs(x - newx) < 1:
+            return cnt
+        x = newx
+        cnt += 1
 
 print(int_rac(25, 1), 4)
 print(int_rac(125348, 300), 3)
