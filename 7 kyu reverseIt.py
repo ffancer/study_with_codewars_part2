5193cbe79b9432b2076a3bfb6a2b314feb806bdc
Paul <gafgafgarion05@gmail.com>
2022-05-12 14:00:33 +0300

    init

---
+++ 7 kyu reverseIt.py
@@ -0,0 +1,7 @@
+def reverse_it(data):
+    pass
+
+
+print(reverse_it('Hello'), "olleH", 'Not quite')
+print(reverse_it(314159), 951413, 'Not quite')
+print(reverse_it("314159"), "951413", 'Not quite')
