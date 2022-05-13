9981d296da30b5127f12b3f4492c7d6d7eb9e418
Paul <gafgafgarion05@gmail.com>
2022-05-13 12:40:40 +0300

    1

--- 7 kyu Adding remainders to a list.py
+++ 7 kyu Adding remainders to a list.py
@@ -1,5 +1,9 @@
 def solve(nums, div):
-    pass
+    lst = []
+    for i in nums:
+        temp = i % div
+        lst.append(temp + i)
+    return lst
 
 
 print(solve([2, 7, 5, 9, 100, 34, 32, 0], 3), [4, 8, 7, 9, 101, 35, 34, 0])
