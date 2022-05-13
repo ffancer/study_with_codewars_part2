25e26286c4170b115353769dae3f06f3f31fe7f8
Paul <gafgafgarion05@gmail.com>
2022-05-13 12:53:58 +0300

    1

--- 7 kyu Grid blast!.py
+++ 7 kyu Grid blast!.py
@@ -1,5 +1,11 @@
+grid = ['top left', 'top middle', 'top right',
+        'middle left', 'center', 'middle right',
+        'bottom left', 'bottom middle', 'bottom right']
+
+
 def fire(x, y):
-    pass
+    for i, j in enumerate(grid):
+        print(j[x][y])
 
 
 print(fire(0, 0), "top left")
