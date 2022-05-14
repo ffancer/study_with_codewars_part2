0560a97851a331b7fcfc40850d9a6ef99299f09b
Paul <gafgafgarion05@gmail.com>
2022-05-14 13:28:11 +0300

    4

--- 7 kyu Duplicate sandwich.py
+++ 7 kyu Duplicate sandwich.py
@@ -1,10 +1,6 @@
 def duplicate_sandwich(arr):
-    border = [i for i in arr if arr.count(i) == 2][0]
-    lst = []
-
-    for i in arr:
-        if i == border:
-
+    border1, border2 = [i for i, j in enumerate(arr) if arr.count(j) == 2]
+    return border1, border2
 
 
 print(duplicate_sandwich([0, 1, 2, 3, 4, 5, 6, 1, 7, 8]), [2, 3, 4, 5, 6])
