36723263d2dee2d87c42705d31c81bfeaea0dbf7
Paul <gafgafgarion05@gmail.com>
2022-05-14 13:12:45 +0300

    init

---
+++ 7 kyu Duplicate sandwich.py
@@ -0,0 +1,10 @@
+def duplicate_sandwich(arr):
+    pass
+
+
+print(duplicate_sandwich([0, 1, 2, 3, 4, 5, 6, 1, 7, 8]), [2, 3, 4, 5, 6])
+print(duplicate_sandwich(['None', 'Hello', 'Example', 'hello', 'None', 'Extra']),
+      ['Hello', 'Example', 'hello'])
+print(duplicate_sandwich([0, 0]), [])
+print(duplicate_sandwich([True, False, True]), [False])
+print(duplicate_sandwich(['e', 'x', 'a', 'm', 'p', 'l', 'e']), ['x', 'a', 'm', 'p', 'l'])
