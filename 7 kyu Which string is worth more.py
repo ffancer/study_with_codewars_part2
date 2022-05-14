504faca7bb830fab705b9940897f5226960c6cc2
Paul <gafgafgarion05@gmail.com>
2022-05-14 16:06:58 +0300

    my second solution more compact

--- 7 kyu Which string is worth more.py
+++ 7 kyu Which string is worth more.py
@@ -3,19 +3,9 @@
 
 
 def highest_value(a, b):
-    total_a, total_b = 0, 0
-
-    for i in a:
-        total_a += ord(i)
-
-    for i in b:
-        total_b += ord(i)
-
-    if total_a == total_b:
-        return a
-    elif total_a > total_b:
-        return a
-    return b
+    total_a = sum(ord(i) for i in a)
+    total_b = sum(ord(i) for i in b)
+    return a if total_a >= total_b else b
 
 
 print(highest_value("AaBbCcXxYyZz0189", "KkLlMmNnOoPp4567"), "KkLlMmNnOoPp4567")
