460532ec66ca6a900becc011676d09dd6bc32190
Paul <gafgafgarion05@gmail.com>
2022-05-14 15:32:49 +0300

    my solution

--- 7 kyu No ifs no buts.py
+++ 7 kyu No ifs no buts.py
@@ -1,14 +1,15 @@
 def no_ifs_no_buts(a, b):
-    try:
-        return f'{min(a, b)} is smaller than {max(a, b)}'
-    except:
-        return f'{a} is equal to {b}'
+    return {
+        a == b: f'{a} is equal to {b}',
+        a < b: f'{a} is smaller than {b}',
+        a > b: f'{a} is greater than {b}',
+    }[True]
 
 
-print(no_ifs_no_buts(45,51),"45 is smaller than 51")
-print(no_ifs_no_buts(1,2),"1 is smaller than 2")
-print(no_ifs_no_buts(-3,2),"-3 is smaller than 2")
-print(no_ifs_no_buts(1,1),"1 is equal to 1")
-print(no_ifs_no_buts(100,100),"100 is equal to 100")
-print(no_ifs_no_buts(100,80),"100 is greater than 80")
-print(no_ifs_no_buts(20,19),"20 is greater than 19")
+print(no_ifs_no_buts(45, 51), "45 is smaller than 51")
+print(no_ifs_no_buts(1, 2), "1 is smaller than 2")
+print(no_ifs_no_buts(-3, 2), "-3 is smaller than 2")
+print(no_ifs_no_buts(1, 1), "1 is equal to 1")
+print(no_ifs_no_buts(100, 100), "100 is equal to 100")
+print(no_ifs_no_buts(100, 80), "100 is greater than 80")
+print(no_ifs_no_buts(20, 19), "20 is greater than 19")
