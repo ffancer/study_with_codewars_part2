ff6f1faf573d6c0d69d853f5b2554ec8f523a568
Paul <gafgafgarion05@gmail.com>
2022-05-14 15:22:32 +0300

    init

---
+++ 7 kyu No ifs no buts.py
@@ -0,0 +1,11 @@
+def no_ifs_no_buts(a, b):
+    pass
+
+
+print(no_ifs_no_buts(45,51),"45 is smaller than 51")
+print(no_ifs_no_buts(1,2),"1 is smaller than 2")
+print(no_ifs_no_buts(-3,2),"-3 is smaller than 2")
+print(no_ifs_no_buts(1,1),"1 is equal to 1")
+print(no_ifs_no_buts(100,100),"100 is equal to 100")
+print(no_ifs_no_buts(100,80),"100 is greater than 80")
+print(no_ifs_no_buts(20,19),"20 is greater than 19")
