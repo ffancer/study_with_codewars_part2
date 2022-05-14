f39d0b0d61836a2b5c9c206a1b5d5ecf055576c7
Paul <gafgafgarion05@gmail.com>
2022-05-14 09:33:43 +0300

    2

--- 7 kyu Guess the Sequence.py
+++ 7 kyu Guess the Sequence.py
@@ -1,6 +1,7 @@
 def sequence(x):
     if x < 10:
         return [i for i in range(1, x + 1)]
+    return [1] + [i for i in range(10, x + 1)] + [i for i in range(2, 10)]
 
 
 print(sequence(16), [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9])
