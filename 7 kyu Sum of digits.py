9c23a8fe0a9368574ec6f41e8f72be345ef5dd31
Paul <gafgafgarion05@gmail.com>
2022-05-14 13:31:56 +0300

    init

---
+++ 7 kyu Sum of digits.py
@@ -0,0 +1,12 @@
+def sum_of_digits(digits):
+    # your code here
+    pass
+
+
+print(sum_of_digits("3433"), "3 + 4 + 3 + 3 = 13")
+print(sum_of_digits("64323"), "6 + 4 + 3 + 2 + 3 = 18")
+print(sum_of_digits("8"), "8 = 8")
+print(sum_of_digits(3433), "3 + 4 + 3 + 3 = 13")
+print(sum_of_digits(64323), "6 + 4 + 3 + 2 + 3 = 18")
+print(sum_of_digits(8), "8 = 8")
+print(sum_of_digits(None), "")
