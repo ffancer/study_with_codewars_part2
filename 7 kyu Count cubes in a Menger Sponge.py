941bb412ee42a364699aea3df383de71250976a2
Paul <gafgafgarion05@gmail.com>
2022-05-14 15:43:34 +0300

    init

---
+++ 7 kyu Count cubes in a Menger Sponge.py
@@ -0,0 +1,11 @@
+def calc_ms(n):
+    pass
+
+
+print(calc_ms(0), 1)
+print(calc_ms(1), 20)
+print(calc_ms(2), 400)
+print(calc_ms(3), 8000)
+print(calc_ms(4), 160000)
+print(calc_ms(5), 3200000)
+print(calc_ms(6), 64000000)
