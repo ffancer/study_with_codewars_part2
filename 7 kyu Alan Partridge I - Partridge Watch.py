6e5524aba53ceb28c43b13f4ef86d6884870d19d
Paul <gafgafgarion05@gmail.com>
2022-05-14 16:34:23 +0300

    init

---
+++ 7 kyu Alan Partridge I - Partridge Watch.py
@@ -0,0 +1,12 @@
+def part(arr):
+    pass
+
+
+print(part(["Grouse", "Partridge", "Pheasant"]), "Mine's a Pint!")
+print(part(["Pheasant", "Goose", "Starling", "Robin"]), "Lynn, I've pierced my foot on a spike!!")
+print(part(["Grouse", "Partridge", "Partridge", "Partridge", "Pheasant"]), "Mine's a Pint!!!")
+print(part([]), "Lynn, I've pierced my foot on a spike!!")
+print(part(
+    ["Grouse", "Partridge", "Pheasant", "Goose", "Starling", "Robin", "Thrush", "Emu", "PearTree", "Chat", "Dan",
+     "Square", "Toblerone", "Lynn", "AlphaPapa", "BMW", "Graham", "Tool", "Nomad", "Finger", "Hamster"]),
+    "Mine's a Pint!!!!!!!!")
