5a36c461c577bede076ebe89ee3aea01e12678eb
Paul <gafgafgarion05@gmail.com>
2022-05-14 10:17:26 +0300

    init

---
+++ 7 kyu makeAcronym.py
@@ -0,0 +1,8 @@
+def make_acronym(phrase):
+    pass
+
+
+print(make_acronym('My aunt sally'), 'MAS', 'Output not as expected')
+print(make_acronym('Please excuse my dear aunt Sally'), 'PEMDAS', 'Output not as expected')
+print(make_acronym('How much wood would a woodchuck chuck if a woodchuck could chuck wood'), 'HMWWAWCIAWCCW',
+      'Output not as expected')
