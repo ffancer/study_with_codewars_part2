a0dcfa392edebe4594a0c343e5faa63c94b3acaa
Paul <gafgafgarion05@gmail.com>
2022-05-14 13:24:00 +0300

    3

--- 7 kyu Duplicate sandwich.py
+++ 7 kyu Duplicate sandwich.py
@@ -1,6 +1,11 @@
 def duplicate_sandwich(arr):
     border = [i for i in arr if arr.count(i) == 2][0]
-    return arr[arr.index(border)+1:]
+    lst = []
+
+    for i in arr:
+        if i == border:
+
+
 
 print(duplicate_sandwich([0, 1, 2, 3, 4, 5, 6, 1, 7, 8]), [2, 3, 4, 5, 6])
 print(duplicate_sandwich(['None', 'Hello', 'Example', 'hello', 'None', 'Extra']),
