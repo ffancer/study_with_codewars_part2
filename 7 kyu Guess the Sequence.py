3e6c302a2507fddc155eaede402903cbfbbff9a2
Paul <gafgafgarion05@gmail.com>
2022-05-14 09:37:10 +0300

    my 1 line solution

--- 7 kyu Guess the Sequence.py
+++ 7 kyu Guess the Sequence.py
@@ -1,6 +1,5 @@
 def sequence(x):
-    lst = [str(i) for i in range(1, x + 1)]
-    return [int(i) for i in sorted(lst)]
+    return [int(i) for i in sorted([str(i) for i in range(1, x + 1)])]
 
 
 print(sequence(16), [1, 10, 11, 12, 13, 14, 15, 16, 2, 3, 4, 5, 6, 7, 8, 9])
