97d0b12149e5e471c0adb2bef0d782046c58e9e4
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:26:18 +0300

    init

---
+++ 7 kyu Start with a Vowel.py
@@ -0,0 +1,10 @@
+def vowel_start(st):
+    # your code here
+    pass
+
+
+print(vowel_start('It is beautiful weather today!'), 'it isb e a ut if ulw e ath ert od ay', )
+print(vowel_start('Coding is great'), 'c od ing isgr e at', )
+print(vowel_start('my number is 0208-533-2325'), 'myn umb er is02085332325', )
+print(vowel_start('oranges, apples, melon, pineapple'), 'or ang es appl esm el onp in e appl e', )
+print(vowel_start('under_score'), 'und ersc or e')
