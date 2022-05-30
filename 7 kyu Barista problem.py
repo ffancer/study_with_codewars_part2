f597a7958af75b9be0378b7f4f1cfc5db453b4e3
Paul <gafgafgarion05@gmail.com>
2022-05-30 12:44:00 +0300

    init

---
+++ 7 kyu Barista problem.py
@@ -0,0 +1,9 @@
+def barista(coffees):
+    return
+
+
+print(barista([2, 10, 5, 3, 9]), 85, 'Sorry, but the smallest waiting time possible is: 85')
+print(barista([4, 3, 2]), 22, 'Sorry, but the smallest waiting time possible is: 22')
+print(barista([20, 5]), 32, 'Sorry, but the smallest waiting time possible is: 32')
+print(barista([20, 5, 4, 3, 1, 5, 7, 8]), 211, 'Sorry, but the smallest waiting time possible is: 211')
+print(barista([5, 4, 3, 2, 1]), 55, 'Sorry, but the smallest waiting time possible is: 55')
