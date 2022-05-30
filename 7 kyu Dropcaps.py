92b7f071112c505b30c75a3acefe87ce84d6eebb
Paul <gafgafgarion05@gmail.com>
2022-05-30 12:17:23 +0300

    my solution

--- 7 kyu Dropcaps.py
+++ 7 kyu Dropcaps.py
@@ -1,7 +1,15 @@
 def drop_cap(str_):
-    pass
+    s = str_.split(' ')
+    lst = []
+    for i in s:
+        if len(i) > 2:
+            lst.append(i.title())
+        else:
+            lst.append(i)
+    return ' '.join(lst)
 
 
 print(drop_cap('Apple Banana'), "Apple Banana")
 print(drop_cap('Apple'), "Apple")
 print(drop_cap(''), "")
+print(drop_cap("   space WALK   "), "   Space Walk   ")
