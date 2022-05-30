4f15d64ea64a30cc4f22bcfabdac3e6dc4a8d64a
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:09:22 +0300

    init

---
+++ 7 kyu You Got Change.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# You Got Change?
+
+
+def give_change(amount):
+    # your code here
+    pass
+
+
+print(give_change(365), (0, 1, 1, 0, 1, 3))
+print(give_change(217), (2, 1, 1, 0, 0, 2))
