afef8d9bbddbda4713665656b049856a5513386a
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:16:10 +0300

    2

--- 7 kyu You Got Change.py
+++ 7 kyu You Got Change.py
@@ -5,6 +5,10 @@
 def give_change(amount):
     ans = [0, 0, 0, 0, 0]
 
+    while amount > 0:
+        ans[4] = amount // 100
+        amount -= ans[4] * 100
+        return amount
 
 print(give_change(365), (0, 1, 1, 0, 1, 3))
 print(give_change(217), (2, 1, 1, 0, 0, 2))
