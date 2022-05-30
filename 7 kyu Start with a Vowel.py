8c2b502c88d221284ceec304623dfb1901867127
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:43:28 +0300

    3

--- 7 kyu Start with a Vowel.py
+++ 7 kyu Start with a Vowel.py
@@ -1,6 +1,13 @@
 def vowel_start(st):
+    vowels = 'ieauo'
     st = ''.join(i for i in st if i.isalpha() or i.isdigit()).lower()
-    return st
+    lst = []
+
+    for i in st:
+        if i in vowels:
+            lst.append(st)
+            st = st[st.find(i)::]
+    return lst
 
 
 print(vowel_start('It is beautiful weather today!'), 'it isb e a ut if ulw e ath ert od ay', )
@@ -8,3 +15,4 @@ print(vowel_start('Coding is great'), 'c od ing isgr e at', )
 print(vowel_start('my number is 0208-533-2325'), 'myn umb er is02085332325', )
 print(vowel_start('oranges, apples, melon, pineapple'), 'or ang es appl esm el onp in e appl e', )
 print(vowel_start('under_score'), 'und ersc or e')
+
