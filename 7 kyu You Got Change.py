b23d473c86a68f299696329afed66f1136af6338
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:21:54 +0300

    best solution

--- 7 kyu You Got Change.py
+++ 7 kyu You Got Change.py
@@ -3,23 +3,8 @@
 
 
 def give_change(amount):
-    ans = [0, 0, 0, 0, 0, 0]
-
-    while amount > 0:
-        ans[5] = amount // 100
-        amount -= ans[5] * 100
-        ans[4] = amount // 50
-        amount -= ans[4] * 50
-        ans[3] = amount // 20
-        amount -= ans[3] * 20
-        ans[2] = amount // 10
-        amount -= ans[2] * 10
-        ans[1] = amount // 5
-        amount -= ans[1] * 5
-        ans[0] = amount // 1
-        amount -= ans[0] * 1
-
-    return tuple(ans)
+    return (amount % 5, (amount % 10) // 5, ((amount % 50) % 20) // 10, (amount % 50) // 20, (amount % 100) // 50,
+            amount // 100)
 
 
 print(give_change(365), (0, 1, 1, 0, 1, 3))
