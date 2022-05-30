7b4c7c777c20239fc10686f3b11528a9ab2c5a4f
Paul <gafgafgarion05@gmail.com>
2022-05-30 12:28:50 +0300

    init

---
+++ 7 kyu Zebulan's Nightmare.py
@@ -0,0 +1,8 @@
+def zebulans_nightmare(function):
+    pass
+
+
+print(zebulans_nightmare('camel_case'), 'camelCase')
+print(zebulans_nightmare('zebulans_nightmare'), 'zebulansNightmare')
+print(zebulans_nightmare('get_string'), 'getString')
+print(zebulans_nightmare('convert_to_uppercase'), 'convertToUppercase')
