d0f7f28b25ec306858d09cf1a04c812e34e84534
Paul <gafgafgarion05@gmail.com>
2022-05-30 11:44:09 +0300

    best solution

--- 7 kyu Start with a Vowel.py
+++ 7 kyu Start with a Vowel.py
@@ -1,13 +1,8 @@
-def vowel_start(st):
-    vowels = 'ieauo'
-    st = ''.join(i for i in st if i.isalpha() or i.isdigit()).lower()
-    lst = []
+from re import sub
 
-    for i in st:
-        if i in vowels:
-            lst.append(st)
-            st = st[st.find(i)::]
-    return lst
+
+def vowel_start(st):
+    return sub(r'(?<=.)([aeiou])', r' \1', sub(r'[^a-z0-9]', '', st.lower()))
 
 
 print(vowel_start('It is beautiful weather today!'), 'it isb e a ut if ulw e ath ert od ay', )
@@ -15,4 +10,3 @@ print(vowel_start('Coding is great'), 'c od ing isgr e at', )
 print(vowel_start('my number is 0208-533-2325'), 'myn umb er is02085332325', )
 print(vowel_start('oranges, apples, melon, pineapple'), 'or ang es appl esm el onp in e appl e', )
 print(vowel_start('under_score'), 'und ersc or e')
-
