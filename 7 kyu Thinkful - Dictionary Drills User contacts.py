141f955d9585e790b60ce94402d60e05b1b8569d
Paul <gafgafgarion05@gmail.com>
2022-05-31 09:22:54 +0300

    init

---
+++ 7 kyu Thinkful - Dictionary Drills User contacts.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - Dictionary Drills: User contacts
+
+
+def user_contacts(data):
+    pass
+
+
+data = [["Grae Drake", 98110], ["Bethany Kok"], ["Alex Nussbacher", 94101], ["Darrell Silver", 11201]]
+expected_result = {'Grae Drake': 98110, 'Darrell Silver': 11201, 'Alex Nussbacher': 94101, 'Bethany Kok': None}
+print(user_contacts(data), expected_result)
