151b2a29cccd8e22f6de33fd7d82d7e7e3b9a770
Paul <gafgafgarion05@gmail.com>
2022-05-31 09:54:09 +0300

    init

---
+++ 7 kyu Driving School Series #1.py
@@ -0,0 +1,8 @@
+def passed(lst):
+    pass
+
+
+print(passed([10, 10, 10, 18, 20, 20]), 12)
+print(passed([21, 22, 24]), 'No pass scores registered.')
+print(passed([3, 22, 9, 13, 20, 18, 2, 14, 20, 8, 23, 12, 7, 21, 21, 19, 20, 11, 18, 7, 13, 22, 11, 20, 9]), 10)
+print(passed([19, 16, 8, 11, 25, 10, 29, 22, 23]), 11)
