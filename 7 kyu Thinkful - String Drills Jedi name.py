de4796647708b2b3ca2b45a1112ba4cfce3fa21a
Paul <gafgafgarion05@gmail.com>
2022-05-31 09:43:41 +0300

    init

---
+++ 7 kyu Thinkful - String Drills Jedi name.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - String Drills: Jedi name
+
+
+def greet_jedi(first, last):
+    pass
+
+
+print(greet_jedi('Beyonce', 'Knowles'), 'Greetings, master KnoBe')
+print(greet_jedi('Chris', 'Angelico'), 'Greetings, master AngCh')
+print(greet_jedi('grae', 'drake'), 'Greetings, master DraGr')
