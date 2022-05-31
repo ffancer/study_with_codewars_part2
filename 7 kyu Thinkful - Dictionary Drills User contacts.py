d00705f5c7f207a26b6ee0363ddb4b049466c80d
Paul <gafgafgarion05@gmail.com>
2022-05-31 09:24:42 +0300

    1

--- 7 kyu Thinkful - Dictionary Drills User contacts.py
+++ 7 kyu Thinkful - Dictionary Drills User contacts.py
@@ -3,8 +3,9 @@
 
 
 def user_contacts(data):
-    pass
-
+    dct = {}
+    for i in data:
+        print(type(i))
 
 data = [["Grae Drake", 98110], ["Bethany Kok"], ["Alex Nussbacher", 94101], ["Darrell Silver", 11201]]
 expected_result = {'Grae Drake': 98110, 'Darrell Silver': 11201, 'Alex Nussbacher': 94101, 'Bethany Kok': None}
