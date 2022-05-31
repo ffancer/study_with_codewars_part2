871413abb9db18efaf7424344c863859ffdcf162
Paul <gafgafgarion05@gmail.com>
2022-05-31 09:29:28 +0300

    my 1 line solution

--- 7 kyu Thinkful - Dictionary Drills User contacts.py
+++ 7 kyu Thinkful - Dictionary Drills User contacts.py
@@ -3,9 +3,8 @@
 
 
 def user_contacts(data):
-    dct = {}
-    for i in data:
-        print(type(i))
+    return {i[0]: (i[1] if len(i) == 2 else None) for i in data}
+
 
 data = [["Grae Drake", 98110], ["Bethany Kok"], ["Alex Nussbacher", 94101], ["Darrell Silver", 11201]]
 expected_result = {'Grae Drake': 98110, 'Darrell Silver': 11201, 'Alex Nussbacher': 94101, 'Bethany Kok': None}
