bfcec67e3168545c80092d96a7147a97b4a241ca
Paul <gafgafgarion05@gmail.com>
2022-06-01 13:14:50 +0300

    my solution

--- 7 kyu Thinkful - Logic Drills Graceful addition.py
+++ 7 kyu Thinkful - Logic Drills Graceful addition.py
@@ -6,7 +6,8 @@ def my_add(a, b):
     try:
         return a + b
     except:
-        return 'fff'
+        return None
+
 
 print(my_add(1, 3.414), 4.414)
 print(my_add(42, " is the answer."), None)
