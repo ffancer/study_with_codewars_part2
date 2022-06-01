2e7865b753441b72ce8776dd58afd0eec1dc18c8
Paul <gafgafgarion05@gmail.com>
2022-06-01 11:13:23 +0300

    1

--- 7 kyu The dropWhile Function.py
+++ 7 kyu The dropWhile Function.py
@@ -1,5 +1,11 @@
 def drop_while(arr, pred):
-    pass
+    def is_even(arr):
+        return [i % 2 != 0 for i in arr]
+    def is_odd(arr):
+        return [i % 2 != 0 for i in arr]
+    if pred == is_even:
+        return is_even(arr)
+
 
 
 print(drop_while([2, 6, 4, 10, 1, 5, 4, 3], is_even), [1, 5, 4, 3])
