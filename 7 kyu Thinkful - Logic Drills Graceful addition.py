ccc5c03e47db9e365219cceeca14fc85552e2467
Paul <gafgafgarion05@gmail.com>
2022-06-01 13:12:20 +0300

    init

---
+++ 7 kyu Thinkful - Logic Drills Graceful addition.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Thinkful - Logic Drills: Graceful addition
+
+
+def my_add(a, b):
+    pass
+
+
+print(my_add(1, 3.414), 4.414)
+print(my_add(42, " is the answer."), None)
+print(my_add(10, "2"), None)
