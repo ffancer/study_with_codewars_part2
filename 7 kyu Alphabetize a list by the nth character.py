aa7d312af00a5e4d2e36dd30fe9b49f38861c237
Paul <gafgafgarion05@gmail.com>
2022-06-01 11:46:15 +0300

    init

---
+++ 7 kyu Alphabetize a list by the nth character.py
@@ -0,0 +1,6 @@
+def sort_it(list_, n):
+    pass
+
+
+print(sort_it('bill, bell, ball, bull', 2), 'ball, bell, bill, bull', 'Sort by the second letter')
+print(sort_it('cat, dog, eel, bee', 3), 'bee, dog, eel, cat', 'Sort by the third letter')
