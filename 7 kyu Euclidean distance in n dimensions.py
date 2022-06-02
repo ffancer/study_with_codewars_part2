518d24fb70a66a8ae1d7e907b3fe3ddee7f2e42e
Paul <gafgafgarion05@gmail.com>
2022-06-02 09:50:48 +0300

    init

---
+++ 7 kyu Euclidean distance in n dimensions.py
@@ -0,0 +1,15 @@
+def euclidean_distance(point1, point2):
+    return  # the distance between the two points, rounded to two decimal places
+
+
+point1 = (-1,)
+point2 = (2,)
+print(euclidean_distance(point1, point2), 3.0)
+
+point1 = (-1, 2)
+point2 = (2, 4)
+print(euclidean_distance(point1, point2), 3.61)
+
+point1 = (-1, 2, 5)
+point2 = (2, 4, 9)
+print(euclidean_distance(point1, point2), 5.39)
