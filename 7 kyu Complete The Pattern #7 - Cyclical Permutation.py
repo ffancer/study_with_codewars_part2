1426120a26a4add8126db10949f34188dcd7ca2f
Paul <gafgafgarion05@gmail.com>
2022-06-02 08:04:02 +0300

    init

---
+++ 7 kyu Complete The Pattern #7 - Cyclical Permutation.py
@@ -0,0 +1,9 @@
+def pattern(n):
+    pass
+
+
+print(pattern(7), "1234567\n2345671\n3456712\n4567123\n5671234\n6712345\n7123456")
+print(pattern(1), "1")
+print(pattern(4), "1234\n2341\n3412\n4123")
+print(pattern(0), "")
+print(pattern(-25), "")
