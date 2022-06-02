3d130cb4a3b51930e7b38b86dd99e66d699bdcd2
Paul <gafgafgarion05@gmail.com>
2022-06-02 10:10:51 +0300

    init

---
+++ 7 kyu ⚠️Fusion Chamber Shutdown⚠️.py
@@ -0,0 +1,10 @@
+# Make sure you follow the order of reaction
+# output should be H2O,CO2,CH4
+def burner(c, h, o):
+    # code here
+    return water, co2, methane
+
+
+print(burner(45, 11, 100), (5, 45, 0))
+print(burner(354, 1023230, 0), (0, 0, 354))
+print(burner(939, 3, 694), (1, 346, 0))
