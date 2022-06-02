67ff7ca552c27478f18236adc8ddac8cdeb8d7fa
Paul <gafgafgarion05@gmail.com>
2022-06-02 08:37:49 +0300

    2

--- 7 kyu Common Substrings.py
+++ 7 kyu Common Substrings.py
@@ -1,22 +1,31 @@
 def substring_test(s1, s2):
-    return list(s1)
+    flag_1, flag_2 = True, True
+    for i in list(s1):
+        if i not in list(s2):
+            flag_1 = False
+
+    for j in list(s2):
+        if j not in list(s1):
+            flag_2 = False
+
+    return flag_1, flag_2
 
 
-print(substring_test("Something","Home"), True)
-print(substring_test("Something","Fun"), False)
-print(substring_test("Something",""), False)
-print(substring_test("","Something"), False)
-print(substring_test("BANANA","banana"), True)
-print(substring_test("test","lllt"), False)
-print(substring_test("",""), False)
-print(substring_test("1234567","541265"), True)
-print(substring_test("supercalifragilisticexpialidocious","SoundOfItIsAtrocious"), True)
+print(substring_test("Something", "Home"), True)
+print(substring_test("Something", "Fun"), False)
+print(substring_test("Something", ""), False)
+print(substring_test("", "Something"), False)
+print(substring_test("BANANA", "banana"), True)
+print(substring_test("test", "lllt"), False)
+print(substring_test("", ""), False)
+print(substring_test("1234567", "541265"), True)
+print(substring_test("supercalifragilisticexpialidocious", "SoundOfItIsAtrocious"), True)
 print(substring_test("LoremipsumdolorsitametconsecteturadipiscingelitAeneannonal"
-                                  "iquetligulautplaceratorciSuspendissepotentiMorbivolutpatau"
-                                  "ctoripsumegetaliquamPhasellusidmagnaelitNullamerostelluste"
-                                  "mporquismolestieaornarevitaediamNullaaliquamrisusnonviverr"
-                                  "asagittisInlaoreetultricespretiumVestibulumegetnullatincid"
-                                  "untsempersemacrutrumfelisPraesentpurusarcutempusnecvariusi"
-                                  "dultricesaduiPellentesqueultriciesjustolobortisrhoncusdign"
-                                  "issimNuncviverraconsequatblanditUtbibendumatlacusactristiq"
-                                  "ueAliquamimperdietnuncsempertortorefficiturviverra","thisisalongstringtest"), True)
+                     "iquetligulautplaceratorciSuspendissepotentiMorbivolutpatau"
+                     "ctoripsumegetaliquamPhasellusidmagnaelitNullamerostelluste"
+                     "mporquismolestieaornarevitaediamNullaaliquamrisusnonviverr"
+                     "asagittisInlaoreetultricespretiumVestibulumegetnullatincid"
+                     "untsempersemacrutrumfelisPraesentpurusarcutempusnecvariusi"
+                     "dultricesaduiPellentesqueultriciesjustolobortisrhoncusdign"
+                     "issimNuncviverraconsequatblanditUtbibendumatlacusactristiq"
+                     "ueAliquamimperdietnuncsempertortorefficiturviverra", "thisisalongstringtest"), True)
