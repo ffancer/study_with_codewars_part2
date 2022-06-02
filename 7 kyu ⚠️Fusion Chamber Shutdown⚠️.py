6d296bf49b6de57c10b4984f9cba8be7db5a3dc4
Paul <gafgafgarion05@gmail.com>
2022-06-02 10:18:05 +0300

    my solution

--- 7 kyu ⚠️Fusion Chamber Shutdown⚠️.py
+++ 7 kyu ⚠️Fusion Chamber Shutdown⚠️.py
@@ -1,8 +1,27 @@
 # Make sure you follow the order of reaction
 # output should be H2O,CO2,CH4
 def burner(c, h, o):
-    # code here
-    return water, co2, methane
+    water = 0
+    carbon_dioxide = 0
+    methane = 0
+
+    while h > 1 and o > 0:
+        h -= 2
+        o -= 1
+        water += 1
+
+    while c > 0 and o > 1:
+        c -= 1
+        o -= 2
+        carbon_dioxide += 1
+
+    while c > 0 and h > 3:
+        c -= 1
+        h -= 4
+        methane += 1
+
+
+    return water, carbon_dioxide, methane
 
 
 print(burner(45, 11, 100), (5, 45, 0))
