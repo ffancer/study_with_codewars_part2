d1cac2617758bedfb99982d123ba12b161ba31f7
Paul <gafgafgarion05@gmail.com>
2022-06-02 10:04:25 +0300

    init

---
+++ 7 kyu Reducing Problems - Bug Fixing #8.py
@@ -0,0 +1,11 @@
+def calculate_total(t1, t2):
+    t1s = reduce(lambda a, b: a ^ b, t1, 0)
+    t2s = reduce(lambda a, b: a & b, t2, 0)
+    return t1s > t2s
+
+
+print(calculate_total([1, 2, 2], [1, 0, 0]), True, 'Calculate the Winner!')
+print(calculate_total([6, 45, 1], [1, 55, 0]), False, 'Calculate the Winner!')
+print(calculate_total([57, 2, 1], []), True, 'Calculate the Winner!')
+print(calculate_total([], [3, 4, 3]), False, 'Calculate the Winner!')
+print(calculate_total([], []), False, 'Calculate the Winner!')
