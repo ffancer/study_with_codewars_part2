1d7411207ea7e0251d220981e6b1a194d9c8cdff
Paul <gafgafgarion05@gmail.com>
2022-06-02 08:24:56 +0300

    init

---
+++ 7 kyu Excel sheet column numbers.py
@@ -0,0 +1,9 @@
+def title_to_number(title):
+    pass
+
+
+print(title_to_number('A'), 1)
+print(title_to_number('Z'), 26)
+print(title_to_number('AA'), 27)
+print(title_to_number('AZ'), 52)
+print(title_to_number('BA'), 53)
