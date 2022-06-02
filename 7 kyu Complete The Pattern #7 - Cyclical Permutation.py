729538fcfd20b31689ff7400b9439bf1a0af9088
Paul <gafgafgarion05@gmail.com>
2022-06-02 08:16:09 +0300

    my solution

--- 7 kyu Complete The Pattern #7 - Cyclical Permutation.py
+++ 7 kyu Complete The Pattern #7 - Cyclical Permutation.py
@@ -1,5 +1,18 @@
 def pattern(n):
-    pass
+    s = ""
+
+    if n == 0 or not n:
+        return ""
+
+    for i in range(1, n + 1):
+        if i != 1:
+            s += '\n'
+        for j in range(i, n + 1):
+            s += str(j)
+        for j in range(1, i):
+            s += str(j)
+
+    return s
 
 
 print(pattern(7), "1234567\n2345671\n3456712\n4567123\n5671234\n6712345\n7123456")
