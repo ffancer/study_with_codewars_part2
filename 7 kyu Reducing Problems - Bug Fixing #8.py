316fb9ec34c870385cefb4ecc02eb141e77daefb
Paul <gafgafgarion05@gmail.com>
2022-06-02 10:07:14 +0300

    my solution

--- 7 kyu Reducing Problems - Bug Fixing #8.py
+++ 7 kyu Reducing Problems - Bug Fixing #8.py
@@ -1,9 +1,6 @@
-from functools import reduce
-
-
 def calculate_total(t1, t2):
-    t1s = reduce(lambda a, b: a ^ b, t1, 0)
-    t2s = reduce(lambda a, b: a & b, t2, 0)
+    t1s = sum(t1)
+    t2s = sum(t2)
     return t1s > t2s
 
 
