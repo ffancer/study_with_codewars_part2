3df044e31d2704478a988232be45464a305284d4
Paul <gafgafgarion05@gmail.com>
2022-06-03 11:15:59 +0300

    init

---
+++ 7 kyu Complete The Pattern #5 - Even Ladder.py
@@ -0,0 +1,10 @@
+def pattern(n):
+    pass
+
+
+print(pattern(2), "22")
+print(pattern(1), "")
+print(pattern(5), "22\n4444")
+print(pattern(6), "22\n4444\n666666")
+print(pattern(0), "")
+print(pattern(-25), "")
