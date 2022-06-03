613b46cfc62430aed86bec48f7f58234c8774909
Paul <gafgafgarion05@gmail.com>
2022-06-03 10:21:06 +0300

    init

---
+++ 7 kyu Character Concatenation.py
@@ -0,0 +1,6 @@
+def char_concat(word):
+    pass
+
+
+print(char_concat("abc def"), 'af1be2cd3', "Should work for example test")
+print(char_concat("CodeWars"), 'Cs1or2da3eW4', "Should work for 'CodeWars'")
