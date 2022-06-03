9649f1e4e2a9aa38514faee3fb9613665557381d
Paul <gafgafgarion05@gmail.com>
2022-06-03 10:29:10 +0300

    2 just 1 error

--- 7 kyu Character Concatenation.py
+++ 7 kyu Character Concatenation.py
@@ -1,4 +1,8 @@
 def char_concat(word):
+    if len(word) % 2 != 0:
+        word = word[:len(word)//2] + word[len(word)//2+1:]
+
+    word = ''.join(i for i in word if i != ' ')
     cnt = 1
     s = ''
 
@@ -14,3 +18,4 @@ def char_concat(word):
 
 print(char_concat("abc def"), 'af1be2cd3', "Should work for example test")
 print(char_concat("CodeWars"), 'Cs1or2da3eW4', "Should work for 'CodeWars'")
+print(char_concat("CodeWar"), 'Cs1or2da3eW4', "Should work for 'CodeWars'")
