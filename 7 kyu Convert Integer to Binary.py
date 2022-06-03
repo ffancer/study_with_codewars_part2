7fe0ff38f6151eb89420d12e63f6243754a53bf7
Paul <gafgafgarion05@gmail.com>
2022-06-03 10:33:06 +0300

    init

---
+++ 7 kyu Convert Integer to Binary.py
@@ -0,0 +1,9 @@
+def to_binary(n):
+    pass
+
+
+print(to_binary(2), "10")
+print(to_binary(3), "11")
+print(to_binary(4), "100")
+print(to_binary(-3), "11111111111111111111111111111101")
+print(to_binary(0), "0")
