ecfdb74c89078210ac7fbc9ea221c1644c8bd7c4
Paul <gafgafgarion05@gmail.com>
2022-06-03 11:43:16 +0300

    init

---
+++ 7 kyu Are there doubles.py
@@ -0,0 +1,14 @@
+# 7 kyu
+# Are there doubles?
+
+
+def double_check(strng):
+    pass
+
+
+print(double_check("abca"), False)
+print(double_check("aabc"), True)
+print(double_check("a 11 c d"), True)
+print(double_check("AabBcC"), True)
+print(double_check("a b  c"), True)
+print(double_check("a b c d e f g h i h k"), False)
