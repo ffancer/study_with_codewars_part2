89656ef7e1a2be0f097af6f6ca95e6e4d1995b35
Paul <gafgafgarion05@gmail.com>
2022-06-03 11:26:41 +0300

    init

---
+++ 7 kyu Ch4113ng3.py
@@ -0,0 +1,8 @@
+def nerdify(txt):
+    pass
+
+
+print(nerdify("Fundamentals"),"Fund4m3nt41s")
+print(nerdify("Seven"),"S3v3n")
+print(nerdify("Los Angeles"),"Los 4ng313s")
+print(nerdify("Seoijselawuue"),"S3oijs314wuu3")
