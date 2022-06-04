ea2fa7a4a2958fc6d80e88db6c33b7c1053cd96f
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:06:47 +0300

    my solution

--- 7 kyu Sum of differences between products and LCMs.py
+++ 7 kyu Sum of differences between products and LCMs.py
@@ -1,5 +1,15 @@
 def sum_differences_between_products_and_LCMs(pairs):
-    pass
+    ans = 0
+
+    for i, [a, b] in enumerate(pairs):
+        product = a * b
+        lcm = product
+        for j in range(product, max(a - 1, b - a, 1), -max(a, b, 1)):
+            if j % a == j % b == 0:
+                lcm = j
+        ans += product - lcm
+
+    return ans
 
 
 print(sum_differences_between_products_and_LCMs([[15, 18], [4, 5], [12, 60]]), 840)
