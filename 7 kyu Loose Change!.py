f3c9d5c9c7eaa8023151d1bf16f697e3b6b8b07c
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:52:43 +0300

    init

---
+++ 7 kyu Loose Change!.py
@@ -0,0 +1,14 @@
+# Remember you have a CHANGE dictionary to work with ;)
+CHANGE = {'penny': 0.01, 'nickel': 0.05, 'dime': 0.1, 'quarter': 0.25, 'dollar': 1.0}
+
+
+def change_count(change):
+    # Calculate Value Below!
+    return '$1.00'
+
+
+print(change_count('dime penny dollar'), '$1.11')
+print(change_count('dime penny nickel'), '$0.16')
+print(change_count('quarter quarter'), '$0.50')
+print(change_count('dollar penny dollar'), '$2.01')
+print(change_count('dollar dollar dollar dollar dollar dollar dollar dollar dollar dollar penny'), '$10.01')
