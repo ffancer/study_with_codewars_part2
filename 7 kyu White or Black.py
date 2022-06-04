1cc027d86a6f3a3b3c3293c76f56af02bd599828
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:19:37 +0300

    init

---
+++ 7 kyu White or Black.py
@@ -0,0 +1,10 @@
+# 7 kyu
+# White or Black?
+
+def square_color(file, rank):
+    return 'white'
+
+
+print(square_color("a", 8), "white")
+print(square_color("b", 2), "black")
+print(square_color("f", 5), "white")
