f72d97d77852c328906c25badbf7e6ac796aa19e
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:17:05 +0300

    my solution

--- 7 kyu Negation of a Value.py
+++ 7 kyu Negation of a Value.py
@@ -1,9 +1,12 @@
 def negation_value(s, val):
-    if '!' in s and val is True:
-        return False
-    if '!' in s and val is False:
-        return True
+    if not s:
+        return bool(val)
 
+    while s:
+        val = not val
+        s = s[1:]
+
+    return val
 
 
 print(negation_value("!", False), True)
