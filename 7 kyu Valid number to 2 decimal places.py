58218b92f0445ac09922d569dc69b425b011a79d
Paul <gafgafgarion05@gmail.com>
2022-06-04 11:32:24 +0300

    init

---
+++ 7 kyu Valid number to 2 decimal places.py
@@ -0,0 +1,16 @@
+def valid_number(n):
+    pass
+
+
+print(valid_number("0.00"), True)
+print(valid_number(".00"), True)
+print(valid_number("-.00"), True)
+print(valid_number(".30"), True)
+print(valid_number("0.40"), True)
+print(valid_number("34443.33"), True)
+print(valid_number(".0a"), False)
+print(valid_number("1.00."), False)
+print(valid_number(".+00"), False)
+print(valid_number("w.00"), False)
+print(valid_number("..00"), False)
+print(valid_number("1,00"), False)
