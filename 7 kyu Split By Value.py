9826db84bd85440135d9d454427b2204129405b8
Paul <gafgafgarion05@gmail.com>
2022-06-04 11:41:02 +0300

    init

---
+++ 7 kyu Split By Value.py
@@ -0,0 +1,7 @@
+def split_by_value(k, elements):
+    pass
+
+
+print(split_by_value(5, [1, 3, 5, 7, 6, 4, 2]), [1, 3, 4, 2, 5, 7, 6])
+print(split_by_value(0, [5, 2, 7, 3, 2]), [5, 2, 7, 3, 2])
+print(split_by_value(6, [6, 4, 10, 10, 6]), [4, 6, 10, 10, 6])
