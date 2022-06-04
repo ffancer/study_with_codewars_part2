6df70025290156ffcba231114f926b293498101a
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:30:16 +0300

    init

---
+++ 7 kyu Supernatural.py
@@ -0,0 +1,7 @@
+def bob(what):
+    return "Huh?"
+
+
+print(bob('vampire'), 'Behead it with a machete, idjits!')
+print(bob('pagan god'), 'It depends on which one it is, idjits!')
+print(bob('werepuppy'), 'I have friggin no idea yet, idjits!')
