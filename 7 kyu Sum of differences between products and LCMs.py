80b26a2725b60ba05ba2f1f31edf3b6f4b26687e
Paul <gafgafgarion05@gmail.com>
2022-06-04 10:04:49 +0300

    init

---
+++ 7 kyu Sum of differences between products and LCMs.py
@@ -0,0 +1,9 @@
+def sum_differences_between_products_and_LCMs(pairs):
+    pass
+
+
+print(sum_differences_between_products_and_LCMs([[15, 18], [4, 5], [12, 60]]), 840)
+print(sum_differences_between_products_and_LCMs([[1, 1], [0, 0], [13, 91]]), 1092)
+print(sum_differences_between_products_and_LCMs([[15, 7], [4, 5], [19, 60]]), 0)
+print(sum_differences_between_products_and_LCMs([[20, 50], [10, 10], [50, 20]]), 1890)
+print(sum_differences_between_products_and_LCMs([]), 0)
