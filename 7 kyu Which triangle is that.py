b2b99c8cef9a215b68c74e9b74d006b61d7cc2c1
Paul <gafgafgarion05@gmail.com>
2022-06-06 10:25:59 +0300

    init

---
+++ 7 kyu Which triangle is that.py
@@ -0,0 +1,13 @@
+# 7 kyu
+# Which triangle is that?
+
+
+def type_of_triangle(a, b, c):
+    # your code here
+    pass
+
+
+print(type_of_triangle(1, 1, 1), "Equilateral")
+print(type_of_triangle(3, 2, 4), "Scalene")
+print(type_of_triangle(2, 2, 1), "Isosceles")
+print(type_of_triangle('.', 5, 82), "Not a valid triangle")
