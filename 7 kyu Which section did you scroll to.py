0bfc62caf9d0f597419793d28c4c65dfc0106645
Paul <gafgafgarion05@gmail.com>
2022-06-06 08:57:43 +0300

    init

---
+++ 7 kyu Which section did you scroll to.py
@@ -0,0 +1,11 @@
+# 7 kyu
+# Which section did you scroll to?
+
+
+def get_section_id(scroll, sizes):
+    return
+
+
+print(get_section_id(299, [300, 200, 400, 600, 100]), 0)
+print(get_section_id(300, [300, 200, 400, 600, 100]), 1)
+print(get_section_id(1600, [300, 200, 400, 600, 100]), -1)
