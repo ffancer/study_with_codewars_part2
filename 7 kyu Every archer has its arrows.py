e7e856f8ea959d135915642b477cbe35a766aacc
Paul <gafgafgarion05@gmail.com>
2022-06-06 09:14:03 +0300

    my solution

--- 7 kyu Every archer has its arrows.py
+++ 7 kyu Every archer has its arrows.py
@@ -1,7 +1,14 @@
 def archers_ready(archers):
-    pass
+    if not archers:
+        return False
+
+    for i in archers:
+        if i < 5:
+            return False
+    return True
 
 
 print(archers_ready([]), False, "Should handle no archers")
 print(archers_ready([1, 2, 3, 4]), False, "Should handle unprepared archers")
 print(archers_ready([5, 6, 7, 8]), True, "Should handle prepared archers")
+print(archers_ready([17, 15, 12, 7, 2, 12, 13, 15, 7, 11, 10, 5]), False)
