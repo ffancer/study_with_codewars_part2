d68e74e3708cee38c2f366e8c5fe7101d9a4a9d9
Paul <gafgafgarion05@gmail.com>
2022-06-06 10:41:25 +0300

    init

---
+++ 7 kyu Sum of squares less than some number.py
@@ -0,0 +1,9 @@
+def get_number_of_squares(n):
+    pass
+
+
+print(get_number_of_squares(1))
+print(get_number_of_squares(2))
+print(get_number_of_squares(5))
+print(get_number_of_squares(6))
+print(get_number_of_squares(15))
