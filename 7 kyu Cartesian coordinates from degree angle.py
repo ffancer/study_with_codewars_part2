9e3b7df59766a49c984905df43b072bc0c5ce483
Paul <gafgafgarion05@gmail.com>
2022-06-06 08:53:42 +0300

    my solution

--- 7 kyu Cartesian coordinates from degree angle.py
+++ 7 kyu Cartesian coordinates from degree angle.py
@@ -4,6 +4,8 @@ import math
 def coordinates(degrees, radius):
     x = math.cos(degrees * math.pi / 180) * radius
     y = math.sin(degrees * math.pi / 180) * radius
-    return x, y
+
+    return round(x, 10), round(y, 10)
+
 
 print(coordinates(90, 1), (0.0, 1.0))
