3e41f7a12f5c3dde12a032384ddd4a9285a1fb34
Paul <gafgafgarion05@gmail.com>
2022-06-06 08:52:25 +0300

    1

--- 7 kyu Cartesian coordinates from degree angle.py
+++ 7 kyu Cartesian coordinates from degree angle.py
@@ -1,5 +1,9 @@
-def coordinates(degrees, radius):
-    pass
+import math
 
+
+def coordinates(degrees, radius):
+    x = math.cos(degrees * math.pi / 180) * radius
+    y = math.sin(degrees * math.pi / 180) * radius
+    return x, y
 
 print(coordinates(90, 1), (0.0, 1.0))
