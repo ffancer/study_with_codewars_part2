8cc7c29765afab4d4c4d353616b509a8bb211d3f
Paul <gafgafgarion05@gmail.com>
2022-06-06 09:19:35 +0300

    1

--- 7 kyu How sexy is your name.py
+++ 7 kyu How sexy is your name.py
@@ -2,9 +2,15 @@
 # How sexy is your name?
 
 
+SCORES = {'A': 100, 'B': 14, 'C': 9, 'D': 28, 'E': 145, 'F': 12, 'G': 3,
+          'H': 10, 'I': 200, 'J': 100, 'K': 114, 'L': 100, 'M': 25,
+          'N': 450, 'O': 80, 'P': 2, 'Q': 12, 'R': 400, 'S': 113, 'T': 405,
+          'U': 11, 'V': 10, 'W': 10, 'X': 3, 'Y': 210, 'Z': 23}
+
+
 def sexy_name(name):
-    # calculate the sexy name score and return the sexy score ranking
-    pass
+    name = ''.join(i.upper() for i in name)
+    return name
 
 
 print(sexy_name('GUV'), 'NOT TOO SEXY')
