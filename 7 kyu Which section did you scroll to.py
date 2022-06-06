efeb1db2226313b8e2a2bed5b290841e05cb0773
Paul <gafgafgarion05@gmail.com>
2022-06-06 09:05:05 +0300

    my solution

--- 7 kyu Which section did you scroll to.py
+++ 7 kyu Which section did you scroll to.py
@@ -4,14 +4,14 @@
 
 def get_section_id(scroll, sizes):
     total = 0
-    for i in range(len(sizes)):
-        if total - 1 < len(sizes):
-            return i - 1
+
+    for i, size in enumerate(sizes):
+        total += size
+        if scroll >= total:
+            continue
         else:
-            total += sizes[i]
-        if total - 1 >= scroll:
             return i
-    # return -1
+    return -1
 
 
 print(get_section_id(299, [300, 200, 400, 600, 100]), 0)
