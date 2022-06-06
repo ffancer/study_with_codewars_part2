e570b8ba15ef9beffb7dfebbea5f6201f94ce819
Paul <gafgafgarion05@gmail.com>
2022-06-06 09:17:59 +0300

    init

---
+++ 7 kyu How sexy is your name.py
@@ -0,0 +1,31 @@
+# 7 kyu
+# How sexy is your name?
+
+
+def sexy_name(name):
+    # calculate the sexy name score and return the sexy score ranking
+    pass
+
+
+print(sexy_name('GUV'), 'NOT TOO SEXY')
+print(sexy_name('PHUG'), "NOT TOO SEXY")
+print(sexy_name('FFFFF'), 'NOT TOO SEXY')
+print(sexy_name(''), "NOT TOO SEXY")
+print(sexy_name('PHUG'), "NOT TOO SEXY")
+print(sexy_name('BOB'), "PRETTY SEXY")
+print(sexy_name('JLJ'), 'PRETTY SEXY')
+print(sexy_name('HHHHHU'), 'PRETTY SEXY')
+print(sexy_name('BOB'), "PRETTY SEXY")
+print(sexy_name('WWWWWU'), "PRETTY SEXY")
+print(sexy_name('YOU'), 'VERY SEXY')
+print(sexy_name('FABIO'), "VERY SEXY")
+print(sexy_name('ARUUUUUUUUU'), 'VERY SEXY')
+print(sexy_name('ROBBY'), 'THE ULTIMATE SEXIEST')
+print(sexy_name('SAMANTHA'), 'THE ULTIMATE SEXIEST')
+print(sexy_name('DONALD TRUMP'), "THE ULTIMATE SEXIEST")
+print(sexy_name('BILL GATES'), "THE ULTIMATE SEXIEST")
+print(sexy_name('SCARLETT JOHANSSON'), "THE ULTIMATE SEXIEST")
+print(sexy_name('CODEWARS'), "THE ULTIMATE SEXIEST")
+print(sexy_name('PAMELA ANDERSON'), "THE ULTIMATE SEXIEST")
+print(sexy_name('you'), 'VERY SEXY')
+print(sexy_name('Codewars'), "THE ULTIMATE SEXIEST")
