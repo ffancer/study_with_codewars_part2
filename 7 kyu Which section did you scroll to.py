f56453a9c0a51d0249f6ec6dfc6a0f23d4025bc0
Paul <gafgafgarion05@gmail.com>
2022-06-06 09:02:45 +0300

    1

--- 7 kyu Which section did you scroll to.py
+++ 7 kyu Which section did you scroll to.py
@@ -3,7 +3,15 @@
 
 
 def get_section_id(scroll, sizes):
-    return
+    total = 0
+    for i in range(len(sizes)):
+        if total - 1 < len(sizes):
+            return i - 1
+        else:
+            total += sizes[i]
+        if total - 1 >= scroll:
+            return i
+    # return -1
 
 
 print(get_section_id(299, [300, 200, 400, 600, 100]), 0)
