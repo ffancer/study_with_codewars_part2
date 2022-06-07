9410ee46e8693606086acffcc99fb4dd8c0b007c
Paul <gafgafgarion05@gmail.com>
2022-06-07 08:18:00 +0300

    init

---
+++ 6 kyu The Supermarket Queue.py
@@ -0,0 +1,10 @@
+def queue_time(customers, n):
+    pass
+
+
+print(queue_time([], 1), 0, "wrong answer for case with an empty queue")
+print(queue_time([5], 1), 5, "wrong answer for a single person in the queue")
+print(queue_time([2], 5), 2, "wrong answer for a single person in the queue")
+print(queue_time([1, 2, 3, 4, 5], 1), 15, "wrong answer for a single till")
+print(queue_time([1, 2, 3, 4, 5], 100), 5, "wrong answer for a case with a large number of tills")
+print(queue_time([2, 2, 3, 3, 4, 4], 2), 9, "wrong answer for a case with two tills")
