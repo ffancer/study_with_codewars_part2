a455bbf6f4882acfb5df0bdbb478b4e7b00ca227
Paul <gafgafgarion05@gmail.com>
2022-06-07 09:29:35 +0300

    init

---
+++ 6 kyu Roman Numerals Encoder.py
@@ -0,0 +1,15 @@
+def solution(n):
+    pass
+
+
+print(solution(1), 'I', "solution(1),'I'")
+print(solution(4), 'IV', "solution(4),'IV'")
+print(solution(6), 'VI', "solution(6),'VI'")
+print(solution(14), 'XIV', "solution(14),'XIV")
+print(solution(21), 'XXI', "solution(21),'XXI'")
+print(solution(89), 'LXXXIX', "solution(89),'LXXXIX'")
+print(solution(91), 'XCI', "solution(91),'XCI'")
+print(solution(984), 'CMLXXXIV', "solution(984),'CMLXXXIV'")
+print(solution(1000), 'M', 'solution(1000), M')
+print(solution(1889), 'MDCCCLXXXIX', "solution(1889),'MDCCCLXXXIX'")
+print(solution(1989), 'MCMLXXXIX', "solution(1989),'MCMLXXXIX'")
