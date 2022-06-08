fc92ccd2e43ad0baacdef068468bc6cd1e8632bc
Paul <gafgafgarion05@gmail.com>
2022-06-08 10:37:33 +0300

    my solution

--- 6 kyu Tortoise racing.py
+++ 6 kyu Tortoise racing.py
@@ -1,6 +1,14 @@
 def race(v1, v2, g):
     if v1 >= v2:
         return None
+    else:
+        time = g * 3600 / (v2 - v1)
+        h = int(time / 3600)
+        m = int((time % 3600) / 60)
+        s = int(time % 3600 % 60)
+        time = [h, m, s]
+
+    return time
 
 
 print(race(720, 850, 70), [0, 32, 18])
