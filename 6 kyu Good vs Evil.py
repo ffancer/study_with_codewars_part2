016104e0080e5879f67a2fd00b380c0c501a0222
Paul <gafgafgarion05@gmail.com>
2022-06-08 10:13:56 +0300

    init

---
+++ 6 kyu Good vs Evil.py
@@ -0,0 +1,7 @@
+def good_vs_evil(good, evil):
+    return
+
+
+print(good_vs_evil('1 1 1 1 1 1', '1 1 1 1 1 1 1'), 'Battle Result: Evil eradicates all trace of Good')
+print(good_vs_evil('0 0 0 0 0 10', '0 1 1 1 1 0 0'), 'Battle Result: Good triumphs over Evil')
+print(good_vs_evil('1 0 0 0 0 0', '1 0 0 0 0 0 0'), 'Battle Result: No victor on this battle field')
