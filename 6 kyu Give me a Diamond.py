7eed6230ac897c7cd82ac0993e299a352778581a
Paul <gafgafgarion05@gmail.com>
2022-06-08 08:52:44 +0300

    2

--- 6 kyu Give me a Diamond.py
+++ 6 kyu Give me a Diamond.py
@@ -1,18 +1,20 @@
 def diamond(n):
     up_s, down_s, mid_s = '', '', n * '*'
 
-    for i in range(1, n, 1):
-        up_s += '*' * i + '\n'
+    for i in range(1, n, 2):
+        up_s += " " * ((n - i) // 2) + "*" * i + "\n"
 
     return up_s
 
 
-expected = " *\n"
-expected += "***\n"
-expected += " *\n"
-print(diamond(1), "*\n")
-print(diamond(2), None)
-print(diamond(3), expected)
-print(diamond(5), "  *\n ***\n*****\n ***\n  *\n")
-print(diamond(0), None)
-print(diamond(-3), None)
+# expected = " *\n"
+# expected += "***\n"
+# expected += " *\n"
+# print(diamond(1), "*\n")
+# print(diamond(2), None)
+# print(diamond(3), expected)
+# print(diamond(5), "  *\n ***\n*****\n ***\n  *\n")
+# print(diamond(0), None)
+# print(diamond(-3), None)
+print(diamond(7))
+
