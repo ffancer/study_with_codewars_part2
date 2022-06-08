b4f9385565ca7fb4c4bea36229282d07d61fc753
Paul <gafgafgarion05@gmail.com>
2022-06-08 10:32:43 +0300

    my solution

--- 6 kyu Good vs Evil.py
+++ 6 kyu Good vs Evil.py
@@ -2,13 +2,11 @@ def good_vs_evil(good, evil):
     good_values = (1, 2, 3, 3, 4, 10)
     evil_values = (1, 2, 2, 2, 3, 5, 10)
 
-    total_good, total_evil = sum(i * int(j) for i, j in zip(good_values, good.split())), sum(int(i) for i in evil.split())
+    total_good, total_evil = sum(i * int(j) for i, j in zip(good_values, good.split())), sum(
+        i * int(j) for i, j in zip(evil_values, evil.split()))
 
-
-    return total_good
-    # return 'Battle Result: No victor on this battle field' if total_good == total_evil else \
-    #     'Battle Result: Evil eradicates all trace of Good' if total_evil > total_good else \
-    #         'Battle Result: Good triumphs over Evil'
+    return 'Battle Result: No victor on this battle field' if total_good == total_evil else \
+        'Battle Result: Evil eradicates all trace of Good' if total_evil > total_good else 'Battle Result: Good triumphs over Evil'
 
 
 print(good_vs_evil('1 1 1 1 1 1', '1 1 1 1 1 1 1'), 'Battle Result: Evil eradicates all trace of Good')
