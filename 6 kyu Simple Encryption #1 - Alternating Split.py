38f32cd65f21b045deda04fc675b44bb7377ad82
Paul <gafgafgarion05@gmail.com>
2022-06-08 09:02:09 +0300

    solution

--- 6 kyu Simple Encryption #1 - Alternating Split.py
+++ 6 kyu Simple Encryption #1 - Alternating Split.py
@@ -1,9 +1,32 @@
 def decrypt(encrypted_text, n):
-    pass
+    if encrypted_text is None:
+        return None
+
+    encrypt_lenght = len(encrypted_text)
+    second_chars = encrypted_text[:encrypt_lenght // 2]
+    other_chars = encrypted_text[encrypt_lenght // 2:]
+
+    if n <= 0:
+        return encrypted_text
+    else:
+        for pos in range(len(other_chars)):
+            encrypted_text += other_chars[pos] + second_chars[pos:pos + 1]
+        return decrypt(encrypted_text[encrypt_lenght:], n - 1)
 
 
 def encrypt(text, n):
-    pass
+    second_chars = ""
+    other_chars = ""
+
+    if n <= 0:
+        return text
+    else:
+        for pos in range(len(text)):
+            if pos % 2 != 0:
+                second_chars += text[pos]
+            else:
+                other_chars += text[pos]
+        return encrypt(second_chars + other_chars, n - 1)
 
 
 print(encrypt("This is a test!", 0), "This is a test!")
