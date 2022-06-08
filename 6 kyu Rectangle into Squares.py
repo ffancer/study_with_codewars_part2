dabfa6b46a2bd5fc81f845740fb251356fde9188
Paul <gafgafgarion05@gmail.com>
2022-06-08 10:50:42 +0300

    my solution

--- 6 kyu Rectangle into Squares.py
+++ 6 kyu Rectangle into Squares.py
@@ -1,6 +1,18 @@
+min_max = lambda x, y: (min(x, y), max(x, y))
+
+
 def sqInRect(lng, wdth):
-    # your code
-    return None
+    if lng == wdth:
+        return None
+    r, (minimum, maximum) = [], min_max(lng, wdth)
+
+    while True:
+        if minimum == 1:
+            r += [minimum] * maximum; return r
+        elif not maximum % minimum:
+            r += [minimum] * (maximum // minimum); return r
+        else:
+            r.append(minimum); (minimum, maximum) = min_max(minimum, maximum - minimum)
 
 
 print(sqInRect(5, 5), None)
