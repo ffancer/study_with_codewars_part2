d28eb5d75ded2bd758d67fc56ffa597818cd8960
Paul <gafgafgarion05@gmail.com>
2022-06-08 10:46:23 +0300

    init

---
+++ 6 kyu Rectangle into Squares.py
@@ -0,0 +1,9 @@
+def sqInRect(lng, wdth):
+    # your code
+    return None
+
+
+print(sqInRect(5, 5), None)
+print(sqInRect(5, 3), [3, 2, 1, 1])
+print(sqInRect(20, 14), [14, 6, 6, 2, 2, 2])
+print(sqInRect(37, 14), [14, 14, 9, 5, 4, 1, 1, 1, 1])
