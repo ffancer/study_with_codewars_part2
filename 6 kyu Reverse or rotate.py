677f7c658e347f070e8d11fbef9c2d2ac7f61c02
Paul <gafgafgarion05@gmail.com>
2022-06-09 07:54:07 +0300

    init

---
+++ 6 kyu Reverse or rotate.py
@@ -0,0 +1,13 @@
+# 6 kyu
+# Reverse or rotate?
+
+
+def revrot(strng, sz):
+    pass
+
+
+print(revrot("1234", 0), "")
+print(revrot("", 0), "")
+print(revrot("1234", 5), "")
+s = "733049910872815764"
+print(revrot(s, 5), "330479108928157")
