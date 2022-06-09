e8c7656c05775f1af85c665057324a9532abf70f
Paul <gafgafgarion05@gmail.com>
2022-06-09 08:40:03 +0300

    my solution

--- 6 kyu Buying a car.py
+++ 6 kyu Buying a car.py
@@ -1,16 +1,16 @@
 def nbMonths(start_price_old, start_price_new, saving_per_month, percent_loss_by_month):
     i = 0
-    saved_this_month = 0
+    saving = 0
 
-    while (start_price_old + saved_this_month) <= start_price_new:
+    while start_price_old + saving < start_price_new:
         i += 1
+        saving += saving_per_month
         if i % 2 == 0:
             percent_loss_by_month += 0.5
-        start_price_old *= 1 - (percent_loss_by_month / 100)
-        start_price_new *= 1 - (percent_loss_by_month / 100)
-        saved_this_month += saving_per_month
+        start_price_old = start_price_old - (start_price_old * percent_loss_by_month) / 100
+        start_price_new = start_price_new - (start_price_new * percent_loss_by_month) / 100
 
-    return [i, round(start_price_old + saved_this_month - start_price_new)]
+    return [i, round(start_price_old + saving - start_price_new)]
 
 
 print(nbMonths(2000, 8000, 1000, 1.5), [6, 766])
