55bc68cb89fe41cc9b7ecdf31cba8907e4721916
Paul <gafgafgarion05@gmail.com>
2022-06-09 08:33:12 +0300

    init

---
+++ 6 kyu Buying a car.py
@@ -0,0 +1,6 @@
+def nbMonths(start_price_old, start_price_new, saving_per_month, percent_loss_by_month):
+    return []
+
+
+print(nbMonths(2000, 8000, 1000, 1.5), [6, 766])
+print(nbMonths(12000, 8000, 1000, 1.5), [0, 4000])
