4e4836bc41b3eda0aa6ca5186dd85255f8ebbb72
Paul <gafgafgarion05@gmail.com>
2022-06-09 08:37:03 +0300

    1 3 failed tests

--- 6 kyu Buying a car.py
+++ 6 kyu Buying a car.py
@@ -1,5 +1,16 @@
 def nbMonths(start_price_old, start_price_new, saving_per_month, percent_loss_by_month):
-    return []
+    i = 0
+    saved_this_month = 0
+
+    while (start_price_old + saved_this_month) <= start_price_new:
+        i += 1
+        if i % 2 == 0:
+            percent_loss_by_month += 0.5
+        start_price_old *= 1 - (percent_loss_by_month / 100)
+        start_price_new *= 1 - (percent_loss_by_month / 100)
+        saved_this_month += saving_per_month
+
+    return [i, round(start_price_old + saved_this_month - start_price_new)]
 
 
 print(nbMonths(2000, 8000, 1000, 1.5), [6, 766])
