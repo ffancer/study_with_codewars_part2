3e58bcd705d9aa7f8e66e1706a0e61e30dadd8bb
Paul <gafgafgarion05@gmail.com>
2022-06-09 07:55:40 +0300

    best solution

--- 6 kyu Reverse or rotate.py
+++ 6 kyu Reverse or rotate.py
@@ -2,8 +2,19 @@
 # Reverse or rotate?
 
 
-def revrot(strng, sz):
-    pass
+def revrot(s, n, res=""):
+    if not s or n < 1 or n > len(s):
+        return ""
+
+    while len(s) >= n:
+        group = s[:n]
+        if sum([int(d) ** 3 for d in group]) % 2 == 0:
+            res += group[::-1]
+        else:
+            res += group[1:] + group[0]
+        s = s[n:]
+
+    return res
 
 
 print(revrot("1234", 0), "")
