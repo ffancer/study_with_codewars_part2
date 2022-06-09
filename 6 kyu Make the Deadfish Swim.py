4e3ab368e9064be4d958baa54cd8b8c188edab69
Paul <gafgafgarion05@gmail.com>
2022-06-09 07:37:15 +0300

    my solution

--- 6 kyu Make the Deadfish Swim.py
+++ 6 kyu Make the Deadfish Swim.py
@@ -1,5 +1,19 @@
 def parse(data):
-    return
+    lst = []
+    total = 0
+
+    for i in data:
+        if i in 'idso':
+            if i == 'i':
+                total += 1
+            if i == 'd':
+                total -= 1
+            if i == 's':
+                total **= 2
+            if i == 'o':
+                lst.append(total)
+
+    return lst
 
 
 print(parse("ooo"), [0, 0, 0])
