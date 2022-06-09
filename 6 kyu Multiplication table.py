f4980a42052dbfa8d6b2d695fa14056e5c579387
Paul <gafgafgarion05@gmail.com>
2022-06-09 08:31:34 +0300

    my solution

--- 6 kyu Multiplication table.py
+++ 6 kyu Multiplication table.py
@@ -1,9 +1,13 @@
 def multiplication_table(size):
     lst = []
 
-    for i in range(1, size ** 2 + 1):
-        lst.append(i)
+    for i in range(1, size + 1):
+        row = []
+        for j in range(1, size + 1):
+            row.append(i * j)
+        lst.append(row)
 
     return lst
+
 
 print(multiplication_table(3), [[1, 2, 3], [2, 4, 6], [3, 6, 9]])
