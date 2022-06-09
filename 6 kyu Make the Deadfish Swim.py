92251f6269a3db03099c5d3259302b622d56d86e
Paul <gafgafgarion05@gmail.com>
2022-06-09 07:28:20 +0300

    init

---
+++ 6 kyu Make the Deadfish Swim.py
@@ -0,0 +1,9 @@
+def parse(data):
+    return
+
+
+print(parse("ooo"), [0, 0, 0])
+print(parse("ioioio"), [1, 2, 3])
+print(parse("idoiido"), [0, 1])
+print(parse("isoisoiso"), [1, 4, 25])
+print(parse("codewars"), [0])
