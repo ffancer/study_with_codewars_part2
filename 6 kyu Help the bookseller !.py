104b6730ddc81af0719e3e581ce51acb04fbc3f9
Paul <gafgafgarion05@gmail.com>
2022-06-09 07:12:37 +0300

    init

---
+++ 6 kyu Help the bookseller !.py
@@ -0,0 +1,7 @@
+def stock_list(listOfArt, listOfCat):
+    pass
+
+
+b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
+c = ["A", "B"]
+print(stock_list(b, c), "(A : 200) - (B : 1140)")
