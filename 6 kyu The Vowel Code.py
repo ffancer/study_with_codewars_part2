2b18fe7abde35cb3d0d493a607854f8d9daa1c19
Paul <gafgafgarion05@gmail.com>
2022-06-10 10:37:38 +0300

    my second solution translate - maketrans = more compact

--- 6 kyu The Vowel Code.py
+++ 6 kyu The Vowel Code.py
@@ -1,9 +1,9 @@
 def encode(st):
-    return st.replace('a', '1').replace('e', '2').replace('i', '3').replace('o', '4').replace('u', '5')
+    return st.translate(str.maketrans('aeiou', '12345'))
 
 
 def decode(st):
-    return st.replace('1', 'a').replace('2', 'e').replace('3', 'i').replace('4', 'o').replace('5', 'u')
+    return st.translate(str.maketrans('12345', 'aeiou'))
 
 
 print(encode('hello'), 'h2ll4')
