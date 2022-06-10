2b3478dd3e96073afdb434aa63101d742c00ec3d
Paul <gafgafgarion05@gmail.com>
2022-06-10 10:08:16 +0300

    solution random test is fail, but sometimes not

--- 6 kyu Find the missing term in an Arithmetic Progression.py
+++ 6 kyu Find the missing term in an Arithmetic Progression.py
@@ -1,6 +1,11 @@
-def find_missing(sequence):
-    for i in range(len(sequence)):
-        if sequence[i] + sequence[i+1]
+def find_missing(sequence: list):
+    step = min({sequence[1]-sequence[0], sequence[2]-sequence[1], sequence[3]-sequence[2]})
+
+    for i, n in enumerate(sequence):
+        if n + step != sequence[i+1]:
+            return n+step
+
+    return sequence[0]
 
 
 print(find_missing([1, 2, 3, 4, 6, 7, 8, 9]), 5)
