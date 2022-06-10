8caab08291ed925c232ec99c144537f5b61fc97f
Paul <gafgafgarion05@gmail.com>
2022-06-10 09:06:15 +0300

    best solution

--- 6 kyu Encrypt this!.py
+++ 6 kyu Encrypt this!.py
@@ -1,16 +1,21 @@
-def encrypt_that(word):
-    if len(word) > 2:
-        return str(ord(word[0]))+word[-1]+word[2:-1]+word[1]
-    elif len(word) == 2:
-        return str(ord(word[0])) + word[-1]
-    elif len(word) == 1:
-        return str(ord(word))
-    else:
-        return ''
-
-
 def encrypt_this(text):
-    return ' '.join([encrypt_that(word) for word in text.split()])
+    result = []
+
+    for word in text.split():
+        # turn word into a list
+        word = list(word)
+
+        # replace first letter with ascii code
+        word[0] = str(ord(word[0]))
+
+        # switch 2nd and last letters
+        if len(word) > 2:
+            word[1], word[-1] = word[-1], word[1]
+
+        # add to results
+        result.append(''.join(word))
+
+    return ' '.join(result)
 
 
 tests = [
