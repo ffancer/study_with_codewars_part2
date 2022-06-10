2da305f196f2e07397bdd86a5f6c62aa52d65153
Paul <gafgafgarion05@gmail.com>
2022-06-10 09:13:08 +0300

    best solution

--- 6 kyu Data Reverse.py
+++ 6 kyu Data Reverse.py
@@ -1,13 +1,10 @@
 def data_reverse(data):
-    lst = []
+    res = []
 
-    for i in range(len(data) // 8):
-        second_data = (len(data) - 8)
-        for j in range(second_data, len(data)):
-            lst.append(data[second_data])
-            data.pop(second_data)
+    for i in range(len(data) - 8, -1, -8):
+        res.extend(data[i:i + 8])
 
-    return lst
+    return res
 
 
 data1 = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]
