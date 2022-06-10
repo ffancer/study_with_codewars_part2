a7fe5d7926fdc632212dc9fd778f5fac28aa7f8f
Paul <gafgafgarion05@gmail.com>
2022-06-10 10:32:34 +0300

    init

---
+++ 6 kyu The Vowel Code.py
@@ -0,0 +1,12 @@
+def encode(st):
+    return
+
+
+def decode(st):
+    return
+
+
+print(encode('hello'), 'h2ll4')
+print(encode('How are you today?'), 'H4w 1r2 y45 t4d1y?')
+print(encode('This is an encoding test.'), 'Th3s 3s 1n 2nc4d3ng t2st.')
+print(decode('h2ll4'), 'hello')
