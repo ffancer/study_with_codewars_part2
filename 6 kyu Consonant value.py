a2eafa93be7f0f1b7b5cd28b88b173699fedb2f5
Paul <gafgafgarion05@gmail.com>
2022-06-11 08:00:45 +0300

    init

---
+++ 6 kyu Consonant value.py
@@ -0,0 +1,11 @@
+def solve(s):
+    pass
+
+
+print(solve("zodiac"), 26)
+print(solve("chruschtschov"), 80)
+print(solve("khrushchev"), 38)
+print(solve("strength"), 57)
+print(solve("catchphrase"), 73)
+print(solve("twelfthstreet"), 103)
+print(solve("mischtschenkoana"), 80)
