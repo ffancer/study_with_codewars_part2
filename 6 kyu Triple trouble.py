1931cfa5ec27173358ce8b53035c3e22e5264fba
Paul <gafgafgarion05@gmail.com>
2022-06-11 08:18:46 +0300

    init

---
+++ 6 kyu Triple trouble.py
@@ -0,0 +1,10 @@
+def triple_double(num1, num2):
+    pass
+
+
+print(triple_double(451999277, 41177722899), 1)
+print(triple_double(1222345, 12345), 0)
+print(triple_double(12345, 12345), 0)
+print(triple_double(666789, 12345667), 1)
+print(triple_double(10560002, 100), 1)
+print(triple_double(1112, 122), 0)
