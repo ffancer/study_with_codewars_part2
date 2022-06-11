58d54131bc0f2247d5e9d7fe9a6d042713598d01
Paul <gafgafgarion05@gmail.com>
2022-06-11 08:26:14 +0300

    3

--- 6 kyu Triple trouble.py
+++ 6 kyu Triple trouble.py
@@ -1,14 +1,14 @@
 def triple_double(num1, num2):
-    cnt = 0
+    triple = 0
     num1, num2 = str(num1), str(num2)
 
     for i in range(len(num1)-2):
         if num1[i] == num1[i+1] == num1[i+2]:
-            cnt += 1
-    for i in range(len(num2)-2):
-        if num2[i] == num2[i+1] == num2[i+2]:
-            cnt += 1
-    return cnt
+            triple += int(num1[i] + num1[i+1] + num1[i+2])
+    # for i in range(len(num2)-2):
+    #     if num2[i] == num2[i+1] == num2[i+2]:
+    #         cnt += 1
+    return triple
 
 
 print(triple_double(451999277, 41177722899), 1)
