3775219d5a09ac8e8b6ad1b1229c8a976049ae1a
Paul <gafgafgarion05@gmail.com>
2022-06-11 09:43:55 +0300

    init

---
+++ 6 kyu N-th Fibonacci.py
@@ -0,0 +1,11 @@
+def nth_fib(n):
+    return n
+
+
+print(nth_fib(1), 0, "1-st Fibo")
+print(nth_fib(2), 1, "2-nd Fibo")
+print(nth_fib(3), 1, "3-rd Fibo")
+print(nth_fib(4), 2, "4-th Fibo")
+print(nth_fib(5), 3, "5-th Fibo")
+print(nth_fib(6), 5, "6-th Fibo")
+print(nth_fib(7), 8, "7-th Fibo")
