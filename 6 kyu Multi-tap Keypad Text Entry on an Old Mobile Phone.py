36cc04ed22dcf98cd5ed05b8ecf1674ebf8e7238
Paul <gafgafgarion05@gmail.com>
2022-06-11 10:01:02 +0300

    my solution

--- 6 kyu Multi-tap Keypad Text Entry on an Old Mobile Phone.py
+++ 6 kyu Multi-tap Keypad Text Entry on an Old Mobile Phone.py
@@ -1,5 +1,20 @@
 def presses(phrase):
-    pass
+    phrase = phrase.lower()
+    cnt = 0
+
+    for i in phrase:
+        if i in '1adgjmptw* #':
+            cnt += 1
+        elif i in 'behknqux0':
+            cnt += 2
+        elif i in 'cfilorvy':
+            cnt += 3
+        elif i in '23456s8z':
+            cnt += 4
+        elif i in '79':
+            cnt += 5
+
+    return cnt
 
 
 print(presses("LOL"), 9)
