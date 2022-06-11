7ea62058a311fa10b7d22b2b28595893bae0a941
Paul <gafgafgarion05@gmail.com>
2022-06-11 10:28:46 +0300

    init

---
+++ 6 kyu Playing with passphrases.py
@@ -0,0 +1,7 @@
+def play_pass(s, n):
+    pass
+
+
+print(play_pass("I LOVE YOU!!!", 1), "!!!vPz fWpM J")
+print(play_pass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2),
+      "4897 NkTrC Hq fT67 GjV Pq aP OqTh gOcE CoPcTi aO")
