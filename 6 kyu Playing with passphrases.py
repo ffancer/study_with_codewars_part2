d2bfb26bc1e5c0800a10a96abd2afd41dff403ac
Paul <gafgafgarion05@gmail.com>
2022-06-11 10:35:09 +0300

    my solution

--- 6 kyu Playing with passphrases.py
+++ 6 kyu Playing with passphrases.py
@@ -1,10 +1,19 @@
 def play_pass(s, n):
-    lst = []
+    res = ''
+    
+    for i in map(ord, s):
+        if i in range(65, 91):
+            res += chr((((i - 65) + n) % 26) + 65)
+        elif i in range(97, 123):
+            res += chr((((i - 97) + n) % 26) + 97)
+        elif i in range(48, 58):
+            res += str(abs(9 - int(chr(i))))
+        else:
+            res += str(chr(i))
+    res = ''.join([res[i].upper() if i % 2 == 0 else res[i].lower() for i in range(len(res))])
 
-    for i in s:
-        lst.append(chr(ord(i) + n))
+    return res[::-1]
 
-    return ''.join(lst)
 
 print(play_pass("I LOVE YOU!!!", 1), "!!!vPz fWpM J")
 print(play_pass("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2),
