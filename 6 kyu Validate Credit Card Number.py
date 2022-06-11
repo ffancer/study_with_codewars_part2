039a107cba595e61ad57a5f6ade973651c8989cf
Paul <gafgafgarion05@gmail.com>
2022-06-11 08:41:29 +0300

    1 line solution

--- 6 kyu Validate Credit Card Number.py
+++ 6 kyu Validate Credit Card Number.py
@@ -1,6 +1,5 @@
-def validate(n):
-    pass
-
+validate = lambda n: not sum(
+    [0 if x == "0" else (int(x) * 2 - 1) % 9 + 1 if i % 2 else int(x) for i, x in enumerate(reversed(str(n)))]) % 10
 
 print(validate(123), False)
 print(validate(1), False)
