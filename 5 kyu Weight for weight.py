65d5ed1ff411e143d60c69c93ab09fed07f69894
Paul <gafgafgarion05@gmail.com>
2022-06-13 10:09:40 +0300

    4

--- 5 kyu Weight for weight.py
+++ 5 kyu Weight for weight.py
@@ -3,12 +3,15 @@ def sum_dig(n):
 
 
 def order_weight(strng):
-    # dct = {i for i in strng.split()}
+    dct = {}  # i for i in strng.split()
     # return dct
-    return list(sum_dig(i) for i in strng.split())
+    # return list(sum_dig(i) for i in strng.split())
+    lst = []
 
-
-
+    for i in strng.split():
+        dct.update({int(i): sum_dig(i)})
+        # print(i)
+    return dct
 
 print(order_weight("103 123 4444 99 2000"), "2000 103 123 4444 99")
 print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"),
