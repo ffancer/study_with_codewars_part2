970e70416f6145789a4ff500ce21aa95539ca95a
Paul <gafgafgarion05@gmail.com>
2022-06-13 10:20:04 +0300

    5 duplicates are missed

--- 5 kyu Weight for weight.py
+++ 5 kyu Weight for weight.py
@@ -3,15 +3,20 @@ def sum_dig(n):
 
 
 def order_weight(strng):
-    dct = {}  # i for i in strng.split()
-    # return dct
-    # return list(sum_dig(i) for i in strng.split())
+    dct = {}
     lst = []
 
     for i in strng.split():
-        dct.update({int(i): sum_dig(i)})
-        # print(i)
-    return dct
+        dct.update({sum_dig(i): int(i)})
+
+    sort_dct = sorted(dct.items())
+
+    for i, j in sort_dct:
+        lst.append(j)
+
+    return lst
+
+
 
 print(order_weight("103 123 4444 99 2000"), "2000 103 123 4444 99")
 print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"),
