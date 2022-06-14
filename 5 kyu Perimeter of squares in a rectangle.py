acd8c1f03eca25d6fd2b47596d57ead24d0a786c
Paul <gafgafgarion05@gmail.com>
2022-06-14 10:34:31 +0300

    2

--- 5 kyu Perimeter of squares in a rectangle.py
+++ 5 kyu Perimeter of squares in a rectangle.py
@@ -2,11 +2,14 @@
 #     pass
 
 
+cache = {0: 0, 1: 1}
+
+
 def fibonacci_of(n):
-    lst = []
-    if n in {0, 1}:
-        return n
-    return lst.append(fibonacci_of(n - 1) + fibonacci_of(n - 2))
+    if n in cache:
+        return cache[n]
+    cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)
+    return cache[n]
 
 
 print(fibonacci_of(6))
