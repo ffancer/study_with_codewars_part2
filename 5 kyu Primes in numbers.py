e0e622640b217bf16e5189475762d33e24b826c4
Paul <gafgafgarion05@gmail.com>
2022-06-14 11:24:47 +0300

    1

--- 5 kyu Primes in numbers.py
+++ 5 kyu Primes in numbers.py
@@ -1,5 +1,15 @@
 def prime_factors(n):
-    pass
+    dct = {}
+
+    for i in range(2, n + 1):
+        while n % i == 0 and n >= 1:
+            if i in dct:
+                dct[i] += 1
+            else:
+                dct[i] = 1
+            n /= i
+
+    return dct
 
 
 print(prime_factors(7775460), "(2**2)(3**3)(5)(7)(11**2)(17)")
