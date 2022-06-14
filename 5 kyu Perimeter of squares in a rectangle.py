202c3760d533a4cd1d6602ccf3f215bed782ecd8
Paul <gafgafgarion05@gmail.com>
2022-06-14 10:29:31 +0300

    init

---
+++ 5 kyu Perimeter of squares in a rectangle.py
@@ -0,0 +1,9 @@
+def perimeter(n):
+    pass
+
+
+print(perimeter(5), 80)
+print(perimeter(7), 216)
+print(perimeter(20), 114624)
+print(perimeter(30), 14098308)
+print(perimeter(100), 6002082144827584333104)
