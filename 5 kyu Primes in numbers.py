3bc266002846d6db2c6b0214f72792274924b679
Paul <gafgafgarion05@gmail.com>
2022-06-14 11:37:25 +0300

    best solution

--- 5 kyu Primes in numbers.py
+++ 5 kyu Primes in numbers.py
@@ -1,33 +1,10 @@
-def get_all_prime_factors(n):
-    if not isinstance(n, int) or n <= 1:
-        if n == 1:
-            return [1]
-        else:
-            return []
-    else:
-        factors = []
-        prime = 2
-        while prime * prime <= n:
-            while n % prime == 0:
-                factors.append(prime)
-                n //= prime
-            prime += 1
-        if n > 1:
-            factors.append(n)
-        return factors
-
-
 def prime_factors(n):
-    res = ""
-    factors = get_all_prime_factors(n)
-
-    for i in sorted(set(factors)):
-        cnt = factors.count(i)
-        if cnt == 1:
-            res += f'({i})'
-        else:
-            res += f'({i}**{cnt})'
-    return res
+    i, j, p = 2, 0, []
+    while n > 1:
+        while n % i == 0: n, j = n / i, j + 1
+        if j > 0: p.append([i,j])
+        i, j = i + 1, 0
+    return ''.join('(%d' %q[0] + ('**%d' %q[1]) * (q[1] > 1) + ')' for q in p)
 
 
 print(prime_factors(7775460), "(2**2)(3**3)(5)(7)(11**2)(17)")
