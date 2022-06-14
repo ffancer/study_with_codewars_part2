a88ae6221719771e600009041abd400dcf3a3953
Paul <gafgafgarion05@gmail.com>
2022-06-14 10:33:04 +0300

    1

--- 5 kyu Perimeter of squares in a rectangle.py
+++ 5 kyu Perimeter of squares in a rectangle.py
@@ -1,9 +1,17 @@
-def perimeter(n):
-    pass
+# def perimeter(n):
+#     pass
 
 
-print(perimeter(5), 80)
-print(perimeter(7), 216)
-print(perimeter(20), 114624)
-print(perimeter(30), 14098308)
-print(perimeter(100), 6002082144827584333104)
+def fibonacci_of(n):
+    lst = []
+    if n in {0, 1}:
+        return n
+    return lst.append(fibonacci_of(n - 1) + fibonacci_of(n - 2))
+
+
+print(fibonacci_of(6))
+# print(perimeter(5), 80)
+# print(perimeter(7), 216)
+# print(perimeter(20), 114624)
+# print(perimeter(30), 14098308)
+# print(perimeter(100), 6002082144827584333104)
