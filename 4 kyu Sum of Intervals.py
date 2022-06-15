5073f0f4351c1bc770f7e54d8eefcefdc3c1c61e
Paul <gafgafgarion05@gmail.com>
2022-06-15 08:52:29 +0300

    init

---
+++ 4 kyu Sum of Intervals.py
@@ -0,0 +1,8 @@
+def sum_of_intervals(intervals):
+    return
+
+
+print(sum_of_intervals([(1, 5)]), 4)
+print(sum_of_intervals([(1, 5), (6, 10)]), 8)
+print(sum_of_intervals([(1, 5), (1, 5)]), 4)
+print(sum_of_intervals([(1, 4), (7, 10), (3, 5)]), 7)
