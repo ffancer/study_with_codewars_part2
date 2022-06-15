52d7ead04ebfc49647d9b2b0774ea43a8793ef81
Paul <gafgafgarion05@gmail.com>
2022-06-15 10:06:13 +0300

    init

---
+++ 4 kyu Snail.py
@@ -0,0 +1,15 @@
+def snail(snail_map):
+    pass
+
+
+array = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+expected = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+print(snail(array), expected)
+
+array = [[1, 2, 3],
+         [8, 9, 4],
+         [7, 6, 5]]
+expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(snail(array), expected)
