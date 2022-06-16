396ccc0323ff0293d18e46afa47100f2db3102f2
Paul <gafgafgarion05@gmail.com>
2022-06-16 10:33:26 +0300

    my solution

--- 4 kyu Twice linear.py
+++ 4 kyu Twice linear.py
@@ -1,6 +1,23 @@
 def dbl_linear(n):
-    # your code
-    return 0
+    cnt_y, cnt_z = 0, 0
+    lst = [1]
+
+    while len(lst) <= n:
+        y = 2 * lst[cnt_y] + 1
+        z = 3 * lst[cnt_z] + 1
+
+        if y > z:
+            lst.append(z)
+            cnt_z += 1
+        elif y < z:
+            lst.append(y)
+            cnt_y += 1
+        else:
+            lst.append(y)
+            cnt_y += 1
+            cnt_z += 1
+
+    return lst[n]
 
 
 print(dbl_linear(10), 22)
