aebf94cc341309b4f9b219e326ffa2eeb9ba11b9
Paul <gafgafgarion05@gmail.com>
2022-06-17 11:04:05 +0300

    1

--- 4 kyu Sum by Factors.py
+++ 4 kyu Sum by Factors.py
@@ -1,3 +1,18 @@
+def prime_factors(n):
+    f = []
+    n = abs(n)
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n /= i
+            f.append(i)
+    if n > 1:
+        f.append(n)
+    return f
+
+
 def sum_for_list(lst):
     pass
 
