4fb51062ed877250347a1513c34820cfc82f12a0
Paul <gafgafgarion05@gmail.com>
2022-06-17 11:01:06 +0300

    init

---
+++ 4 kyu Sum by Factors.py
@@ -0,0 +1,9 @@
+def sum_for_list(lst):
+    pass
+
+
+a = [12, 15]
+print(sum_for_list(a), [[2, 12], [3, 27], [5, 15]])
+
+a = [15, 21, 24, 30, 45]
+print(sum_for_list(a), [[2, 54], [3, 135], [5, 90], [7, 21]])
