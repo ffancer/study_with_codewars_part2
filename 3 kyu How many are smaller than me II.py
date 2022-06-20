d3bef05a12e32b49325d6e6d1ca5338ea8252eee
Paul <gafgafgarion05@gmail.com>
2022-06-20 15:51:50 +0300

    9 try to use dictionary

--- 3 kyu How many are smaller than me II.py
+++ 3 kyu How many are smaller than me II.py
@@ -2,34 +2,31 @@
 # How many are smaller than me II?
 
 
+# def smaller(arr):
+#     sort_nums = sorted(arr)
+#     dct = {}
+#     lst = []
+#
+#     for i in range(len(sort_nums)):
+#         if sort_nums[i] not in dct:
+#             dct[sort_nums[i]] = i
+#     for i in arr:
+#         lst.append(dct[i])
+#     return lst
 def smaller(arr):
-    cnt = 0
     lst = []
-    i, j = 0, 1
+    dct = {}
 
-    while arr:
-        while j < len(arr):
-            if arr[i] > arr[j]:
-                cnt += 1
-                j += 1
-            # elif arr[i] < arr[j]:
-            #     j += 1
-            # elif arr[i] == arr[j]:
-            #     j += 1
-            else:
-                j += 1
-        lst.append(cnt)
-        cnt = 0
-        arr = arr[1:]
-        j = 1
+    for i in range(len(arr)):
+        if arr[i] not in dct:
+            dct[arr[i]] = i
+    return dct
 
 
-    return lst
 
-
-# print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])
-# print(smaller([1, 2, 3]), [0, 0, 0])
-# print(smaller([1, 2, 0]), [1, 1, 0])
+print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])
+print(smaller([1, 2, 3]), [0, 0, 0])
+print(smaller([1, 2, 0]), [1, 1, 0])
 print(smaller([1, 2, 1]), [0, 1, 0])
 print(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0])
 print(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0])
