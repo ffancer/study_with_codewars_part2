f0a9f07065f8e3d2a3905cba19ba1d0dcb6b948b
Paul <gafgafgarion05@gmail.com>
2022-06-20 10:55:39 +0300

    7 "Timed OutPassed: 67" 67 tests are ok, but code is slow

--- 3 kyu How many are smaller than me II.py
+++ 3 kyu How many are smaller than me II.py
@@ -14,6 +14,8 @@ def smaller(arr):
                 j += 1
             elif arr[i] < arr[j]:
                 j += 1
+            elif arr[i] == arr[j]:
+                j += 1
         lst.append(cnt)
         cnt = 0
         arr = arr[1:]
@@ -23,9 +25,9 @@ def smaller(arr):
     return lst
 
 
-print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])
-print(smaller([1, 2, 3]), [0, 0, 0])
-print(smaller([1, 2, 0]), [1, 1, 0])
+# print(smaller([5, 4, 3, 2, 1]), [4, 3, 2, 1, 0])
+# print(smaller([1, 2, 3]), [0, 0, 0])
+# print(smaller([1, 2, 0]), [1, 1, 0])
 print(smaller([1, 2, 1]), [0, 1, 0])
 print(smaller([1, 1, -1, 0, 0]), [3, 3, 0, 0, 0])
 print(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6]), [4, 1, 5, 5, 0, 0, 0, 0, 0])
