d43000f872d1846e7dfd0e8cf306882405d1cf00
Paul <gafgafgarion05@gmail.com>
2022-06-23 14:59:11 +0300

    init

---
+++ 2 kyu Evaluate mathematical expression.py
@@ -0,0 +1,12 @@
+def calc(expression):
+    return
+
+
+print(calc("1 + 1"), 2)
+print(calc("8/16"), 0.5)
+print(calc("3 -(-1)"), 4)
+print(calc("2 + -2"), 0)
+print(calc("10- 2- -5"), 13)
+print(calc("(((10)))"), 10)
+print(calc("3 * 5"), 15)
+print(calc("-7 * -(6 / 3)"), 14)
