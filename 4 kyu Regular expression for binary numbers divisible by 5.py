eb22922c59b0e867006b34a6b8e389c1bc7fc9c4
Paul <gafgafgarion05@gmail.com>
2022-06-29 13:18:50 +0300

    init

---
+++ 4 kyu Regular expression for binary numbers divisible by 5.py
@@ -0,0 +1,29 @@
+import re
+
+PATTERN = r'0|(101(0)*)$'
+
+tests = [(False, ""),
+         (False, "abc"),
+         (True, "000"),
+         (True, "101"),
+         (True, "1010"),
+         (True, "10100"),
+         (True, "{:b}".format(65020)),
+         (True, "{:b}".format(6039865)),
+
+         (False, "10110101"),
+         (False, "1110001"),
+
+         (False, "{:b}".format(21)),
+         (False, "{:b}".format(15392)),
+         (False, "{:b}".format(23573)),
+         (False, "{:b}".format(19344)),
+
+         (False, "{:b}".format(43936)),
+         (False, "{:b}".format(32977)),
+         (False, "{:b}".format(328)),
+         (False, "{:b}".format(5729)),
+         ]
+
+for exp, s in tests:
+    print(bool(re.match(PATTERN, s)), exp, "Should work with '{}'".format(s))
