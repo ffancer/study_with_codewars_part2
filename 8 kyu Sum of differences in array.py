0e5325f6bd6ada12773b5594d2613c2ad581ef98
Paul <gafgafgarion05@gmail.com>
2022-07-25 10:51:46 +0300

    init

---
+++ 8 kyu Sum of differences in array.py
@@ -0,0 +1,12 @@
+def sum_of_differences(arr):
+    return 0
+
+
+print(sum_of_differences([1, 2, 10]), 9)
+print(sum_of_differences([-3, -2, -1]), 2)
+print(sum_of_differences([1, 1, 1, 1, 1]), 0)
+print(sum_of_differences([-17, 17]), 34)
+print(sum_of_differences([]), 0)
+print(sum_of_differences([0]), 0)
+print(sum_of_differences([-1]), 0)
+print(sum_of_differences([1]), 0)
