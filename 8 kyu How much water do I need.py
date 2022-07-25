c162295b108d8ac5ed8151326432cafe0cf3ac99
Paul <gafgafgarion05@gmail.com>
2022-07-25 10:34:46 +0300

    my 1 line solution -added forgot about 2

--- 8 kyu How much water do I need.py
+++ 8 kyu How much water do I need.py
@@ -3,7 +3,7 @@
 
 
 def how_much_water(L, X, N):
-    return 'Too much clothes' if N > X * 2 else 'Not enough clothes' if N < X else L * 1.1 ** (N - X)
+    return 'Too much clothes' if N > X * 2 else 'Not enough clothes' if N < X else round(L * 1.1 ** (N - X), 2)
 
 
 print(how_much_water(10, 10, 21), 'Too much clothes')
