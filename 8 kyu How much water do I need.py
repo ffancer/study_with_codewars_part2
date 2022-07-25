3a1ceba75ba8fda104fcf42639fcf2a504f85bec
Paul <gafgafgarion05@gmail.com>
2022-07-25 09:46:13 +0300

    init

---
+++ 8 kyu How much water do I need.py
@@ -0,0 +1,12 @@
+# 8 kyu
+# How much water do I need?
+
+
+def how_much_water(L, X, N):
+    pass
+
+
+print(how_much_water(10, 10, 21), 'Too much clothes', '')
+print(how_much_water(10, 10, 2), 'Not enough clothes', '')
+print(how_much_water(10, 11, 20), 23.58, '')
+print(how_much_water(50, 15, 29), 189.87, '')
