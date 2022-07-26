b43ff220f0e0bf99817e3e1a9bf875a81c794c71
Paul <gafgafgarion05@gmail.com>
2022-07-26 16:01:44 +0300

    2

--- 8 kyu Is the date today.py
+++ 8 kyu Is the date today.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def is_today(date):
-    return datetime.today()
+    return str(datetime.today()).split('-')
 
 
 print(is_today(datetime(2020, 10, 1, 1, 1, 1, 1)), False)
