1384a265b921e6c625983845e086e9677cdf2e05
Paul <gafgafgarion05@gmail.com>
2022-07-26 16:09:05 +0300

    my solution

--- 8 kyu Is the date today.py
+++ 8 kyu Is the date today.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def is_today(date):
-    return date.date()
+    return datetime.today().date() == date.date()
 
 
 print(is_today(datetime(2020, 10, 1, 1, 1, 1, 1)), False)
