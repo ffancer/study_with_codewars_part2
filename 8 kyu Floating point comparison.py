a3062049030ac11971d64944133a1e0396672c5e
Paul <gafgafgarion05@gmail.com>
2022-07-27 10:25:14 +0300

    init

---
+++ 8 kyu Floating point comparison.py
@@ -0,0 +1,13 @@
+def approx_equals(a, b):
+    a = round(a, 3)
+    b = round(b, 3)
+    return a == b
+
+
+print(approx_equals(175.9827, 82.25))
+print(approx_equals(-156.24037, -156.24038))
+print(approx_equals(123.2345, 123.234501))
+print(approx_equals(1456.3652, 1456.3641))
+print(approx_equals(-1.234, -1.233999))
+print(approx_equals(98.7655, 98.7654999))
+print(approx_equals(-7.28495, -7.28596))
