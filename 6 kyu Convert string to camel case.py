c4103f24fe358832bdb31ef2b9801610df3b3dfa
Paul <gafgafgarion05@gmail.com>
2022-07-27 10:12:59 +0300

    best solution + 1 line

--- 6 kyu Convert string to camel case.py
+++ 6 kyu Convert string to camel case.py
@@ -1,16 +1,8 @@
 def to_camel_case(text):
-    if '_' in text:
-        text = text.replace('_', '-')
-    if '-' in text:
-        text = text.split('-')
-        if text[0][0].islower():
-            return text[0] + ''.join(i.title() for i in text[1:])
-        elif text[0][0].isupper():
-            return ''.join(i.title() for i in text)
-    return ''
+    return text[:1] + text.title()[1:].replace('_', '').replace('-', '')
 
 
-print(to_camel_case(''), '')
-print(to_camel_case("the_stealth_warrior"), "theStealthWarrior")
-print(to_camel_case("The-stealth-warrior"), "TheStealthWarrior")
-print(to_camel_case("A-B-C"), "ABC")
+print(to_camel_case(''))
+print(to_camel_case("the_stealth_warrior"))
+print(to_camel_case("The-stealth-warrior"))
+print(to_camel_case("A-B-C"))
