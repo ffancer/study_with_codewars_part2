cf97bbd1324a33e313ee830d90b9b6f73d6faa33
Paul <gafgafgarion05@gmail.com>
2022-07-27 09:29:24 +0300

    3

--- 6 kyu Convert string to camel case.py
+++ 6 kyu Convert string to camel case.py
@@ -2,7 +2,7 @@ def to_camel_case(text):
     if '-' in text:
         text = text.split('-')
         if text[0][0].islower():
-
+            return ''.join(i.title for i in text)
         return ''.join(text)
     elif '_' in text:
         text = text.split('_')
@@ -10,9 +10,7 @@ def to_camel_case(text):
     return ''
 
 
-print(to_camel_case(''), '', "An empty string was provided but not returned")
-print(to_camel_case("the_stealth_warrior"), "theStealthWarrior",
-      "to_camel_case('the_stealth_warrior') did not return correct value")
-print(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior",
-      "to_camel_case('The-Stealth-Warrior') did not return correct value")
-print(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
+print(to_camel_case(''), '')
+print(to_camel_case("the_stealth_warrior"), "theStealthWarrior")
+print(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior")
+print(to_camel_case("A-B-C"), "ABC")
