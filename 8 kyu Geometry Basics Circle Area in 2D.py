26259c8ec6fa48a9fd8ffc3cca250d8032f35e52
Paul <gafgafgarion05@gmail.com>
2022-07-28 10:29:11 +0300

    init

---
+++ 8 kyu Geometry Basics Circle Area in 2D.py
@@ -0,0 +1,12 @@
+# 8 kyu
+# Geometry Basics: Circle Area in 2D
+
+
+def circle_area(circle):
+    pass
+
+
+print(round(circle_area(Circle(Point(10, 10), 30)), 6), 2827.433388)
+print(round(circle_area(Circle(Point(25, -70), 30)), 6), 2827.433388)
+print(round(circle_area(Circle(Point(-15, 5), 0)), 6), 0)
+print(round(circle_area(Circle(Point(-15, 5), 12.5)), 6), 490.873852)
