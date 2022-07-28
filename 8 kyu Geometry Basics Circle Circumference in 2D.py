22765b98efaafe000e8c44389d19d0443d6e0858
Paul <gafgafgarion05@gmail.com>
2022-07-28 10:21:15 +0300

    1

--- 8 kyu Geometry Basics Circle Circumference in 2D.py
+++ 8 kyu Geometry Basics Circle Circumference in 2D.py
@@ -1,9 +1,11 @@
 # 8 kyu
 # Geometry Basics: Circle Circumference in 2D
 
+from math import pi
+
 
 def circle_circumference(circle):
-    pass
+    return circle * pi * 2
 
 
 print(circle_circumference(10))
