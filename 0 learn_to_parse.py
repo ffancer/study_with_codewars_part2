329b608d15fe819d8c8bb5a9eba6be9fc7a01760
Paul <gafgafgarion05@gmail.com>
2022-07-28 14:59:04 +0300

    -

--- 0 learn_to_parse.py
+++ 0 learn_to_parse.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+import lxml
 
 url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
 
 response = requests.get(url)
 
-print(response)
+soup = BeautifulSoup(response.text, 'lxml')
+print(soup)
