e72b6f9d735583f66c2a009a5cfa00a2cadae4cb
Paul <gafgafgarion05@gmail.com>
2022-07-29 11:38:59 +0300

    take only img

--- 0 learn_to_parse.py
+++ 0 learn_to_parse.py
@@ -1,6 +1,6 @@
 # import requests
 # from bs4 import BeautifulSoup
-# import lxml
+#
 #
 # url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
 #
@@ -10,14 +10,16 @@
 #
 # data = soup.find('div', class_='col-lg-4 col-md-6 mb-4')  # в диве нужный нам класс с таким названием
 #
-# name = data.find('h4', class_='card-title')
-# print(name)
+# name = data.find('h4', class_='card-title').text.replace('\n', '')  # метод text и не надо самому сплитить название, replace для удаления красных строк
+# price = data.find('h5').text
+# url_img = data.find('a')
+# print(url_img)
 
-# s = '<a href="/exercise/list_basic_detail/90008-E/">Short Dress</a>'
-s = '<a href="/exercise/list_basic_detail/90008-E/">Patterned Slacks</a>'
 
-s = s.split('>')[1][:-3]
-print(s)
+s = '<a href="/exercise/list_basic_detail/90008-E/"><img alt="" class="card-img-top img-fluid" src="/static/img/90008-E.jpg"/></a>'
+
+s = s.split('src=')
+print(s[1][:-6])
 
 
 # 16-35
