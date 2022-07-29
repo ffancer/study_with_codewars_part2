a17a62e776ddb4c28e30d0c92e698254a8a2b141
Paul <gafgafgarion05@gmail.com>
2022-07-29 11:27:48 +0300

    take only name of product

--- 0 learn_to_parse.py
+++ 0 learn_to_parse.py
@@ -1,10 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
-import lxml
+# import requests
+# from bs4 import BeautifulSoup
+# import lxml
+#
+# url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+#
+# response = requests.get(url)
+#
+# soup = BeautifulSoup(response.text, 'lxml')
+#
+# data = soup.find('div', class_='col-lg-4 col-md-6 mb-4')  # в диве нужный нам класс с таким названием
+#
+# name = data.find('h4', class_='card-title')
+# print(name)
 
-url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+# s = '<a href="/exercise/list_basic_detail/90008-E/">Short Dress</a>'
+s = '<a href="/exercise/list_basic_detail/90008-E/">Patterned Slacks</a>'
 
-response = requests.get(url)
+s = s.split('>')[1][:-3]
+print(s)
 
-soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+
+# 16-35
