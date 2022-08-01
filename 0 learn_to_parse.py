1bcbb5e394f8aeb69a35224ce231534dc2f7e463
Paul <gafgafgarion05@gmail.com>
2022-08-01 15:16:33 +0300

    finish created folder => download images there

--- 0 learn_to_parse.py
+++ 0 learn_to_parse.py
@@ -3,9 +3,18 @@ from bs4 import BeautifulSoup
 from time import sleep  # нужно что бы сайт думал что действует человек
 import xlsxwriter
 
-
 # хэдерс делается чтобы сайт думла что действует человек
-headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)'}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)'}
+
+
+def download(url):
+    resp = requests.get(url,
+                        stream=True)  # stream=True хначит что картинка будет грузится по мере загрузки минуя оперативку
+    r = open(r'C:\Users\Paul\Desktop\images\\' + url.split('/')[-1], 'wb')
+    for value in resp.iter_content(1048576):  # 1048576 = 1024 * 1024 или 1 мегабайт
+        r.write(value)
+    r.close()
 
 
 def get_url():
@@ -16,19 +25,20 @@ def get_url():
         data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')  # в диве нужный нам класс с таким названием
         for i in data:
             card_url = 'https://scrapingclub.com' + i.find('a').get('href')
-            yield card_url # в этот момент весь код в функции выше превращается в генератор
+            yield card_url  # в этот момент весь код в функции выше превращается в генератор
 
 
 def array():
     for card_url in get_url():
         response = requests.get(card_url, headers=headers)
-        # sleep(3)
+        sleep(1)
         soup = BeautifulSoup(response.text, 'lxml')
         data = soup.find('div', class_='card mt-4 my-4')
         name = data.find('h3', class_='card-title').text
         price = data.find('h4').text
         text = data.find('p', class_='card-text').text
-        url_img = 'https://scrapingclub.com' + data.find('img').get('src') # , class_='card-img-top img-fluid'
+        url_img = 'https://scrapingclub.com' + data.find('img').get('src')  # , class_='card-img-top img-fluid'
+        download(url_img)
         yield name, price, text, url_img
 
 
@@ -47,12 +57,11 @@ def writer(parametr):
 
     for item in parametr():
         page.write(row, column, item[0])
-        page.write(row, column+1, item[1])
-        page.write(row, column+2, item[2])
-        page.write(row, column+3, item[3])
+        page.write(row, column + 1, item[1])
+        page.write(row, column + 2, item[2])
+        page.write(row, column + 3, item[3])
         row += 1
     book.close()
 
 
 writer(array)
-
