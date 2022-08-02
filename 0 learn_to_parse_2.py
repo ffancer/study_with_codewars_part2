881b35ec5aeafa82379f5142183587e13548bf4e
Paul <gafgafgarion05@gmail.com>
2022-08-02 14:32:01 +0300

    find 'logout' in text, all ok

--- 0 learn_to_parse_2.py
+++ 0 learn_to_parse_2.py
@@ -1,7 +1,6 @@
 from requests import Session  # для сохранения файлов куки
 from bs4 import BeautifulSoup
 
-
 # хэдерс делается чтобы сайт думла что действует человек
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 '
@@ -13,4 +12,13 @@ work.get('https://quotes.toscrape.com/', headers=headers)
 response = work.get('https://quotes.toscrape.com/login', headers=headers)
 
 soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+token = soup.find('form').find('input').get('value')
+data = {'csrf_token': token,
+        'username': 'noname',
+        'password': 'password'
+        }
+
+# наш запрос проходит 3 стадии, это и будет отображено далее
+# по умолчанию перенаправление не поддерживается но allow_redirects=True разрешает
+result = work.post('https://quotes.toscrape.com/login', headers=headers, data=data, allow_redirects=True)
+print(result.text)
