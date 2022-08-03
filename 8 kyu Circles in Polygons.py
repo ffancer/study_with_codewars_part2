697c70513195d39abd5e9aa862df5e2572e783ce
Paul <gafgafgarion05@gmail.com>
2022-08-03 15:06:26 +0300

    init

---
+++ 8 kyu Circles in Polygons.py
@@ -0,0 +1,8 @@
+def circle_diameter(sides, side_length):
+    # your code here
+    pass
+
+
+print(circle_diameter(4, 5), 5.000)
+print(circle_diameter(8, 9), 21.728)
+print(circle_diameter(3, 4), 2.309)
