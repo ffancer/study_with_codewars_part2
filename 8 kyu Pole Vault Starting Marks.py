b3a15373f7105633dfa8176485ac76b664337e39
Paul <gafgafgarion05@gmail.com>
2022-08-05 16:02:46 +0300

    init

---
+++ 8 kyu Pole Vault Starting Marks.py
@@ -0,0 +1,9 @@
+def starting_mark(height):
+    pass
+
+
+print(starting_mark(1.52), 9.45)
+print(starting_mark(1.83), 10.67)
+print(starting_mark(1.22), 8.27)
+print(starting_mark(2.13), 11.85)
+print(starting_mark(1.75), 10.36)
