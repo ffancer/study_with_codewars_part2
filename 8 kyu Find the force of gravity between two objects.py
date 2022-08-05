45f875977f1f6d13bc02cefe063c87fb90fd940d
Paul <gafgafgarion05@gmail.com>
2022-08-05 16:01:23 +0300

    my solution

--- 8 kyu Find the force of gravity between two objects.py
+++ 8 kyu Find the force of gravity between two objects.py
@@ -1,5 +1,11 @@
+dct = {"kg": 1, "g": 1e-3, "mg": 1e-6, "μg": 1e-9, "lb": 0.453592,
+       "m": 1, "cm": 1e-2, "mm": 1e-3, "μm": 1e-6, "ft": 0.3048,
+       }
+
+
 def solution(arr_val, arr_unit):
-    pass
+    m1, m2, r = (arr_val[i] * dct[arr_unit[i]] for i in range(3))
+    return 6.67e-11 * m1 * m2 / r ** 2
 
 
 print(solution([1000, 1000, 100], ["g", "kg", "m"]), 6.67e-12)
