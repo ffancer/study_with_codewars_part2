2b11dccafe4aa00fe52154dc5052163bcf28c065
Paul <gafgafgarion05@gmail.com>
2022-08-05 14:50:29 +0300

    my solution

--- 8 kyu Help the Elite Squad of Brazilian forces BOPE.py
+++ 8 kyu Help the Elite Squad of Brazilian forces BOPE.py
@@ -9,12 +9,12 @@ weapons_dct = {
 }
 
 
-def mag_number(str_, int_):
-    return ceil(int_ * 3 / weapons_dct[str_])
+def mag_number(tuple_):
+    return ceil(tuple_[1] * 3 / weapons_dct[tuple_[0]])
 
 
-print(mag_number("PT92", 6), 2)
-print(mag_number("M4A1", 8), 1)
-print(mag_number("M16A2", 19), 2)
-print(mag_number("PSG1", 31), 19)
-print(mag_number("PT92", 19), 4)
+print(mag_number(("PT92", 6)), 2)
+print(mag_number(("M4A1", 8)), 1)
+print(mag_number(("M16A2", 19)), 2)
+print(mag_number(("PSG1", 31)), 19)
+print(mag_number(("PT92", 19)), 4)
