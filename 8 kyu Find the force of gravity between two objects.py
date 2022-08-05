fb49f450e21920ed47141f8d12d6219b33e17d64
Paul <gafgafgarion05@gmail.com>
2022-08-05 14:56:47 +0300

    init

---
+++ 8 kyu Find the force of gravity between two objects.py
@@ -0,0 +1,7 @@
+def solution(arr_val, arr_unit):
+    pass
+
+
+print(solution([1000, 1000, 100], ["g", "kg", "m"]), 6.67e-12)
+print(solution([1000, 1000, 100], ["kg", "kg", "m"]), 6.667e-9)
+print(solution([1000, 1000, 100], ["kg", "kg", "cm"]), 0.0000667)
