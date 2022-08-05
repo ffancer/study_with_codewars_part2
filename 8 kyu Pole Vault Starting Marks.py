d5ca3d5992cb3ef0ea84db18cb6a61ad68601a45
Paul <gafgafgarion05@gmail.com>
2022-08-05 16:08:38 +0300

    my solution

--- 8 kyu Pole Vault Starting Marks.py
+++ 8 kyu Pole Vault Starting Marks.py
@@ -1,5 +1,9 @@
+first = (10.67 - 9.45) / (1.83 - 1.52)
+second = 9.45 - first * 1.52
+
+
 def starting_mark(height):
-    pass
+    return round(first * height + second, 2)
 
 
 print(starting_mark(1.52), 9.45)
