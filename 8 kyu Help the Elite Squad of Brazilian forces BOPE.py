dafb8701c31d3fb4fd130156c6ffe8a88d43b4be
Paul <gafgafgarion05@gmail.com>
2022-08-05 14:25:48 +0300

    1

--- 8 kyu Help the Elite Squad of Brazilian forces BOPE.py
+++ 8 kyu Help the Elite Squad of Brazilian forces BOPE.py
@@ -1,18 +1,13 @@
-from typing import Tuple
+weapons_dct = {
+    'PT92': 17,
+    'M4A1': 30,
+    'M16A2': 30,
+    'PSG1': 5
+}
 
 
-def mag_number(str, int):
-    """
-    Returns the number of magazines a soldier needs given a
-    tuple containing the name of a soldier's weapon and
-    the number of streets the soldier has to patrol.
-
-     mag_number(("PT92", 6))
-    2
-     mag_number(("M4A1", 6))
-    1
-    """
-    pass
+def mag_number(str_, int_):
+    return weapons_dct[str_]
 
 
 print(mag_number("PT92", 6), 2)
