f7b3b05843ed1fc143fa32f711b6b4ec3115ea24
Paul <gafgafgarion05@gmail.com>
2022-08-06 13:39:59 +0300

    was mistake in "open" coz no encoding, but I add it and all hrefs at the point

--- 0 learn_to_parse_3.py
+++ 0 learn_to_parse_3.py
@@ -1,4 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
+# url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
+#
+# headers = {
+#     'Accept': '*/*',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+# }
+#
+# req = requests.get(url, headers=headers)
+# src = req.text
+#
+# # сохранили инфу себе на комп, что б не забанили, кодировка что б небыло "?????"
+# with open('index.html', 'w', encoding='utf-8-sig') as file:
+#     file.write(src)
+
+
+with open('index.html', encoding='utf-8-sig') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+all_products_href = soup.find_all(class_='mzr-tc-group-item-href')
+
+for item in all_products_href:
+    print(item)
