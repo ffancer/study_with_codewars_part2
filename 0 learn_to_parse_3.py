71b426d895d3556d713442a4be23d5c7f3227085
Paul <gafgafgarion05@gmail.com>
2022-08-08 12:00:20 +0300

    delete some symbols from JSON file

--- 0 learn_to_parse_3.py
+++ 0 learn_to_parse_3.py
@@ -3,10 +3,10 @@ import requests
 import json
 # url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
 #
-# headers = {
-#     'Accept': '*/*',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-# }
+headers = {
+    'Accept': '*/*',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+}
 #
 # req = requests.get(url, headers=headers)
 # src = req.text
@@ -17,23 +17,39 @@ import json
 
 
 # без энкодинга ошибка
-with open('index.html', encoding='utf-8-sig') as file:
-    src = file.read()
+# with open('index.html', encoding='utf-8-sig') as file:
+#     src = file.read()
+#
+# soup = BeautifulSoup(src, 'lxml')
+# all_products_href = soup.find_all(class_='mzr-tc-group-item-href')
+#
+#
+# all_categories_dct = {}
+# for item in all_products_href:
+#     item_text = item.text
+#     item_href = "http://health-diet.ru" + item.get('href')
+#     # print(f'{item_text}: {item_href}')
+#
+#     # пополняем словарь
+#     all_categories_dct[item_text] = item_href
+#
+#
+# # сохраняем JSON file
+# with open('all_categories_dct.json', 'w', encoding='utf-8') as file:
+#     # indent = отступы, без нее все данные будут в строку; ensure помогает в работе с кириллицей и не экранирует
+#     json.dump(all_categories_dct, file, indent=4, ensure_ascii=False)
 
-soup = BeautifulSoup(src, 'lxml')
-all_products_href = soup.find_all(class_='mzr-tc-group-item-href')
 
 
-all_categories_dct = {}
-for item in all_products_href:
-    item_text = item.text
-    item_href = "http://health-diet.ru" + item.get('href')
-    # print(f'{item_text}: {item_href}')
-
-    # пополняем словарь
-    all_categories_dct[item_text] = item_href
+with open('all_categories_dct.json', encoding='utf-8') as file:
+    all_categories = json.load(file)
+# print(all_categories)  # проверяем файл
 
 
-# сохраняем JSON file
-with open('all_categories_dct.json', 'w', encoding='utf-8') as file:
-    json.dump(all_categories_dct, file, indent=4, ensure_ascii=False)
+for category_name, category_href in all_categories.items():
+    lst = [',', '-', ' ', "'"]
+
+    for item in lst:
+        if item in category_name:
+            category_name = category_name.replace(item, '_')
+    print(category_name)
