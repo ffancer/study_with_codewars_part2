44304bc048943f8266e2be8117bf80b6a1dbdb10
Paul <gafgafgarion05@gmail.com>
2022-08-08 11:46:33 +0300

    JSON create

--- 0 learn_to_parse_3.py
+++ 0 learn_to_parse_3.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import json
 # url = 'http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie'
 #
 # headers = {
@@ -23,10 +23,17 @@ with open('index.html', encoding='utf-8-sig') as file:
 soup = BeautifulSoup(src, 'lxml')
 all_products_href = soup.find_all(class_='mzr-tc-group-item-href')
 
+
+all_categories_dct = {}
 for item in all_products_href:
     item_text = item.text
     item_href = "http://health-diet.ru" + item.get('href')
-    print(f'{item_text}: {item_href}')
+    # print(f'{item_text}: {item_href}')
+
+    # пополняем словарь
+    all_categories_dct[item_text] = item_href
 
 
-# 6-27
+# сохраняем JSON file
+with open('all_categories_dct.json', 'w', encoding='utf-8') as file:
+    json.dump(all_categories_dct, file, indent=4, ensure_ascii=False)
