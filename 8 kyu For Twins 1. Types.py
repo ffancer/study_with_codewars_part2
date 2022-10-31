9e2c72a399c89e0ad9d4a057b999d5a093bc6391
Paul <gafgafgarion05@gmail.com>
2022-10-31 12:21:22 +0300

    -

--- 8 kyu For Twins 1. Types.py
+++ 8 kyu For Twins 1. Types.py
@@ -1,6 +1,8 @@
 def type_validation(variable, _type):
-    # your code here
-    pass
+    types = {'int': int,
+             'float': float}
+    if _type in types:
+        return types[_type]
 
 
 print(type_validation(42, "int"), True)
