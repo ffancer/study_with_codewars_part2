8eb4dcbd9ed99d40a6de277e8bc14978f57857f4
Paul <gafgafgarion05@gmail.com>
2022-10-31 13:02:22 +0300

    solution

--- 8 kyu For Twins 1. Types.py
+++ 8 kyu For Twins 1. Types.py
@@ -1,21 +1,6 @@
 def type_validation(variable, _type):
-    if _type == 'int':
-        _type = int
-    elif _type == 'float':
-        _type = float
-    elif _type == bool:
-        _type = bool
-    elif _type == list:
-        _type = list
-    elif _type == dict:
-        _type = dict
-    elif _type is None:
-        _type = None
-    elif _type is set:
-        _type = set
-    elif _type is tuple:
-        _type = tuple
-    return _type == type(variable)
+    return type(variable).__name__ == _type
+
 
 print(type_validation(42, "int"), True)
 print(type_validation("42", "int"), False)
