13bb385d30a07d4bbb23b6f88678ce32b25ed12b
Paul <gafgafgarion05@gmail.com>
2023-03-31 10:12:18 +0300

    init

---
+++ 8 kyu Be Concise IV - Index of an element in an array.py
@@ -0,0 +1,28 @@
+from math import pi
+
+
+def find(array, element):
+    for i in range(len(array)):
+        if array[i] == element:
+            return i
+    return 'Not found'
+
+
+array = [2, 3, 5, 7, 11]
+print(find(array, 5), 2)
+print(find(array, 11), 4)
+print(find(array, 3), 1)
+print(find(array, 2), 0)
+print(find(array, 7), 3)
+print(find(array, 1), 'Not found')
+print(find(array, 8), 'Not found')
+
+array = [True, 'Hello World', False, 'Lorem Ipsum', 6, pi]
+print(find(array, 'Hello World'), 1)
+print(find(array, 'lorem ipsum'), 'Not found')
+print(find(array, 'Lorem Ipsum'), 3)
+print(find(array, False), 2)
+print(find(array, True), 0)
+print(find(array, pi), 5)
+print(find(array, 3.14), 'Not found')
+print(find(array, 6), 4)
