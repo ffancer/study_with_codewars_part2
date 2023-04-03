7bbb1b4e49411729e7fb6c4af96d119f3f39172e
Paul <gafgafgarion05@gmail.com>
2023-04-03 09:06:32 +0300

    init

---
+++ 7 kyu Valid Parentheses.py
@@ -0,0 +1,21 @@
+def valid_parentheses(paren_str):
+    pass
+
+
+# all True
+print(valid_parentheses("()"))
+print(valid_parentheses("((()))"))
+print(valid_parentheses("()()()"))
+print(valid_parentheses("(()())()"))
+print(valid_parentheses("()(())((()))(())()"))
+
+# all False
+print(valid_parentheses(")("))
+print(valid_parentheses("()()("))
+print(valid_parentheses("((())"))
+print(valid_parentheses("())(()"))
+print(valid_parentheses(")()"))
+print(valid_parentheses(")"))
+
+# "Should return True for empty strings"
+print(valid_parentheses(""))
