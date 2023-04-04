fe0ec86ecfd8bfabf5b305fd25f7a5ac5c446300
Paul <gafgafgarion05@gmail.com>
2023-04-04 08:52:26 +0300

    init

---
+++ 7 kyu Keep the Order.py
@@ -0,0 +1,15 @@
+def keep_order(ary, val):
+    # your code here
+    return
+
+
+print(keep_order([1, 2, 3, 4, 7], 5), 4, "Testing keep_order([1, 2, 3, 4, 7], 5)")
+print(keep_order([1, 2, 3, 4, 7], 0), 0, "Testing keep_order([1, 2, 3, 4, 7], 0)")
+print(keep_order([1, 1, 2, 2, 2], 2), 2, "Testing keep_order([1, 1, 2, 2, 2], 2)")
+print(keep_order([1, 2, 3, 4], 5), 4, "Testing keep_order([1, 2, 3, 4], 5)")
+print(keep_order([1, 2, 3, 4], -1), 0, "Testing keep_order([1, 2, 3, 4], -1)")
+print(keep_order([1, 2, 3, 4], 2), 1, "Testing keep_order([1, 2, 3, 4], 2)")
+print(keep_order([1, 2, 3, 4], 0), 0, "Testing keep_order([1, 2, 3, 4], 0)")
+print(keep_order([1, 2, 3, 4], 1), 0, "Testing keep_order([1, 2, 3, 4], 1)")
+print(keep_order([1, 2, 3, 4], 2), 1, "Testing keep_order([1, 2, 3, 4], 2)")
+print(keep_order([1, 2, 3, 4], 3), 2, "Testing keep_order([1, 2, 3, 4], 3)")
