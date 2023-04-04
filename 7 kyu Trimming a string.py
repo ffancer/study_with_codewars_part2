4c974dc0f7b6097037cd9af2226f04b35dcb0a34
Paul <gafgafgarion05@gmail.com>
2023-04-04 09:25:27 +0300

    init

---
+++ 7 kyu Trimming a string.py
@@ -0,0 +1,11 @@
+def trim(phrase, size):
+    return phrase
+
+
+print(trim("Creating kata is fun", 14), "Creating ka...")
+print(trim("He", 1), "H...")
+print(trim("Hey", 2), "He...")
+print(trim("Hey", 3), "Hey")
+print(trim("Coding rocks", 12), "Coding rocks")
+print(trim("Code Wars is pretty rad", 50), "Code Wars is pretty rad")
+print(trim("London is freezing", 18), "London is freezing")
