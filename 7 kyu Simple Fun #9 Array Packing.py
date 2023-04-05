102cb34bdd88793886585bb9603c49b6f5be10e0
Paul <gafgafgarion05@gmail.com>
2023-04-05 09:15:57 +0300

    init

---
+++ 7 kyu Simple Fun #9 Array Packing.py
@@ -0,0 +1,9 @@
+def array_packing(arr):
+    pass
+
+
+print(array_packing([24, 85, 0]), 21784)
+print(array_packing([23, 45, 39]), 2567447)
+print(array_packing([1, 1]), 257)
+print(array_packing([0]), 0)
+print(array_packing([255, 255, 255, 255]), 4294967295)
