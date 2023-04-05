04ee17d92a99fbe4284c39d315e99ac934e0fc68
Paul <gafgafgarion05@gmail.com>
2023-04-05 09:28:26 +0300

    my solution

--- 7 kyu Simple Fun #9 Array Packing.py
+++ 7 kyu Simple Fun #9 Array Packing.py
@@ -8,13 +8,15 @@ def array_packing(arr):
             i_in_2 = i_in_2.zfill(8)
         lst.append(i_in_2)
 
+    lst = reversed(lst)
+
     for i in lst:
         total += i
 
-    return total
+    return int(total, 2)
 
 
-print(array_packing([24, 85, 0]), 21784)
+print(array_packing([24, 85, 0]), 21784)  # [00011000, 01010101, 00000000]
 print(array_packing([23, 45, 39]), 2567447)
 print(array_packing([1, 1]), 257)
 print(array_packing([0]), 0)
