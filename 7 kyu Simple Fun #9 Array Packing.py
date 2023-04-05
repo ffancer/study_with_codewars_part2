b85d23795a2dea9f3a049dcaf4cd36f47e517198
Paul <gafgafgarion05@gmail.com>
2023-04-05 09:32:02 +0300

    my solution

--- 7 kyu Simple Fun #9 Array Packing.py
+++ 7 kyu Simple Fun #9 Array Packing.py
@@ -21,8 +21,3 @@ print(array_packing([23, 45, 39]), 2567447)
 print(array_packing([1, 1]), 257)
 print(array_packing([0]), 0)
 print(array_packing([255, 255, 255, 255]), 4294967295)
-
-
-# print(bin(24)[2:], bin(85)[2:])
-# print(bin(int('11000', 2) + int('1010101', 2))[2:])
-# print(int('000000000101010100011000', 2))
