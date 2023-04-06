2cccc834905e7a9afa16fb9dea241c99881d7c76
Paul <gafgafgarion05@gmail.com>
2023-04-06 09:21:40 +0300

    init

---
+++ 7 kyu Double value every next call.py
@@ -0,0 +1,11 @@
+class Class:
+    @staticmethod
+    def get_number():
+        return 1
+
+
+print(Class.get_number(),  1, "1st call should return 1")
+print(Class.get_number(),  2, "2nd call should return 2")
+print(Class.get_number(),  4, "3rd call should return 4")
+print(Class.get_number(),  8, "4th call should return 8")
+print(Class.get_number(), 16, "5th call should return 16")
