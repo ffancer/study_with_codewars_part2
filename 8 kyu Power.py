e4e9637b94e4de2862ea38482275483da29cea20
Paul <gafgafgarion05@gmail.com>
2023-04-07 10:11:23 +0300

    my solution

--- 8 kyu Power.py
+++ 8 kyu Power.py
@@ -1,15 +1,12 @@
 def number_to_pwr(number, p):
-    total = 0
-    ans = 1
-    while p != 0:
-        total = number * number
-        ans /= total
-        p -= 1
+    total = 1
 
-    return ans
+    for i in range(p):
+        total *= number
+
+    return total
 
 
-
-# print(number_to_pwr(4, 2), 16)
+print(number_to_pwr(4, 2), 16)
 print(number_to_pwr(10, 4), 10000)
-# print(number_to_pwr(10, 0), 1)
+print(number_to_pwr(10, 0), 1)
