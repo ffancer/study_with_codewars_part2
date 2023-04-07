963e525d0b7046545b701dbc23a7335eb1dd9ecc
Paul <gafgafgarion05@gmail.com>
2023-04-07 10:07:11 +0300

    -

--- 8 kyu Power.py
+++ 8 kyu Power.py
@@ -1,7 +1,15 @@
 def number_to_pwr(number, p):
-    return pow(number, p)
+    total = 0
+    ans = 1
+    while p != 0:
+        total = number * number
+        ans /= total
+        p -= 1
+
+    return ans
 
 
-print(number_to_pwr(4, 2), 16)
+
+# print(number_to_pwr(4, 2), 16)
 print(number_to_pwr(10, 4), 10000)
-print(number_to_pwr(10, 0), 1)
+# print(number_to_pwr(10, 0), 1)
