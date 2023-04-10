30e9ef5d0334ec3ff2b676303cdfb332989bd83a
Paul <gafgafgarion05@gmail.com>
2023-04-10 09:34:22 +0300

    my solution

--- 8 kyu Byte me!.py
+++ 8 kyu Byte me!.py
@@ -1,15 +1,15 @@
+import sys
+
+
 def total_bytes(object):
-    # for i in object:
-    # return object.encode('utf-8')
-    for i in object:
-        print(ord(i))
+    return sys.getsizeof(object)
 
 
 print(total_bytes("hello"), 54, "hello")
-# print(total_bytes(123), 28, 123)
-# print(total_bytes('[":)", 1]'), 58, '[":)", 1]')
-# print(total_bytes({'john': 19}), 232, '{"john": 19}')
-# print(total_bytes("¡◢龘"), 80)
-# print(total_bytes(999_999), 28)
-# print(total_bytes((1,2)), 56)
-# print(total_bytes("㋛"*12345), 24764)
+print(total_bytes(123), 28, 123)
+print(total_bytes('[":)", 1]'), 58, '[":)", 1]')
+print(total_bytes({'john': 19}), 232, '{"john": 19}')
+print(total_bytes("¡◢龘"), 80)
+print(total_bytes(999_999), 28)
+print(total_bytes((1, 2)), 56)
+print(total_bytes("㋛" * 12345), 24764)
